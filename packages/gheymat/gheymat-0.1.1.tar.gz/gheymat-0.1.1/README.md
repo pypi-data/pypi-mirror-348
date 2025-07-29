8462aff764adc04e80534aa268bafa56b419ca52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:37 +0000

    Add package gheymat/0.1.1/gheymat-0.1.1.tar.gz

---
+++ packages/gheymat/gheymat-0.1.1.tar.gz/gheymat-0.1.1/README.md
@@ -0,0 +1,17 @@
+# قیمت لحظه‌ای دلار و پوند و تبدیل به ریال | Gheymat
+
+کتابخانه‌ای برای دریافت قیمت دلار و پوند به صورت لحظه‌ای و تبدیل آن به ریال.
+
+  
+## نصب
+
+```bash
+pip install gheymat
+```
+## استفاده
+```python
+from gheymat.currency import USD #or GBP
+
+dollar_price = USD()
+print(dollar_price)
+```
---
+++ packages/gheymat/gheymat-0.1.1.tar.gz/gheymat-0.1.1/gheymat/currency.py
@@ -0,0 +1,28 @@
+import requests as rq 
+from bs4 import BeautifulSoup as bs 
+
+
+def USD():
+    url = 'https://www.tgju.org/profile/price_dollar_rl'
+    response = rq.get(url)
+    soup = bs(response.text, 'html.parser')
+    price = soup.find('span', {'data-col': 'info.last_trade.PDrCotVal'}).text
+    if price:
+        gh = str(price).replace(',', '')
+        rial = int(gh) // 10
+        return rial
+    else:
+        return 'Dollar price not found.'
+
+def GBP():
+    url = 'https://www.tgju.org/profile/price_gbp'
+    response = rq.get(url)
+    soup = bs(response.text, 'html.parser')
+    price = soup.find('span', {'data-col': 'info.last_trade.PDrCotVal'}).text
+    if price:
+        gh = str(price).replace(',', '')
+        rial = int(gh) // 10
+        return rial
+    else:
+        return 'Pound price not found.'    
+    
