430a034ee838e39fe25e58686ffd00b35e9286ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:08:19 +0000

    Add package AgentService/0.0.13/agentservice-0.0.13-py3-none-any.whl

---
+++ packages/AgentService/agentservice-0.0.13-py3-none-any.whl/AgentService/agent/response.py
@@ -0,0 +1,20 @@
+
+import enum
+from AgentService.dtypes.message import Tool
+
+
+class AgentResponseType(enum.Enum):
+    tools = "tools"
+    answer = "answer"
+
+
+class AgentResponse:
+    def __init__(self, type: AgentResponseType, content: str | list[Tool]):
+        if isinstance(type, str):
+            type = AgentResponseType(type)
+
+        self.type = type
+        self.content = content
+
+    def __str__(self):
+        return "AgentResponse(type=" + self.type.value + ", content=" + str(self.content) + ")"
