827f9c2de3f1fab89916a6ae0eb8ea7d981fcaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:00 +0000

    Add package irpf-investidor/2025.16.5/irpf_investidor-2025.16.5-py3-none-any.whl

---
+++ packages/irpf-investidor/irpf_investidor-2025.16.5-py3-none-any.whl/irpf_investidor/formatting.py
@@ -0,0 +1,40 @@
+"""Formatting module."""
+
+import locale
+import math
+from collections.abc import Callable
+
+import irpf_investidor.responses as res
+
+
+def set_pt_br_locale() -> res.ResponseFailure | res.ResponseSuccess:
+    """Set pt_BR locale."""
+    # Get available locale from shell `locale -a`
+    supported_locales = ["pt_BR.utf8", "pt_BR.UTF-8"]
+    for loc in supported_locales:
+        try:
+            locale.setlocale(locale.LC_ALL, loc)
+            return res.ResponseSuccess()
+        except locale.Error:
+            pass
+    return res.ResponseFailure(
+        res.ResponseTypes.SYSTEM_ERROR,
+        "locale pt_BR não encontrado, confira a documentação para mais informações.",
+    )
+
+
+def get_currency_format() -> Callable[..., str]:
+    """Return currency function."""
+    return locale.currency
+
+
+def fmt_money(amount: float, ndigits: int = 2) -> str:
+    """Return padded and rounded value."""
+    if math.isnan(amount):
+        return "N/A"
+    rounded = round(amount, ndigits)
+    result = str(rounded).replace(".", ",")
+    rounded_digits = result.split(",")[1]
+    missing_digits = ndigits - len(rounded_digits)
+    padded_result = result + "0" * missing_digits
+    return padded_result
---
+++ packages/irpf-investidor/irpf_investidor-2025.16.5-py3-none-any.whl/irpf_investidor/prompt.py
@@ -0,0 +1,28 @@
+"""Prompt module."""
+
+import prompt_toolkit.shortcuts as shortcuts
+
+TITLE = "IRPF Investidor"
+
+
+def select_trades(trades: list[tuple[int, str]]) -> list[int]:
+    """Checkbox selection of auction trades."""
+    text = (
+        "Informe as operações realizadas em horário de leilão para cálculo dos "
+        "emolumentos.\nEssa informação é obtida através de sua corretora."
+    )
+    while True:
+        operations: list[int] = shortcuts.checkboxlist_dialog(
+            title=TITLE,
+            text=text,
+            values=trades,
+        ).run()
+        if not operations or len(operations) == 0:
+            confirmed = shortcuts.yes_no_dialog(
+                title=TITLE,
+                text="Nenhuma operação selecionada.\nIsso está correto?",
+            ).run()
+            if confirmed:
+                return []
+        else:
+            return operations
