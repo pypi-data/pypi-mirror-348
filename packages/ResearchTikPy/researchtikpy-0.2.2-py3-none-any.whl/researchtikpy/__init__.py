3a07f2791b971b80d59ad4e319714191413bd4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:07:15 +0000

    Add package ResearchTikPy/0.2.2/researchtikpy-0.2.2-py3-none-any.whl

---
+++ packages/ResearchTikPy/researchtikpy-0.2.2-py3-none-any.whl/researchtikpy/__init__.py
@@ -0,0 +1,27 @@
+from .get_access_token import get_access_token
+from .social_graph import get_followers, get_following
+from .get_liked_videos import get_liked_videos
+from .get_pinned_videos import get_pinned_videos
+from .get_users_info import get_users_info
+from .get_video_comments import get_video_comments
+from .get_query import get_videos_hashtag, get_videos_query, get_videos_info
+from .query_lang import Fields, Operators, Condition, Query, RegionCodes, VideoLengths
+
+__all__ = [
+    'get_access_token',
+    'get_followers',
+    'get_following',
+    'get_liked_videos',
+    'get_pinned_videos',
+    'get_users_info',
+    'get_video_comments',
+    'get_videos_hashtag',
+    'get_videos_query',
+    'get_videos_info',
+    'Fields',
+    'Operators',
+    'Condition',
+    'Query',
+    'RegionCodes',
+    'VideoLengths',
+]
---
+++ packages/ResearchTikPy/researchtikpy-0.2.2-py3-none-any.whl/researchtikpy/get_access_token.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import requests
+from cachetools.func import ttl_cache
+from logging import getLogger
+
+
+logger = getLogger(__name__)
+
+
+def get_access_token(client_key, client_secret):
+    """
+    Requests an access token from the TikTok API using client credentials.
+
+    Parameters:
+    - client_key (str): The client key provided by TikTok.
+    - client_secret (str): The client secret provided by TikTok.
+
+    Returns:
+    - dict: A dictionary containing the access token, its expiry duration, and the token type.
+
+    Raises:
+    - Exception: If the request to the TikTok API fails or is not successful.
+    """
+    endpoint_url = "https://open.tiktokapis.com/v2/oauth/token/"
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {
+        'client_key': client_key,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials',
+    }
+
+    response = requests.post(endpoint_url, headers=headers, data=data)
+
+    if response.status_code == 200:
+        response_json = response.json()
+        return {
+            "access_token": response_json['access_token'],
+            "expires_in": response_json['expires_in'],
+            "token_type": response_json['token_type']
+        }
+    else:
+        raise Exception(f"Failed to obtain access token: {response.text}")
+
+
+@ttl_cache(ttl=7200 - 1)  # to be safe
+def get_access_token_cached() -> str:
+    client_key = os.environ["TIKTOK_CLIENT_KEY"]
+    client_secret = os.environ["TIKTOK_CLIENT_SECRET"]
+    logger.info("Getting access token...")
+    data: dict = get_access_token(
+        client_key=client_key, client_secret=client_secret
+    )
+    return data["access_token"]
