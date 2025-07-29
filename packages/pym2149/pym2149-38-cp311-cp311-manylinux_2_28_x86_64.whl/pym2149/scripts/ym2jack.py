e9c5e246e5e1d1806df178158b9774e776a52e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:23 +0000

    Add package pym2149/38/pym2149-38-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/pym2149/pym2149-38-cp311-cp311-manylinux_2_28_x86_64.whl/pym2149/scripts/ym2jack.py
@@ -0,0 +1,42 @@
+# Copyright 2014, 2018, 2019, 2020 Andrzej Cichocki
+
+# This file is part of pym2149.
+#
+# pym2149 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pym2149 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
+
+'Play a YM file via JACK.'
+from . import boot
+from ..client import jack
+from ..config import ConfigName
+from ..timerimpl import SyncTimer
+from ..util import MainThread
+from ..ymformat import YMOpen
+from ..ymplayer import PhysicalBundle, Player
+from diapyr.start import Started
+from foyndation import initlogging
+
+def main():
+    initlogging()
+    config, di = boot(ConfigName('inpath'))
+    with di:
+        di.add(YMOpen)
+        jack.configure(di)
+        di.add(SyncTimer)
+        di.add(PhysicalBundle)
+        di.add(Player)
+        di.all(Started)
+        di(MainThread).sleep()
+
+if '__main__' == __name__:
+    main()
