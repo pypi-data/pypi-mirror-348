68a82f26525a28f8b9c97daa102df1de74557ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:20 +0000

    Add package maleo-foundation/0.2.10/maleo_foundation-0.2.10.tar.gz

---
+++ packages/maleo-foundation/maleo_foundation-0.2.10.tar.gz/maleo_foundation-0.2.10/maleo_foundation/expanded_types/encryption/aes.py
@@ -0,0 +1,13 @@
+from typing import Union
+from maleo_foundation.models.transfers.results.encryption.aes import MaleoFoundationAESEncryptionResultsTransfers
+
+class MaleoFoundationAESEncryptionResultsTypes:
+    Encrypt = Union[
+        MaleoFoundationAESEncryptionResultsTransfers.Fail,
+        MaleoFoundationAESEncryptionResultsTransfers.Encrypt
+    ]
+
+    Decrypt = Union[
+        MaleoFoundationAESEncryptionResultsTransfers.Fail,
+        MaleoFoundationAESEncryptionResultsTransfers.Decrypt
+    ]
