4bbfeb2168b61c52060b3c69125a9a2ecd9b39aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:34 +0000

    Add package sipmessage/0.6.4/sipmessage-0.6.4-py3-none-any.whl

---
+++ packages/sipmessage/sipmessage-0.6.4-py3-none-any.whl/sipmessage/grammar.py
@@ -0,0 +1,75 @@
+#
+# Copyright (C) Spacinov SAS
+# Distributed under the 2-clause BSD license
+#
+
+import re
+import string
+
+
+def cset(chars: str) -> str:
+    """
+    Transform a collection of characters into a regular expression
+    character set.
+    """
+    return "[" + chars.replace("-", "\\-").replace("[", "\\[").replace("]", "\\]") + "]"
+
+
+def simplify_whitespace(value: str) -> str:
+    """
+    Replace any whitespace by a single space.
+
+    According to RFC 3261, all linear white space, including folding,
+    has the same semantics as SP.
+    """
+    return re.sub(r"\s+", " ", value).strip()
+
+
+# Character collections.
+C_ALPHA = string.ascii_letters
+C_ALPHANUM = string.ascii_letters + string.digits
+C_MARK = "-_.!~*/'()"
+C_TOKEN = C_ALPHANUM + "-.!%*_+`'~"
+C_UNRESERVED = C_ALPHANUM + C_MARK
+C_PASSWORD_SAFE = C_UNRESERVED + "&=+$,"
+C_USER_SAFE = C_UNRESERVED + "&=+$,;?/"
+C_URI_PARAM_SAFE = C_UNRESERVED + "[]/:&+$"
+
+# Regular expression fragments.
+ALPHA = cset(C_ALPHA)
+ALPHANUM = cset(C_ALPHANUM)
+DIGIT = "\\d"
+HEXDIG = cset(string.hexdigits)
+ESCAPED = f"%{HEXDIG}{{2}}"
+LWS = "[ ]+"
+SWS = "[ ]*"
+TOKEN = f"{cset(C_TOKEN)}+"
+QUOTED_STRING = '"(?:[^"]|\\")*"'
+
+COMMA = f"{SWS},{SWS}"
+EQUAL = f"{SWS}={SWS}"
+SEMI = f"{SWS};{SWS}"
+SLASH = f"{SWS}/{SWS}"
+
+HEXSEQ = f"{HEXDIG}{{1,4}}(?::{HEXDIG}{{1,4}})*"
+HEXPART = f"(?:{HEXSEQ}|{HEXSEQ}::(?:{HEXSEQ})?|::(?:{HEXSEQ})?)"
+IPV4ADDRESS = f"{DIGIT}{{1,3}}\\.{DIGIT}{{1,3}}\\.{DIGIT}{{1,3}}\\.{DIGIT}{{1,3}}"
+IPV6ADDRESS = f"{HEXPART}(?::{IPV4ADDRESS})?"
+IPV6REFERENCE = f"\\[{IPV6ADDRESS}\\]"
+
+DOMAINLABEL = f"{ALPHANUM}+([\\-]+{ALPHANUM}+)*"
+TOPLABEL = f"{ALPHA}+([\\-]+{ALPHANUM}+)*"
+HOSTNAME = f"(?:{DOMAINLABEL}\\.)*{TOPLABEL}[\\.]?"
+
+HOST = f"(?:{HOSTNAME}|{IPV4ADDRESS}|{IPV6REFERENCE})"
+USER = f"(?:{cset(C_USER_SAFE)}|{ESCAPED})+"
+PASSWORD = f"(?:{cset(C_PASSWORD_SAFE)}|{ESCAPED})+"
+PORT = f"{DIGIT}+"
+
+URI_PARAMCHAR = f"(?:{cset(C_URI_PARAM_SAFE)}|{ESCAPED})"
+URI_PARAM = f"{URI_PARAMCHAR}+(?:={URI_PARAMCHAR}+)?"
+
+GENERIC_VALUE = f"(?:{TOKEN}|{HOST}|{QUOTED_STRING})"
+GENERIC_PARAM = f"{TOKEN}(?:{EQUAL}{GENERIC_VALUE})?"
+
+AUTH_PARAM = f"{TOKEN}{EQUAL}(?:{TOKEN}|{QUOTED_STRING})"
---
+++ packages/sipmessage/sipmessage-0.6.4-py3-none-any.whl/sipmessage/via.py
@@ -0,0 +1,77 @@
+#
+# Copyright (C) Spacinov SAS
+# Distributed under the 2-clause BSD license
+#
+
+import dataclasses
+import re
+
+from . import grammar, utils
+from .parameters import Parameters
+
+VIA_EXCEPTION = ValueError("Via is not valid")
+VIA_PATTERN = re.compile(
+    f"^SIP{grammar.SLASH}2\\.0{grammar.SLASH}(?P<transport>{grammar.TOKEN}) "
+    f"(?P<host>{grammar.HOST})"
+    f"(?::(?P<port>{grammar.PORT}))?"
+    f"(?P<parameters>(?:{grammar.SEMI}{grammar.GENERIC_PARAM})*)"
+)
+
+
+@dataclasses.dataclass(frozen=True)
+class Via:
+    """
+    A `Via` header, indicating a reponse location for a transaction.
+    """
+
+    transport: str
+    'The transport for the transaction, e.g `"UDP"`, `"TCP"`, `"TLS"`.'
+
+    host: str
+    "The host to which responses should to be sent."
+
+    port: int | None = None
+    "The port to which responses should to be sent."
+
+    parameters: Parameters = dataclasses.field(default_factory=Parameters)
+    "The parameters of the address."
+
+    @classmethod
+    def parse(cls, value: str) -> "Via":
+        """
+        Parse the given string into a :class:`Via` instance.
+
+        If parsing fails, a :class:`ValueError` is raised.
+        """
+        return utils.parse_single(cls._parse_one, VIA_EXCEPTION, value)
+
+    @classmethod
+    def parse_many(cls, value: str) -> "list[Via]":
+        """
+        Parse the given string into a list of :class:`Via` instances.
+
+        If parsing fails, a :class:`ValueError` is raised.
+        """
+        return utils.parse_many(cls._parse_one, VIA_EXCEPTION, value)
+
+    @classmethod
+    def _parse_one(cls, value: str) -> tuple["Via", str]:
+        m = VIA_PATTERN.match(value)
+        if m is None:
+            raise VIA_EXCEPTION
+
+        port = m.group("port")
+
+        return cls(
+            transport=m.group("transport"),
+            host=m.group("host"),
+            port=int(port) if port else None,
+            parameters=Parameters.parse(m.group("parameters")),
+        ), value[m.end() :]
+
+    def __str__(self) -> str:
+        s = f"SIP/2.0/{self.transport} {self.host}"
+        if self.port is not None:
+            s += f":{self.port}"
+        s += str(self.parameters)
+        return s
