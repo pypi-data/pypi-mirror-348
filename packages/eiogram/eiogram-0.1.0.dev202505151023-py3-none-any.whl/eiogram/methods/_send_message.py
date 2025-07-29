5d1b3bb1295c6fb67ac8c06695fa01fe3ff68973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:04 +0000

    Add package eiogram/0.1.0.dev202505151023/eiogram-0.1.0.dev202505151023-py3-none-any.whl

---
+++ packages/eiogram/eiogram-0.1.0.dev202505151023-py3-none-any.whl/eiogram/methods/_send_message.py
@@ -0,0 +1,24 @@
+from typing import Optional, Union
+from ..types import Message, InlineKeyboardMarkup
+from ._base import MethodBase
+
+
+class SendMessage(MethodBase):
+    async def execute(
+        self,
+        chat_id: Union[int, str],
+        text: str,
+        reply_markup: Optional[InlineKeyboardMarkup] = None,
+    ) -> Message:
+        data = {
+            "chat_id": chat_id,
+            "text": text,
+            "parse_mode": "HTML",
+        }
+
+        if reply_markup:
+            data["reply_markup"] = reply_markup.dict()
+        response = await self._make_request("POST", "sendMessage", data)
+        message = Message.from_dict(response["result"])
+        message.set_bot(self.bot)
+        return message
