59666f4a83be10b3d143b7607bc1d196562c1260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:40 +0000

    Add package meishiki/0.3.5/meishiki-0.3.5-py3-none-any.whl

---
+++ packages/meishiki/meishiki-0.3.5-py3-none-any.whl/meishiki/consts.py
@@ -0,0 +1,26 @@
+import sys
+from enum import Enum
+import yaml
+from box import Box
+import importlib.resources
+
+try:
+    f = importlib.resources.read_binary("meishiki", "consts.yaml")
+    raw = yaml.safe_load(f)
+except Exception as e:
+    print("Exception occurred while loading YAML...", file=sys.stderr)
+    print(e, file=sys.stderr)
+    sys.exit(1)
+
+kd = Box(raw)
+
+
+class Sex(Enum):
+    """性別を示すEnumクラス"""
+    MALE = 0  # 男性
+    FEMALE = 1  # 女性
+
+
+class TemplateType(Enum):
+    TYPE_I = "template.html"
+    TYPE_II = "template2.html"
