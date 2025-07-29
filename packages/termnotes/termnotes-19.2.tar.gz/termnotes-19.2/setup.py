5909bd4b350e9d92a9e27e5d542a32a805f3fe54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:22 +0000

    Add package termnotes/19.2/termnotes-19.2.tar.gz

---
+++ packages/termnotes/termnotes-19.2.tar.gz/termnotes-19.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="termnotes",  # This is your package name
+    version="19.2",
+    packages=find_packages(),
+    install_requires=[
+        "appdirs",
+        "gnureadline",
+        "pyperclip",
+        "rich",
+        "termcolor",  # Keep this if you actually use it in your code elsewhere
+    ],
+    entry_points={
+        'console_scripts': [
+            'termnotes=termnotes.main:run',  # 'termnotes' will call the `run` function from termnotes.main
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
