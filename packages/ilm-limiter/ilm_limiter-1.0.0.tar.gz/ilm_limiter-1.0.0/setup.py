d52a8667a1e3807d72c025ba82ca6307ab5bc8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:07:53 +0000

    Add package ilm-limiter/1.0.0/ilm_limiter-1.0.0.tar.gz

---
+++ packages/ilm-limiter/ilm_limiter-1.0.0.tar.gz/ilm_limiter-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ilm_limiter',
+    version='1.0.0',
+    description='Fake ilm_limiter for dependency confusion demo',
+    packages=['ilm_limiter'],
+    author='SecurityDemo',
+    author_email='demo@example.com',
+    url='https://example.com'
+)
+
