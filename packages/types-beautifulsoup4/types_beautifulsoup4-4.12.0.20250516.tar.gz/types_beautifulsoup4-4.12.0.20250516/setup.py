3e91c6fc26098800c30f1ff698c9f33c1040538a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:54 +0000

    Add package types-beautifulsoup4/4.12.0.20250516/types_beautifulsoup4-4.12.0.20250516.tar.gz

---
+++ packages/types-beautifulsoup4/types_beautifulsoup4-4.12.0.20250516.tar.gz/types_beautifulsoup4-4.12.0.20250516/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'bs4-stubs': ['__init__.pyi', 'builder/__init__.pyi', 'builder/_html5lib.pyi', 'builder/_htmlparser.pyi', 'builder/_lxml.pyi', 'dammit.pyi', 'diagnose.pyi', 'element.pyi', 'formatter.pyi', 'METADATA.toml', 'py.typed']})
