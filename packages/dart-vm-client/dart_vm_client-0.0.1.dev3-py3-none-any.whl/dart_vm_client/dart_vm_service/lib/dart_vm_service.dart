ca26794c548145d4c069edf83aa71c891a21fd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:36 +0000

    Add package dart-vm-client/0.0.1.dev3/dart_vm_client-0.0.1.dev3-py3-none-any.whl

---
+++ packages/dart-vm-client/dart_vm_client-0.0.1.dev3-py3-none-any.whl/dart_vm_client/dart_vm_service/lib/dart_vm_service.dart
@@ -0,0 +1,10 @@
+/// Dart VM Service library.
+///
+/// This library provides tools for interacting with the Dart VM Service,
+/// allowing for debugging and introspection of Dart applications.
+library dart_vm_service;
+
+export 'dart_vm_service_tool.dart';
+export 'dart_vm_service_grpc_server.dart';
+export 'src/generated/dart_vm_service.pb.dart';
+export 'src/generated/dart_vm_service.pbgrpc.dart';
