c151ea297413c24ec80c2f0125b8199673b1cbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:12 +0000

    Add package thumbor/7.7.7/thumbor-7.7.7-cp39-abi3-musllinux_1_2_i686.whl

---
+++ packages/thumbor/thumbor-7.7.7-cp39-abi3-musllinux_1_2_i686.whl/thumbor/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com thumbor@googlegroups.com
+
+"""This is the main module in thumbor"""
+
+__version__ = "7.7.7"
+__release_date__ = "15-May-2025"
