29a18acb205e0e6efb657b1b65da94c88754b7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:39 +0000

    Add package google-account-client/1.1.2/google_account_client-1.1.2-py3-none-any.whl

---
+++ packages/google-account-client/google_account_client-1.1.2-py3-none-any.whl/google_account_client/src/models/factory/google_account_factory.py
@@ -0,0 +1,44 @@
+from ..google_account.google_account import GoogleAccount
+from ...settings import Config
+
+import os
+
+class GoogleAccountFactory():
+    def __init__(self, client_secrets: str, port: int = None, enable_logs: bool = None):
+        """
+        Factory to create GoogleAccount instances using a shared client secret (OAuth client).
+
+        Args:
+            client_secrets_path (str): Path to the OAuth client secrets JSON file.
+            logging (int): Port for the local server flow to listen on.
+            enable_logs (bool): If True, enables debug/info logging.
+        """
+        self.name = 'GoogleAccountFactory'
+        self._client_secrets = client_secrets
+        
+        configs = {
+            'port': port,
+            'enable_logs': enable_logs
+        }
+        Config.update_configs(configs)
+
+        if not os.path.exists(client_secrets):
+            raise FileNotFoundError(f'Client secret file not found: {client_secrets}')
+
+    def create_account(self, name: str, token: any = None) -> GoogleAccount:
+        """
+        Creates a new GoogleAccount instance with valid credentials.
+
+        Args:
+            name (str): Account identifier.
+            token (str | dict | Credentials, optional): Path, dict or Credentials instance for the user token.
+
+        Returns:
+            GoogleAccount
+        """
+
+        return GoogleAccount(
+            name=name,
+            user_token=token,
+            credentials=self._client_secrets,
+        )
