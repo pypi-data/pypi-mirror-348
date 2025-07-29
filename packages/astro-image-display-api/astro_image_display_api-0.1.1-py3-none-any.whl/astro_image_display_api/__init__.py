a13131ef6115b288eaba29411db98d9ee684b8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:31 +0000

    Add package astro-image-display-api/0.1.1/astro_image_display_api-0.1.1-py3-none-any.whl

---
+++ packages/astro-image-display-api/astro_image_display_api-0.1.1-py3-none-any.whl/astro_image_display_api/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2025-present Matt Craig <mattwcraig@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .interface_definition import *  # noqa: F403
+from .widget_api_test import *       # noqa: F403
