7ff3580043ef670c2d303148dc2764dafd527dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:16 +0000

    Add package kuavo-humanoid-sdk/1.1.3a1240/kuavo_humanoid_sdk-1.1.3a1240-py3-none-any.whl

---
+++ packages/kuavo-humanoid-sdk/kuavo_humanoid_sdk-1.1.3a1240-py3-none-any.whl/kuavo_humanoid_sdk/common/global_config.py
@@ -0,0 +1,15 @@
+
+class GlobalConfig:
+
+    _instance = None
+
+    use_websocket = False
+    websocket_host = '127.0.0.1'
+    websocket_port = 9090
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+
