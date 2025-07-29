884b1b416ca49b18f822fdb65ca79f884b47f080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:12 +0000

    Add package aquilax/1.2.10/aquilax-1.2.10.tar.gz

---
+++ packages/aquilax/aquilax-1.2.10.tar.gz/aquilax-1.2.10/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# Read version from a file
+version_file = this_directory / "VERSION"
+version = version_file.read_text().strip()
+
+setup(
+    name='aquilax',
+    version=version,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'python-dotenv',
+        'requests-toolbelt',
+        'tabulate',
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'aquilax=aquilax.__main__:main',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/AquilaX-AI/AquilaX-Client",
+    author="Omer",
+    author_email="admin@aquilax.io",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    license="Apache License 2.0",
+    python_requires='>=3.6',
+    include_package_data=True,
+    package_data={
+        '': ['VERSION'],
+    },
+)
