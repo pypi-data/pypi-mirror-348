0c2725093459e1a92b064b252ecde8bd9f8c7923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:50 +0000

    Add package neo4j-viz/0.3.0/neo4j_viz-0.3.0-py3-none-any.whl

---
+++ packages/neo4j-viz/neo4j_viz-0.3.0-py3-none-any.whl/neo4j_viz/node.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from typing import Any, Optional, Union
+
+from pydantic import AliasChoices, BaseModel, Field, field_serializer, field_validator
+from pydantic_extra_types.color import Color, ColorType
+
+from .node_size import RealNumber
+from .options import CaptionAlignment
+
+NodeIdType = Union[str, int]
+
+
+class Node(BaseModel, extra="allow"):
+    """
+    A node in a graph to visualize.
+
+    For more info on each field, see the NVL library docs: https://neo4j.com/docs/nvl/current/base-library/#_nodes
+    """
+
+    #: Unique identifier for the node
+    id: NodeIdType = Field(
+        validation_alias=AliasChoices("id", "nodeId", "node_id"), description="Unique identifier for the node"
+    )
+    #: The caption of the node
+    caption: Optional[str] = Field(None, description="The caption of the node")
+    #: The alignment of the caption text
+    caption_align: Optional[CaptionAlignment] = Field(
+        None, serialization_alias="captionAlign", description="The alignment of the caption text"
+    )
+    #: The size of the caption text. The font size to node radius ratio
+    caption_size: Optional[int] = Field(
+        None,
+        ge=1,
+        le=3,
+        serialization_alias="captionSize",
+        description="The size of the caption text. The font size to node radius ratio",
+    )
+    #: The size of the node as radius in pixel
+    size: Optional[RealNumber] = Field(None, ge=0, description="The size of the node as radius in pixel")
+    #: The color of the node. Allowed input is for example "#FF0000", "red" or (255, 0, 0)
+    color: Optional[ColorType] = Field(None, description="The color of the node")
+    #: Whether the node is pinned in the visualization
+    pinned: Optional[bool] = Field(None, description="Whether the node is pinned in the visualization")
+    #: The x-coordinate of the node
+    x: Optional[RealNumber] = Field(None, description="The x-coordinate of the node")
+    #: The y-coordinate of the node
+    y: Optional[RealNumber] = Field(None, description="The y-coordinate of the node")
+    #: Additional properties of the node that do not directly impact the visualization
+    properties: dict[str, Any] = Field(
+        default_factory=dict,
+        description="Additional properties of the node that do not directly impact the visualization",
+    )
+
+    @field_serializer("color")
+    def serialize_color(self, color: Color) -> str:
+        return color.as_hex(format="long")
+
+    @field_serializer("id")
+    def serialize_id(self, id: Union[str, int]) -> str:
+        return str(id)
+
+    @field_validator("color")
+    @classmethod
+    def cast_color(cls, color: ColorType) -> Color:
+        if not isinstance(color, Color):
+            return Color(color)
+
+        return color
+
+    def to_dict(self) -> dict[str, Any]:
+        return self.model_dump(exclude_none=True, by_alias=True)
---
+++ packages/neo4j-viz/neo4j_viz-0.3.0-py3-none-any.whl/neo4j_viz/relationship.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+from typing import Any, Optional, Union
+from uuid import uuid4
+
+from pydantic import AliasChoices, BaseModel, Field, field_serializer, field_validator
+from pydantic_extra_types.color import Color, ColorType
+
+from .options import CaptionAlignment
+
+
+class Relationship(BaseModel, extra="allow"):
+    """
+    A relationship in a graph to visualize.
+
+    For more info on each field, see the NVL library docs: https://neo4j.com/docs/nvl/current/base-library/#_relationships
+    """
+
+    #: Unique identifier for the relationship
+    id: Union[str, int] = Field(
+        default_factory=lambda: uuid4().hex, description="Unique identifier for the relationship"
+    )
+    #: Node ID where the relationship points from
+    source: Union[str, int] = Field(
+        serialization_alias="from",
+        validation_alias=AliasChoices("source", "sourceNodeId", "source_node_id", "from"),
+        description="Node ID where the relationship points from",
+    )
+    #: Node ID where the relationship points to
+    target: Union[str, int] = Field(
+        serialization_alias="to",
+        validation_alias=AliasChoices("target", "targetNodeId", "target_node_id", "to"),
+        description="Node ID where the relationship points to",
+    )
+    #: The caption of the relationship
+    caption: Optional[str] = Field(None, description="The caption of the relationship")
+    #: The alignment of the caption text
+    caption_align: Optional[CaptionAlignment] = Field(
+        None, serialization_alias="captionAlign", description="The alignment of the caption text"
+    )
+    #: The size of the caption text
+    caption_size: Optional[Union[int, float]] = Field(
+        None, gt=0.0, serialization_alias="captionSize", description="The size of the caption text"
+    )
+    #: The color of the relationship. Allowed input is for example "#FF0000", "red" or (255, 0, 0)
+    color: Optional[ColorType] = Field(None, description="The color of the relationship")
+    #: Additional properties of the relationship that do not directly impact the visualization
+    properties: dict[str, Any] = Field(
+        default_factory=dict,
+        description="Additional properties of the relationship that do not directly impact the visualization",
+    )
+
+    @field_serializer("color")
+    def serialize_color(self, color: Color) -> str:
+        return color.as_hex(format="long")
+
+    @field_serializer("id")
+    def serialize_id(self, id: Union[str, int]) -> str:
+        return str(id)
+
+    @field_serializer("source")
+    def serialize_source(self, source: Union[str, int]) -> str:
+        return str(source)
+
+    @field_serializer("target")
+    def serialize_target(self, target: Union[str, int]) -> str:
+        return str(target)
+
+    @field_validator("color")
+    @classmethod
+    def cast_color(cls, color: ColorType) -> Color:
+        if not isinstance(color, Color):
+            return Color(color)
+
+        return color
+
+    def to_dict(self) -> dict[str, Any]:
+        return self.model_dump(exclude_none=True, by_alias=True)
