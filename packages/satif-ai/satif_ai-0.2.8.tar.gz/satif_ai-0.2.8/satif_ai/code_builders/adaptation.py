d2d0dc2e5d2601bf3d7e892054568890aba22ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:29 +0000

    Add package satif-ai/0.2.8/satif_ai-0.2.8.tar.gz

---
+++ packages/satif-ai/satif_ai-0.2.8.tar.gz/satif_ai-0.2.8/satif_ai/code_builders/adaptation.py
@@ -0,0 +1,9 @@
+from satif_core.code_builders.base import AsyncCodeBuilder, CodeBuilder
+
+
+class AdaptationCodeBuilder(CodeBuilder):
+    pass
+
+
+class AdaptationAsyncCodeBuilder(AsyncCodeBuilder):
+    pass
