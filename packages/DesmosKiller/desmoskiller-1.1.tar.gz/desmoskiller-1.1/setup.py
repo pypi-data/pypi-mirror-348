a6897d98ebaa4865aac02154c2185e09c1345d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:08:06 +0000

    Add package DesmosKiller/1.1/desmoskiller-1.1.tar.gz

---
+++ packages/DesmosKiller/desmoskiller-1.1.tar.gz/desmoskiller-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="DesmosKiller",
+      version="1.1",
+      packages=['uDesmos'],
+      install_requires=['numpy>=2.2.5','matplotlib>=3.10.1'],author="YusufA442",author_email="yusuf365820@gmail.com",
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('readme.txt').read())
