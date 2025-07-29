359bd3fd37e73e56011e6a1e867cd65466ca9055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:07:10 +0000

    Add package aioafero/3.1.0/aioafero-3.1.0-py3-none-any.whl

---
+++ packages/aioafero/aioafero-3.1.0-py3-none-any.whl/aioafero/v1/models/__init__.py
@@ -0,0 +1,23 @@
+__all__ = [
+    "Device",
+    "Light",
+    "Lock",
+    "AferoSensor",
+    "AferoBinarySensor",
+    "Switch",
+    "Valve",
+    "Fan",
+    "ResourceTypes",
+    "Thermostat",
+]
+
+
+from .device import Device
+from .fan import Fan
+from .light import Light
+from .lock import Lock
+from .resource import ResourceTypes
+from .sensor import AferoBinarySensor, AferoSensor
+from .switch import Switch
+from .thermostat import Thermostat
+from .valve import Valve
