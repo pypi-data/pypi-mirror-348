ef9515040e9c38eab758f448fb3aa3139e5be76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:54 +0000

    Add package qzone-api/0.0.10/qzone_api-0.0.10.tar.gz

---
+++ packages/qzone-api/qzone_api-0.0.10.tar.gz/qzone_api-0.0.10/qzone_api/__init__.py
@@ -0,0 +1,11 @@
+from .api import QzoneApi
+
+__all__ = ["QzoneApi"]
+__version__ = "0.0.10"
+__author__ = "Huan Xin"
+__email__ = "mc.xiaolang@foxmail.com"
+
+__all__ = ["QzoneApi"]
+"""
+QQ空间API封装
+"""
---
+++ packages/qzone-api/qzone_api-0.0.10.tar.gz/qzone_api-0.0.10/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qzone_api",
+    version="0.0.10",
+    author="Huan Xin",
+    author_email="mc.xiaolang@foxmail.com",
+    description="QQ空间API封装",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/huanxin996/qzone_api",
+    packages=setuptools.find_packages(),
+    install_requires=['aiohttp>=3.11.11','lxml>=5.3.0','loguru>=0.7.3','requests<=2.32.3','pyzbar>=0.1.9','Pillow>=10.2.0','qrcode>=7.4.2'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
