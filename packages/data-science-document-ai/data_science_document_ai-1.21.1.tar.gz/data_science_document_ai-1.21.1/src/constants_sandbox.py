cd38341506b6a1a55ca876104fb2379fe9662392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:01 +0000

    Add package data-science-document-ai/1.21.1/data_science_document_ai-1.21.1.tar.gz

---
+++ packages/data-science-document-ai/data_science_document_ai-1.21.1.tar.gz/data_science_document_ai-1.21.1/src/constants_sandbox.py
@@ -0,0 +1,11 @@
+"""Config constant params for data science project(s)."""
+# This is the sandbox version of the project parameters but different constants
+project_parameters_sandbox = {
+    # Google Constants
+    "g_ai_project_name": "forto-data-science-sandbox",
+    "g_ai_project_id": "882852108312",
+    # Google Cloud Storage
+    "doc_ai_bucket_name": "ds_document_capture",  # Creating a new bucket bcz the prod bucket name is already taken
+    "doc_ai_bucket_batch_input": "ds_batch_process_docs",
+    "doc_ai_bucket_batch_output": "ds_batch_process_output",
+}
