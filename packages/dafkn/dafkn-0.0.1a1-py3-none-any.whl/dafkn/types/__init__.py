d20c75d27d8a65f8f7c9b5ac4aa486530cf0fc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:55 +0000

    Add package dafkn/0.0.1a1/dafkn-0.0.1a1-py3-none-any.whl

---
+++ packages/dafkn/dafkn-0.0.1a1-py3-none-any.whl/dafkn/types/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .run_task_execute_params import RunTaskExecuteParams as RunTaskExecuteParams
