25c1bd5440d59f44760d1b7b01d1bbdf9dee9b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:54 +0000

    Add package toolviper/0.0.12/toolviper-0.0.12.tar.gz

---
+++ packages/toolviper/toolviper-0.0.12.tar.gz/toolviper-0.0.12/src/toolviper/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+from importlib.metadata import version
+from toolviper.utils.logger import setup_logger
+import toolviper.dask
+import toolviper.utils
+import toolviper.config
+
+__version__ = version("toolviper")
+
+# Setup default logger instance for module
+if not os.getenv("VIPER_LOGGER_NAME"):
+    os.environ["VIPER_LOGGER_NAME"] = "toolviper"
+    setup_logger(
+        logger_name="toolviper",
+        log_to_term=True,
+        log_to_file=False,
+        log_file="toolviper-logfile",
+        log_level="INFO",
+    )
