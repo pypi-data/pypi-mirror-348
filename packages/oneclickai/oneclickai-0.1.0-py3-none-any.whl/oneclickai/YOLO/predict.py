e241afd56e091c15ca3dae42dc556f1a432cb1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:51 +0000

    Add package oneclickai/0.1.0/oneclickai-0.1.0-py3-none-any.whl

---
+++ packages/oneclickai/oneclickai-0.1.0-py3-none-any.whl/oneclickai/YOLO/predict.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+import os
+
+from . import labelDecode
+from .drawImage import draw_result
+
+
+def predict(model, frame, conf=0.5):
+    img_size = 320
+    high_stride = model.output_shape[0][1]
+    low_stride = model.output_shape[1][1]
+    
+    frame = cv2.resize(frame, (img_size, img_size))
+
+    result = model.predict(frame[np.newaxis, ...])
+    annotations = labelDecode.decode(result[0][0], result[1][0], high_stride, low_stride, conf)
+    return annotations
+
+
+
+def predict_and_show(model, frame, conf=0.5, cls_names=None):
+    img_size = 320
+    high_stride = model.output_shape[0][1]
+    low_stride = model.output_shape[1][1]
+    
+    frame = cv2.resize(frame, (img_size, img_size))
+
+    result = model.predict(frame[np.newaxis, ...])
+    annotations = labelDecode.decode(result[0][0], result[1][0], high_stride, low_stride, conf)
+    result_image = draw_result(np.array(image), annotations, cls_names)
+    cv2.imshow('image', result_image)
+    cv2.waitKey(0)
+    return annotations
+
+
+
+
+
+
+
+
+# test code
+if __name__ == '__main__':
+    from load_model import load_model
+
+    data_path = 'C:/Users/osy04/Desktop/wok_me/project/yolo/images/train2017/'
+    label_path = 'C:/Users/osy04/Desktop/wok_me/project/yolo/labels_bbox/train2017/'
+
+    file_img = os.listdir(data_path)
+    file_txt = os.listdir(label_path)
+
+    model = load_model("YOLO")
+    
+
+    # # method1: load model and predict
+    # for i in range(5):
+    #     image = cv2.imread(data_path + file_img[i])/255.0
+    #     result_annotation = predict(model, image)
+    #     result_image = draw_result(np.array(image), result_annotation)
+    #     cv2.imshow('image', result_image)
+    #     cv2.waitKey(0)
+
+
+    # coco dataset cls names
+    coco_cls_names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat',
+                    'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+                    'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+                    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+                    'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork',
+                    'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog',
+                    'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+                    'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+                    'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+    
+
+    # method2: load model and predict
+    for i in range(5):
+        image = cv2.imread(data_path + file_img[i])/255.0
+        result = predict_and_show(model, image, cls_names=coco_cls_names)
---
+++ packages/oneclickai/oneclickai-0.1.0-py3-none-any.whl/test/test_fit.py
@@ -0,0 +1,14 @@
+from oneclickai.YOLO import fit_yolo_model
+
+
+#% encode data
+# encode training data
+train_data_path = 'C:/Users/osy04/Desktop/image'
+train_label_path = 'C:/Users/osy04/Desktop/image'
+
+# # encode validation data
+val_data_path = 'C:/Users/osy04/Desktop/image'
+val_label_path = 'C:/Users/osy04/Desktop/image'
+
+# fit model
+fit_yolo_model(train_data_path, train_label_path, val_data_path, val_label_path, epochs=10)
