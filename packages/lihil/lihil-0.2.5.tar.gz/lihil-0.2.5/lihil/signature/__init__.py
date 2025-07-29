ed1cea860b4dc801080e9ead2e5c7e414b8f3b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:56 +0000

    Add package lihil/0.2.5/lihil-0.2.5.tar.gz

---
+++ packages/lihil/lihil-0.2.5.tar.gz/lihil-0.2.5/lihil/signature/__init__.py
@@ -0,0 +1,8 @@
+from lihil.interface.struct import CustomEncoder as CustomEncoder
+
+from .params import ParamMeta as ParamMeta
+from .params import Param as Param
+from .params import RequestParam as RequestParam
+from .parser import EndpointParser as EndpointParser
+from .signature import EndpointSignature as EndpointSignature
+from .signature import ParseResult as ParseResult
