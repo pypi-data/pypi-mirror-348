05e9ddde84fcea5d96d265773d9381095b32c36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:38 +0000

    Add package whiskerrag/0.0.61.dev20250516061310/whiskerrag-0.0.61.dev20250516061310-py3-none-any.whl

---
+++ packages/whiskerrag/whiskerrag-0.0.61.dev20250516061310-py3-none-any.whl/whiskerrag_utils/parser/base_image_parser.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from whiskerrag_types.interface.parser_interface import BaseParser, ParseResult
+from whiskerrag_types.model.knowledge import Knowledge
+from whiskerrag_types.model.multi_modal import Image
+from whiskerrag_utils.registry import RegisterTypeEnum, register
+
+
+@register(RegisterTypeEnum.Parser, "image")
+class BaseTextParser(BaseParser[Image]):
+
+    def parse(
+        self,
+        knowledge: Knowledge,
+        content: Image,
+    ) -> ParseResult:
+        return [content]
+
+    def batch_parse(
+        self,
+        knowledge: Knowledge,
+        content_list: List[Image],
+    ) -> List[ParseResult]:
+        return [self.parse(knowledge, content) for content in content_list]
