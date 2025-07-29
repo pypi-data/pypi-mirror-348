fca52946b861fc60941a3e0ba404b887c49c5546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:07:03 +0000

    Add package acl-anthology/0.5.2/acl_anthology-0.5.2.tar.gz

---
+++ packages/acl-anthology/acl_anthology-0.5.2.tar.gz/acl_anthology-0.5.2/acl_anthology/collections/types.py
@@ -0,0 +1,35 @@
+# Copyright 2023-2024 Marcel Bollmann <marcel@bollmann.me>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+
+
+class PaperDeletionType(Enum):
+    """Type of deletion of a paper."""
+
+    RETRACTED = "retracted"
+    """Paper was retracted.  A retraction occurs when serious, unrecoverable errors are discovered, which drastically affect the findings of the original work."""
+
+    REMOVED = "removed"
+    """Paper was removed.  A removal occurs in rare circumstances where serious ethical or legal issues arise, such as plagiarism."""
+
+
+class VolumeType(Enum):
+    """Type of publication a volume represents."""
+
+    JOURNAL = "journal"
+    """A journal issue."""
+
+    PROCEEDINGS = "proceedings"
+    """A conference/workshop proceedings volume."""
