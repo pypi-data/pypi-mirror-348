da03ec231fc2381f3728e7e9eed41b828e4bd7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:50 +0000

    Add package ankur/0.2/ankur-0.2-py3-none-any.whl

---
+++ packages/ankur/ankur-0.2-py3-none-any.whl/ankur/__init__.py
@@ -0,0 +1,10 @@
+# ankur/__init__.py
+
+def say_hello():
+    print("Hello from Ankur's custom package!")
+
+def show_links():
+    print("\n🔗 Connect with Ankur:")
+    print("GitHub:    https://github.com/7007259Ankur")
+    print("LinkedIn:  https://linkedin.com/in/yourusername")
+    print("LeetCode:  https://leetcode.com/yourusername/\n")
