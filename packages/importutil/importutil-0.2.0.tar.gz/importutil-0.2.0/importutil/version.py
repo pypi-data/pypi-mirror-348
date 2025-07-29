3c1dfdcde02b737d5da18495994eea5376815be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:38 +0000

    Add package importutil/0.2.0/importutil-0.2.0.tar.gz

---
+++ packages/importutil/importutil-0.2.0.tar.gz/importutil-0.2.0/importutil/version.py
@@ -0,0 +1,11 @@
+"""
+v0.1.0      20241128    1. first release
+v0.1.1      20241129    1. add import_file()
+v0.1.2      20250321    1. add get_sys_path() and add_sys_path()
+v0.1.5      20250322    1. add is_venv(), create_pth_file() and add_site_path()
+v0.1.6      20250508    1. add import_package()
+v0.2.0      20250516    1. modify import_package() input
+"""
+
+__version__ = "0.2.0"
+__repository__ = r"https://github.com/Hom-Wang/importutil.git"
