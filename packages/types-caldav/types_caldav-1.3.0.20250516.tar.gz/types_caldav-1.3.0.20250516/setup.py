1ada791c63edde9d41177d2e33974721327c6bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:57 +0000

    Add package types-caldav/1.3.0.20250516/types_caldav-1.3.0.20250516.tar.gz

---
+++ packages/types-caldav/types_caldav-1.3.0.20250516.tar.gz/types_caldav-1.3.0.20250516/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'caldav-stubs': ['__init__.pyi', 'davclient.pyi', 'elements/__init__.pyi', 'elements/base.pyi', 'elements/cdav.pyi', 'elements/dav.pyi', 'elements/ical.pyi', 'lib/__init__.pyi', 'lib/error.pyi', 'lib/namespace.pyi', 'lib/url.pyi', 'lib/vcal.pyi', 'objects.pyi', 'requests.pyi', 'METADATA.toml', 'py.typed']})
