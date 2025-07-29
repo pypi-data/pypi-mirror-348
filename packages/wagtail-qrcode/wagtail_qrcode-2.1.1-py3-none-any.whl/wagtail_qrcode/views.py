9c064b7f75137eab6faa8d74dadd99d3bc102eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:50 +0000

    Add package wagtail-qrcode/2.1.1/wagtail_qrcode-2.1.1-py3-none-any.whl

---
+++ packages/wagtail-qrcode/wagtail_qrcode-2.1.1-py3-none-any.whl/wagtail_qrcode/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from wagtail.models import Page
+
+
+def qr_code_page_view(request):
+    """QR code redirect view."""
+    page_id = int(request.GET.get("id"))
+
+    try:
+        specific_cls = Page.objects.get(id=page_id).specific_class
+        page = specific_cls.objects.get(id=page_id)
+    except Page.DoesNotExist:
+        return HttpResponseNotFound("Page not found")
+
+    if hasattr(page, "qr_code_usage"):
+        page.qr_code_usage += 1
+        page.save()
+        return HttpResponseRedirect(page.url)
+
+    return HttpResponseNotFound("Page not found")
