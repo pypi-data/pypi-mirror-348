646e630e4197edc2e04b4b6499f7df81dde98042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:54 +0000

    Add package grvt-pysdk/0.1.26/grvt_pysdk-0.1.26.tar.gz

---
+++ packages/grvt-pysdk/grvt_pysdk-0.1.26.tar.gz/grvt_pysdk-0.1.26/tests/pysdk/test_ccxt.py
@@ -0,0 +1,29 @@
+import ccxt
+
+from pysdk.grvt_ccxt_logging_selector import logger
+
+# from pysdk.grvt_ccxt_env import GrvtEnv
+# from pysdk.grvt_ccxt_pro import GrvtCcxtPro
+
+exchange = ccxt.binance(
+    {
+        "enableRateLimit": True,
+        "options": {
+            "defaultType": "spot",
+        },
+    }
+)
+exchange.load_markets()
+exchange_desc = exchange.describe()
+logger.info(f"Exchange Description: {list(exchange_desc)[0:20]}...")
+required_features = ["margin", "fetchBalance", "fetchLeverageTiers"]
+for feat in required_features:
+    try:
+        if not exchange.has[feat]:
+            logger.warning(f"Exchange {exchange.name} | does not support: {feat}")
+        else:
+            logger.info(f"Exchange {exchange.name} | supports: {feat}")
+    except KeyError:
+        logger.warning(f"Exchange {exchange.name} | does not support: {feat}")
+    except Exception as e:
+        logger.warning(f"Exchange {exchange.name} | does not support: {feat} | {e}")
