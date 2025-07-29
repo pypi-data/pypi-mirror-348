ef7ed45e9485789bd1e4db67131945cc631dd308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:08:18 +0000

    Add package libifcb/0.2/libifcb-0.2.tar.gz

---
+++ packages/libifcb/libifcb-0.2.tar.gz/libifcb-0.2/scripts/build-install-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+cd ..
+bash scripts/get-test-data.sh
+mkdir -p dist
+mkdir -p testout
+python3 -m build
+pip install --ignore-installed --no-deps dist/libifcb*.whl
+python3 scripts/test.py
+# python3 -m twin upload --repository testpypi dist/*
