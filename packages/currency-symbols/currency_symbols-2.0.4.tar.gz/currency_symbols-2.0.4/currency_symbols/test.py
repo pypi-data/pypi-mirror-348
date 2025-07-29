11c38096fa9273150489b4572d33f2ecc7e5a239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:46 +0000

    Add package currency-symbols/2.0.4/currency_symbols-2.0.4.tar.gz

---
+++ packages/currency-symbols/currency_symbols-2.0.4.tar.gz/currency_symbols-2.0.4/currency_symbols/test.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from . import CurrencySymbols
+
+
+class TestSum(unittest.TestCase):
+    def test_currency_symbols(self) -> None:
+        self.assertEqual(
+            CurrencySymbols.get_symbol("USD"),
+            "$",
+            "Should return dollars symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("nzd"),
+            "$",
+            "Should return dollars symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("INR"),
+            "₹",
+            "Should return indian ruppees symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("CNY"),
+            "¥",
+            "Should return CNY symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("KZT"),
+            "₸",
+            "Should return KZT symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("CLF"),
+            "UF",
+            "Should return CLF symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("CNH"),
+            "¥",
+            "Should return CNH symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("DEM"),
+            "DM",
+            "Should return DEM symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("FRF"),
+            "₣",
+            "Should return FRF symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("SLE"),
+            "Le",
+            "Should return SLE symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("VES"),
+            "Bs.",
+            "Should return VES symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("XAG"),
+            "Ag",
+            "Should return XAG symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("XAU"),
+            "Au",
+            "Should return XAU symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("XDR"),
+            "SDR",
+            "Should return XDR symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("ZMK"),
+            "ZK",
+            "Should return ZMK symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("ZMW"),
+            "ZK",
+            "Should return ZMW symbol"
+        )
+        self.assertEqual(
+            CurrencySymbols.get_symbol("ZWL"),
+            "Z$",
+            "Should return ZWL symbol"
+        )
