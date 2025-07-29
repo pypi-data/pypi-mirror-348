b146ebd2733b48e7e0641102392cbac90baae4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:09:43 +0000

    Add package scorecard-ai/2.0.0a4/scorecard_ai-2.0.0a4.tar.gz

---
+++ packages/scorecard-ai/scorecard_ai-2.0.0a4.tar.gz/scorecard_ai-2.0.0a4/src/scorecard_ai/resources/runs.py
@@ -0,0 +1,305 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List
+from typing_extensions import Literal
+
+import httpx
+
+from ..types import run_create_params, run_update_params
+from .._types import NOT_GIVEN, Body, Query, Headers, NotGiven
+from .._utils import maybe_transform, async_maybe_transform
+from .._compat import cached_property
+from .._resource import SyncAPIResource, AsyncAPIResource
+from .._response import (
+    to_raw_response_wrapper,
+    to_streamed_response_wrapper,
+    async_to_raw_response_wrapper,
+    async_to_streamed_response_wrapper,
+)
+from ..types.run import Run
+from .._base_client import make_request_options
+from ..types.run_update_response import RunUpdateResponse
+
+__all__ = ["RunsResource", "AsyncRunsResource"]
+
+
+class RunsResource(SyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> RunsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/scorecard-ai/scorecard-python#accessing-raw-response-data-eg-headers
+        """
+        return RunsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> RunsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/scorecard-ai/scorecard-python#with_streaming_response
+        """
+        return RunsResourceWithStreamingResponse(self)
+
+    def create(
+        self,
+        project_id: str,
+        *,
+        metric_ids: List[str],
+        testset_id: str,
+        system_config_id: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Run:
+        """
+        Create a new Run.
+
+        Args:
+          metric_ids: The IDs of the metrics this Run is using.
+
+          testset_id: The ID of the Testset this Run is testing.
+
+          system_config_id: The ID of the system configuration this Run is using.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        if not project_id:
+            raise ValueError(f"Expected a non-empty value for `project_id` but received {project_id!r}")
+        return self._post(
+            f"/projects/{project_id}/runs",
+            body=maybe_transform(
+                {
+                    "metric_ids": metric_ids,
+                    "testset_id": testset_id,
+                    "system_config_id": system_config_id,
+                },
+                run_create_params.RunCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Run,
+        )
+
+    def update(
+        self,
+        run_id: str,
+        *,
+        status: Literal[
+            "pending",
+            "awaiting_execution",
+            "running_execution",
+            "awaiting_scoring",
+            "running_scoring",
+            "awaiting_human_scoring",
+            "completed",
+        ],
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> RunUpdateResponse:
+        """
+        Update the status of a Run.
+
+        Args:
+          status: The status of the Run.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        if not run_id:
+            raise ValueError(f"Expected a non-empty value for `run_id` but received {run_id!r}")
+        return self._patch(
+            f"/runs/{run_id}",
+            body=maybe_transform({"status": status}, run_update_params.RunUpdateParams),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=RunUpdateResponse,
+        )
+
+
+class AsyncRunsResource(AsyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AsyncRunsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/scorecard-ai/scorecard-python#accessing-raw-response-data-eg-headers
+        """
+        return AsyncRunsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AsyncRunsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/scorecard-ai/scorecard-python#with_streaming_response
+        """
+        return AsyncRunsResourceWithStreamingResponse(self)
+
+    async def create(
+        self,
+        project_id: str,
+        *,
+        metric_ids: List[str],
+        testset_id: str,
+        system_config_id: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Run:
+        """
+        Create a new Run.
+
+        Args:
+          metric_ids: The IDs of the metrics this Run is using.
+
+          testset_id: The ID of the Testset this Run is testing.
+
+          system_config_id: The ID of the system configuration this Run is using.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        if not project_id:
+            raise ValueError(f"Expected a non-empty value for `project_id` but received {project_id!r}")
+        return await self._post(
+            f"/projects/{project_id}/runs",
+            body=await async_maybe_transform(
+                {
+                    "metric_ids": metric_ids,
+                    "testset_id": testset_id,
+                    "system_config_id": system_config_id,
+                },
+                run_create_params.RunCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Run,
+        )
+
+    async def update(
+        self,
+        run_id: str,
+        *,
+        status: Literal[
+            "pending",
+            "awaiting_execution",
+            "running_execution",
+            "awaiting_scoring",
+            "running_scoring",
+            "awaiting_human_scoring",
+            "completed",
+        ],
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> RunUpdateResponse:
+        """
+        Update the status of a Run.
+
+        Args:
+          status: The status of the Run.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        if not run_id:
+            raise ValueError(f"Expected a non-empty value for `run_id` but received {run_id!r}")
+        return await self._patch(
+            f"/runs/{run_id}",
+            body=await async_maybe_transform({"status": status}, run_update_params.RunUpdateParams),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=RunUpdateResponse,
+        )
+
+
+class RunsResourceWithRawResponse:
+    def __init__(self, runs: RunsResource) -> None:
+        self._runs = runs
+
+        self.create = to_raw_response_wrapper(
+            runs.create,
+        )
+        self.update = to_raw_response_wrapper(
+            runs.update,
+        )
+
+
+class AsyncRunsResourceWithRawResponse:
+    def __init__(self, runs: AsyncRunsResource) -> None:
+        self._runs = runs
+
+        self.create = async_to_raw_response_wrapper(
+            runs.create,
+        )
+        self.update = async_to_raw_response_wrapper(
+            runs.update,
+        )
+
+
+class RunsResourceWithStreamingResponse:
+    def __init__(self, runs: RunsResource) -> None:
+        self._runs = runs
+
+        self.create = to_streamed_response_wrapper(
+            runs.create,
+        )
+        self.update = to_streamed_response_wrapper(
+            runs.update,
+        )
+
+
+class AsyncRunsResourceWithStreamingResponse:
+    def __init__(self, runs: AsyncRunsResource) -> None:
+        self._runs = runs
+
+        self.create = async_to_streamed_response_wrapper(
+            runs.create,
+        )
+        self.update = async_to_streamed_response_wrapper(
+            runs.update,
+        )
---
+++ packages/scorecard-ai/scorecard_ai-2.0.0a4.tar.gz/scorecard_ai-2.0.0a4/tests/api_resources/test_runs.py
@@ -0,0 +1,218 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import os
+from typing import Any, cast
+
+import pytest
+
+from tests.utils import assert_matches_type
+from scorecard_ai import Scorecard, AsyncScorecard
+from scorecard_ai.types import Run, RunUpdateResponse
+
+base_url = os.environ.get("TEST_API_BASE_URL", "http://127.0.0.1:4010")
+
+
+class TestRuns:
+    parametrize = pytest.mark.parametrize("client", [False, True], indirect=True, ids=["loose", "strict"])
+
+    @parametrize
+    def test_method_create(self, client: Scorecard) -> None:
+        run = client.runs.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+        )
+        assert_matches_type(Run, run, path=["response"])
+
+    @parametrize
+    def test_method_create_with_all_params(self, client: Scorecard) -> None:
+        run = client.runs.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+            system_config_id="87654321-4d3b-4ae4-8c7a-4b6e2a19ccf0",
+        )
+        assert_matches_type(Run, run, path=["response"])
+
+    @parametrize
+    def test_raw_response_create(self, client: Scorecard) -> None:
+        response = client.runs.with_raw_response.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        run = response.parse()
+        assert_matches_type(Run, run, path=["response"])
+
+    @parametrize
+    def test_streaming_response_create(self, client: Scorecard) -> None:
+        with client.runs.with_streaming_response.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            run = response.parse()
+            assert_matches_type(Run, run, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    def test_path_params_create(self, client: Scorecard) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `project_id` but received ''"):
+            client.runs.with_raw_response.create(
+                project_id="",
+                metric_ids=["789", "101"],
+                testset_id="246",
+            )
+
+    @parametrize
+    def test_method_update(self, client: Scorecard) -> None:
+        run = client.runs.update(
+            run_id="135",
+            status="awaiting_scoring",
+        )
+        assert_matches_type(RunUpdateResponse, run, path=["response"])
+
+    @parametrize
+    def test_raw_response_update(self, client: Scorecard) -> None:
+        response = client.runs.with_raw_response.update(
+            run_id="135",
+            status="awaiting_scoring",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        run = response.parse()
+        assert_matches_type(RunUpdateResponse, run, path=["response"])
+
+    @parametrize
+    def test_streaming_response_update(self, client: Scorecard) -> None:
+        with client.runs.with_streaming_response.update(
+            run_id="135",
+            status="awaiting_scoring",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            run = response.parse()
+            assert_matches_type(RunUpdateResponse, run, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    def test_path_params_update(self, client: Scorecard) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `run_id` but received ''"):
+            client.runs.with_raw_response.update(
+                run_id="",
+                status="awaiting_scoring",
+            )
+
+
+class TestAsyncRuns:
+    parametrize = pytest.mark.parametrize("async_client", [False, True], indirect=True, ids=["loose", "strict"])
+
+    @parametrize
+    async def test_method_create(self, async_client: AsyncScorecard) -> None:
+        run = await async_client.runs.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+        )
+        assert_matches_type(Run, run, path=["response"])
+
+    @parametrize
+    async def test_method_create_with_all_params(self, async_client: AsyncScorecard) -> None:
+        run = await async_client.runs.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+            system_config_id="87654321-4d3b-4ae4-8c7a-4b6e2a19ccf0",
+        )
+        assert_matches_type(Run, run, path=["response"])
+
+    @parametrize
+    async def test_raw_response_create(self, async_client: AsyncScorecard) -> None:
+        response = await async_client.runs.with_raw_response.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        run = await response.parse()
+        assert_matches_type(Run, run, path=["response"])
+
+    @parametrize
+    async def test_streaming_response_create(self, async_client: AsyncScorecard) -> None:
+        async with async_client.runs.with_streaming_response.create(
+            project_id="314",
+            metric_ids=["789", "101"],
+            testset_id="246",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            run = await response.parse()
+            assert_matches_type(Run, run, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    async def test_path_params_create(self, async_client: AsyncScorecard) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `project_id` but received ''"):
+            await async_client.runs.with_raw_response.create(
+                project_id="",
+                metric_ids=["789", "101"],
+                testset_id="246",
+            )
+
+    @parametrize
+    async def test_method_update(self, async_client: AsyncScorecard) -> None:
+        run = await async_client.runs.update(
+            run_id="135",
+            status="awaiting_scoring",
+        )
+        assert_matches_type(RunUpdateResponse, run, path=["response"])
+
+    @parametrize
+    async def test_raw_response_update(self, async_client: AsyncScorecard) -> None:
+        response = await async_client.runs.with_raw_response.update(
+            run_id="135",
+            status="awaiting_scoring",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        run = await response.parse()
+        assert_matches_type(RunUpdateResponse, run, path=["response"])
+
+    @parametrize
+    async def test_streaming_response_update(self, async_client: AsyncScorecard) -> None:
+        async with async_client.runs.with_streaming_response.update(
+            run_id="135",
+            status="awaiting_scoring",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            run = await response.parse()
+            assert_matches_type(RunUpdateResponse, run, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @parametrize
+    async def test_path_params_update(self, async_client: AsyncScorecard) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `run_id` but received ''"):
+            await async_client.runs.with_raw_response.update(
+                run_id="",
+                status="awaiting_scoring",
+            )
