2efa6426c6f7a33be68bb8118cdb48736455c5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:25 +0000

    Add package jumpstarter-driver-raspberrypi/0.6.0/jumpstarter_driver_raspberrypi-0.6.0.tar.gz

---
+++ packages/jumpstarter-driver-raspberrypi/jumpstarter_driver_raspberrypi-0.6.0.tar.gz/jumpstarter_driver_raspberrypi-0.6.0/README.md
@@ -0,0 +1,26 @@
+# Raspberry Pi driver
+
+`jumpstarter-driver-raspberrypi` provides functionality for interacting with
+Raspberry Pi devices.
+
+## Installation
+
+```shell
+pip3 install --extra-index-url https://pkg.jumpstarter.dev/simple/ jumpstarter-driver-raspberrypi
+```
+
+## Configuration
+
+Example configuration:
+
+```yaml
+export:
+  raspberrypi:
+    type: jumpstarter_driver_raspberrypi.driver.DigitalOutput
+    config:
+      # Add required config parameters here
+```
+
+## API Reference
+
+Add API documentation here.
---
+++ packages/jumpstarter-driver-raspberrypi/jumpstarter_driver_raspberrypi-0.6.0.tar.gz/jumpstarter_driver_raspberrypi-0.6.0/jumpstarter_driver_raspberrypi/client.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from jumpstarter.client import DriverClient
+
+
+@dataclass(kw_only=True)
+class DigitalOutputClient(DriverClient):
+    def off(self) -> None:
+        self.call("off")
+
+    def on(self) -> None:
+        self.call("on")
+
+
+@dataclass(kw_only=True)
+class DigitalInputClient(DriverClient):
+    def wait_for_active(self, timeout: float | None = None):
+        self.call("wait_for_active", timeout)
+
+    def wait_for_inactive(self, timeout: float | None = None):
+        self.call("wait_for_inactive", timeout)
