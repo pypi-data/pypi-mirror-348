16174cee7ef442e4c977517e73248dca6d3543dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:31 +0000

    Add package coloraiz/1.0.3/coloraiz-1.0.3.tar.gz

---
+++ packages/coloraiz/coloraiz-1.0.3.tar.gz/coloraiz-1.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coloraiz",
+    version="1.0.3",
+    author="Morty",
+    author_email="me@mortysmith.org",
+    description="Simple way to add color to your terminal text output",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/morrrty/coloraiz",
+    project_urls={
+        "Bug Tracker": "https://github.com/morrrty/coloraiz",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "colorama",
+        "requests"
+    ]
+)
