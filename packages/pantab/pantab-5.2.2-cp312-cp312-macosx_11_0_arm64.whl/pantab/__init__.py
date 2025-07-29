c766c32a41ce3233e996aeb390266f365af12e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:13 +0000

    Add package pantab/5.2.2/pantab-5.2.2-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/pantab/pantab-5.2.2-cp312-cp312-macosx_11_0_arm64.whl/pantab/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "5.2.2"
+
+
+from pantab._reader import frame_from_hyper, frame_from_hyper_query, frames_from_hyper
+from pantab._writer import frame_to_hyper, frames_to_hyper
+
+__all__ = [
+    "__version__",
+    "frame_from_hyper",
+    "frame_from_hyper_query",
+    "frames_from_hyper",
+    "frame_to_hyper",
+    "frames_to_hyper",
+]
