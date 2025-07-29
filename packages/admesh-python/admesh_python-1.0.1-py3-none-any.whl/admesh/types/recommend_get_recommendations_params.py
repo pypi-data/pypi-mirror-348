495bd2f5b4b7febef74406357bb5af821748cc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:43 +0000

    Add package admesh-python/1.0.1/admesh_python-1.0.1-py3-none-any.whl

---
+++ packages/admesh-python/admesh_python-1.0.1-py3-none-any.whl/admesh/types/recommend_get_recommendations_params.py
@@ -0,0 +1,28 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["RecommendGetRecommendationsParams"]
+
+
+class RecommendGetRecommendationsParams(TypedDict, total=False):
+    agent_id: Required[str]
+
+    query: Required[str]
+
+    followup_suggestions: Optional[str]
+
+    intent_summary: Optional[str]
+
+    model: Optional[str]
+
+    previous_query: Optional[str]
+
+    session_id: Optional[str]
+
+    summary: Optional[str]
+
+    user_id: Optional[str]
