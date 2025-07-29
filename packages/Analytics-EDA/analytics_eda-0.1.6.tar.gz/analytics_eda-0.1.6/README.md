0e639cc1390fa2d2908df455f0a434363066e164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:14 +0000

    Add package Analytics-EDA/0.1.6/analytics_eda-0.1.6.tar.gz

---
+++ packages/Analytics-EDA/analytics_eda-0.1.6.tar.gz/analytics_eda-0.1.6/README.md
@@ -0,0 +1,7 @@
+# analytics-eda
+
+Analytics Exploratory Data Analysis python package
+
+## License
+
+This project is open source under the [Apache License 2.0](LICENSE).
---
+++ packages/Analytics-EDA/analytics_eda-0.1.6.tar.gz/analytics_eda-0.1.6/src/analytics_eda/core/__init__.py
@@ -0,0 +1,6 @@
+from .numeric import alternative_fit_assessment, assess_and_transform, descriptive_statistics, distribution_analysis, distribution_visualizations, inferential_analysis, normality_assessment, outlier_analysis, validate_numeric_named_series
+from .categorical import validate_categorical_named_series
+from .reporting import write_json_report
+from .explore_data import explore_data
+from .missing_data_analysis import missing_data_analysis
+from .clean_series import clean_series
