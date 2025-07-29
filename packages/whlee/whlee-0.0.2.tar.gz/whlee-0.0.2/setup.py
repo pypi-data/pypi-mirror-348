c1819f2492832cbeb8076403d2c6a42ddd93f71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:00 +0000

    Add package whlee/0.0.2/whlee-0.0.2.tar.gz

---
+++ packages/whlee/whlee-0.0.2.tar.gz/whlee-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "whlee",
+    version = "0.0.2",
+    description="toy tools update at 20250516",
+    author = "whl",
+    author_email = "2631@139.com",
+    license="MIT",
+    packages = find_packages(),
+    python_requires=">=3.8",
+    install_requires=['numpy==1.24.3', 'pandas>=1.3.5', 'addict']
+)
