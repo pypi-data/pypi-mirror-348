b92ed8270a98b7b96df618185871f3eff6258a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:04 +0000

    Add package lucidicai/1.1.22/lucidicai-1.1.22-py3-none-any.whl

---
+++ packages/lucidicai/lucidicai-1.1.22-py3-none-any.whl/lucidicai/singleton.py
@@ -0,0 +1,17 @@
+from .errors import LucidicNotInitializedError
+
+lai_inst = {}
+
+def singleton(class_):
+    def getinstance(*args, **kwargs):
+        if class_ not in lai_inst:
+            if class_.__name__ == 'Client' and ('lucidic_api_key' not in kwargs or 'agent_id' not in kwargs):
+                raise LucidicNotInitializedError()
+            lai_inst[class_] = class_(*args, **kwargs)
+        return lai_inst[class_]
+
+    return getinstance
+
+def clear_singletons():
+    global lai_inst
+    lai_inst = {}
