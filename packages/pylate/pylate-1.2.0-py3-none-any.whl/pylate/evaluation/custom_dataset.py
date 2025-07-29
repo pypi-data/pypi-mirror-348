23c4b34a3786bd3a4c005a8a9bf0c31661e5a330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:34 +0000

    Add package pylate/1.2.0/pylate-1.2.0-py3-none-any.whl

---
+++ packages/pylate/pylate-1.2.0-py3-none-any.whl/pylate/evaluation/custom_dataset.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+
+def load_custom_dataset(path: str, split: str = "test") -> tuple[list, list, dict]:
+    """Load a custom dataset.
+
+    Parameters
+    ----------
+    path
+        Path of the dataset.
+    split
+        Split to load.
+
+    Examples
+    --------
+    """
+    from beir.datasets.data_loader import GenericDataLoader
+
+    documents, queries, qrels = GenericDataLoader(path).load(split=split)
+
+    documents = [
+        {
+            "id": document_id,
+            "text": f"{document['title']} {document['text']}".strip()
+            if "title" in document
+            else document["text"].strip(),
+        }
+        for document_id, document in documents.items()
+    ]
+
+    qrels = {
+        queries[query_id]: query_documents
+        for query_id, query_documents in qrels.items()
+    }
+
+    return documents, list(qrels.keys()), qrels
