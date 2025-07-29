b5f74722c899effbac7e5dffd3cbdd3634dacbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package aimhub-license/0.5.0/aimhub_license-0.5.0-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/aimhub-license/aimhub_license-0.5.0-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl/aimhub_license/__init__.py
@@ -0,0 +1,8 @@
+from .utils import (
+    validate_license_expiration_date,
+    validate_license_seats,
+    get_license_expiration_date,
+    get_license_seats,
+    read_license_key,
+    set_license_key
+)
