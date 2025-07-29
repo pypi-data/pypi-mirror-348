8915b08ec9806e00dd1db3b4e0c40e16ef8a2c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:59 +0000

    Add package DecoratedText/1.0/decoratedtext-1.0.tar.gz

---
+++ packages/DecoratedText/decoratedtext-1.0.tar.gz/decoratedtext-1.0/README.md
@@ -0,0 +1,3 @@
+# DecoratedText
+
+It's a great library for text design!
---
+++ packages/DecoratedText/decoratedtext-1.0.tar.gz/decoratedtext-1.0/decoratedtext/main.py
@@ -0,0 +1,6 @@
+def sign(text):
+    textsize = len(text)
+    print(" ___" + "_"*textsize + "___ ")
+    print("/   " + " "*textsize + "   \\")
+    print("|   " + text + "   |")
+    print("\\___" + "_"*textsize + "___/")
