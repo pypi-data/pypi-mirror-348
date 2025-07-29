4388039957d1ba772050b81269f206981665832c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:20 +0000

    Add package quickpool/2.1.0/quickpool-2.1.0.tar.gz

---
+++ packages/quickpool/quickpool-2.1.0.tar.gz/quickpool-2.1.0/src/quickpool/__init__.py
@@ -0,0 +1,4 @@
+from .quickpool import ProcessPool, ThreadPool, for_each, to_args_list, update_and_wait
+
+__version__ = "2.1.0"
+__all__ = ["ProcessPool", "ThreadPool", "update_and_wait", "for_each", "to_args_list"]
