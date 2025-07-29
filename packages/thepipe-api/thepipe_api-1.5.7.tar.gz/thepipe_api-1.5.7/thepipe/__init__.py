8129b31fa4a1b7e87241eb5350cd36a842802039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:24 +0000

    Add package thepipe-api/1.5.7/thepipe_api-1.5.7.tar.gz

---
+++ packages/thepipe-api/thepipe_api-1.5.7.tar.gz/thepipe_api-1.5.7/thepipe/__init__.py
@@ -0,0 +1,32 @@
+import os
+from .scraper import scrape_file, scrape_url, scrape_directory
+from .core import parse_arguments, save_outputs
+
+
+def main() -> None:
+    args = parse_arguments()
+    chunks = None
+    if args.source.startswith("http"):
+        chunks = scrape_url(
+            args.source,
+            ai_extraction=args.ai_extraction,
+            verbose=args.verbose,
+        )
+    elif os.path.isdir(args.source):
+        chunks = scrape_directory(
+            args.source,
+            inclusion_pattern=args.inclusion_pattern,
+            ai_extraction=args.ai_extraction,
+            verbose=args.verbose,
+        )
+    else:
+        chunks = scrape_file(
+            args.source,
+            ai_extraction=args.ai_extraction,
+            verbose=args.verbose,
+        )
+    save_outputs(chunks=chunks, verbose=args.verbose, text_only=args.text_only)
+
+
+if __name__ == "__main__":
+    main()
