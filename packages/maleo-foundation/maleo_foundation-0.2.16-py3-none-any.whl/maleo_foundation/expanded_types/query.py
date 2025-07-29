ca4c56e25f39ef36c1dff9e7c7f772377c965aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:37 +0000

    Add package maleo-foundation/0.2.16/maleo_foundation-0.2.16-py3-none-any.whl

---
+++ packages/maleo-foundation/maleo_foundation-0.2.16-py3-none-any.whl/maleo_foundation/expanded_types/query.py
@@ -0,0 +1,65 @@
+from typing import Awaitable, Callable, Union
+from maleo_foundation.models.transfers.parameters.general import BaseGeneralParametersTransfers
+from maleo_foundation.models.transfers.parameters.service import BaseServiceParametersTransfers
+from maleo_foundation.models.transfers.results.service.query import BaseServiceQueryResultsTransfers
+
+class ExpandedQueryTypes:
+    #* Unpaginated multiple data
+    GetUnpaginatedMultipleParameter = BaseServiceParametersTransfers.GetUnpaginatedMultiple
+    GetUnpaginatedMultipleResult = Union[
+        BaseServiceQueryResultsTransfers.Fail,
+        BaseServiceQueryResultsTransfers.NoData,
+        BaseServiceQueryResultsTransfers.UnpaginatedMultipleData
+    ]
+    SyncGetUnpaginatedMultipleFunction = Callable[
+        [GetUnpaginatedMultipleParameter],
+        GetUnpaginatedMultipleResult
+    ]
+    AsyncGetUnpaginatedMultipleFunction = Callable[
+        [GetUnpaginatedMultipleParameter],
+        Awaitable[GetUnpaginatedMultipleResult]
+    ]
+
+    #* Paginated multiple data
+    GetPaginatedMultipleParameter = BaseServiceParametersTransfers.GetPaginatedMultiple
+    GetPaginatedMultipleResult = Union[
+        BaseServiceQueryResultsTransfers.Fail,
+        BaseServiceQueryResultsTransfers.NoData,
+        BaseServiceQueryResultsTransfers.PaginatedMultipleData
+    ]
+    SyncGetPaginatedMultipleFunction = Callable[
+        [GetPaginatedMultipleParameter],
+        GetPaginatedMultipleResult
+    ]
+    AsyncGetPaginatedMultipleFunction = Callable[
+        [GetPaginatedMultipleParameter],
+        Awaitable[GetPaginatedMultipleResult]
+    ]
+
+    #* Single data
+    GetSingleParameter = BaseGeneralParametersTransfers.GetSingle
+    GetSingleResult = Union[
+        BaseServiceQueryResultsTransfers.Fail,
+        BaseServiceQueryResultsTransfers.NoData,
+        BaseServiceQueryResultsTransfers.SingleData
+    ]
+    SyncGetSingleFunction = Callable[
+        [GetSingleParameter],
+        GetSingleResult
+    ]
+    AsyncGetSingleFunction = Callable[
+        [GetSingleParameter],
+        Awaitable[GetSingleResult]
+    ]
+
+    #* Create or Update
+    CreateOrUpdateResult = Union[
+        BaseServiceQueryResultsTransfers.Fail,
+        BaseServiceQueryResultsTransfers.SingleData
+    ]
+
+    #* Status update
+    StatusUpdateResult = Union[
+        BaseServiceQueryResultsTransfers.Fail,
+        BaseServiceQueryResultsTransfers.SingleData
+    ]
