9aad7ee686a331fc56df5b38cd3e83e8a16cf6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:33 +0000

    Add package vizro/0.1.39/vizro-0.1.39-py3-none-any.whl

---
+++ packages/vizro/vizro-0.1.39-py3-none-any.whl/vizro/models/_components/form/_text_area.py
@@ -0,0 +1,58 @@
+from typing import Literal
+
+import dash_bootstrap_components as dbc
+from dash import html
+from pydantic import Field
+
+from vizro.models import VizroBaseModel
+from vizro.models._action._actions_chain import _action_validator_factory
+from vizro.models._models_utils import _log_call
+from vizro.models.types import ActionType, _IdProperty
+
+
+class TextArea(VizroBaseModel):
+    """Multi-line text input component `TextArea`.
+
+    Based on the underlying [`dcc.TextArea`](https://dash.plotly.com/dash-core-components/textarea).
+
+    Args:
+        type (Literal["text_area"]): Defaults to `"text_area"`.
+        title (str): Title to be displayed. Defaults to `""`.
+        placeholder (str): Default text to display in input field. Defaults to `""`.
+        actions (Optional[list[ActionType]]): Defaults to `[]`.
+
+    """
+
+    type: Literal["text_area"] = "text_area"
+    # TODO: before making public consider naming this field (or giving an alias) label instead of title
+    title: str = Field(default="", description="Title to be displayed")
+    placeholder: str = Field(default="", description="Default text to display in input field")
+    actions: list[ActionType] = []
+
+    # Reused validators
+    # TODO: Before making public, consider how actions should be triggered and what the default property should be
+    # See comment thread: https://github.com/mckinsey/vizro/pull/298#discussion_r1478137654
+    _set_actions = _action_validator_factory("value")
+
+    @property
+    def _action_outputs(self) -> dict[str, _IdProperty]:
+        return {"__default__": f"{self.id}.value"}
+
+    @property
+    def _action_inputs(self) -> dict[str, _IdProperty]:
+        return {"__default__": f"{self.id}.value"}
+
+    @_log_call
+    def build(self):
+        return html.Div(
+            children=[
+                dbc.Label(self.title, html_for=self.id) if self.title else None,
+                dbc.Textarea(
+                    id=self.id,
+                    placeholder=self.placeholder,
+                    persistence=True,
+                    persistence_type="session",
+                    debounce=True,
+                ),
+            ],
+        )
