5d590871cbc43e44b06f8134643f200cffaf52d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:57 +0000

    Add package mkdocs-document-dates/2.3.5/mkdocs_document_dates-2.3.5.tar.gz

---
+++ packages/mkdocs-document-dates/mkdocs_document_dates-2.3.5.tar.gz/mkdocs_document_dates-2.3.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import mkdocs_document_dates
+
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "A MkDocs plugin for displaying accurate document creation and last modification dates."
+
+VERSION = '2.3.5'
+
+setup(
+    name="mkdocs-document-dates",
+    version=VERSION,
+    author="Aaron Wang",
+    description="A MkDocs plugin for displaying accurate document creation and last modification dates.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaywhj/mkdocs-document-dates",
+    packages=find_packages(),
+    install_requires=[
+        'mkdocs>=1.0.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'mkdocs.plugins': [
+            'document-dates = mkdocs_document_dates.plugin:DocumentDatesPlugin',
+        ],
+        'console_scripts': [
+            # 提供手动执行 hooks 安装的入口
+            'mkdocs-document-dates-hooks=mkdocs_document_dates.hooks_installer:install'
+        ],
+    },
+    package_data={
+        'mkdocs_document_dates': ['hooks/*'],
+    },
+    python_requires=">=3.7",
+)
