ea23a0c385fede40d04a8ca51f80c09e2e32e99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:50 +0000

    Add package freeai-utils/0.2.7/freeai_utils-0.2.7-py3-none-any.whl

---
+++ packages/freeai-utils/freeai_utils-0.2.7-py3-none-any.whl/freeai_utils/__init__.py
@@ -0,0 +1,25 @@
+from .audio_record import WavRecorder, MP3Recorder, check_wav_length_and_size, check_mp3_length_and_size
+from .audio_to_text_whisper import OpenAIWhisper
+from .clean_text_for_tts import clean_ai_text_for_tts
+from .geminiAPI import GeminiClient
+from .google_search import GoogleSearcher
+from .image_to_text import ImageCaptioner
+from .text_from_image_easyocr import Text_Extractor_EasyOCR
+from .text_to_speech_gtts import gtts_print_supported_languages, gtts_speak
+from .text_to_speech_pyttsx3 import Text_To_Speech_Pyttsx3
+from .pdf_docx_reader import PDF_DOCX_Reader
+from .document_filter import DocumentFilter
+
+__all__ = [
+    "WavRecorder", "MP3Recorder","check_wav_length_and_size","check_mp3_length_and_size"
+    "OpenAIWhisper",
+    "clean_ai_text_for_tts",
+    "GeminiClient",
+    "GoogleSearcher",
+    "ImageCaptioner",
+    "Text_Extractor_EasyOCR",
+    "gtts_print_supported_languages", "gtts_speak",
+    "Text_To_Speech_Pyttsx3",
+    "PDF_DOCX_Reader",
+    "DocumentFilter"
+]
