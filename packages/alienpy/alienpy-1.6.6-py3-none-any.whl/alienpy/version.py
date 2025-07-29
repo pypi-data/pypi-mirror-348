fddbc8a6a607d4d2c64ad872008ffd74d90bf79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:29 +0000

    Add package alienpy/1.6.6/alienpy-1.6.6-py3-none-any.whl

---
+++ packages/alienpy/alienpy-1.6.6-py3-none-any.whl/alienpy/version.py
@@ -0,0 +1,11 @@
+"""alienpy:: Definitions of version strings"""
+
+import sys
+
+ALIENPY_VERSION_HASH = 'a961658'
+ALIENPY_VERSION_DATE = '20250515_141447'
+ALIENPY_VERSION_STR = '1.6.6'
+
+if __name__ == '__main__':
+    print('This file should not be executed!', file = sys.stderr, flush = True)
+    sys.exit(95)
