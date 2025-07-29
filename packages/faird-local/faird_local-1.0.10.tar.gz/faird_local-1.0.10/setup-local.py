3047a8e40af6d381665c76d7e09364170e09fe32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:59 +0000

    Add package faird-local/1.0.10/faird_local-1.0.10.tar.gz

---
+++ packages/faird-local/faird_local-1.0.10.tar.gz/faird_local-1.0.10/setup-local.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="faird-local",
+    version="1.0.10",
+    description="A local SDK for working with DataFrame",
+    author="rdcn",
+    author_email="rdcn@cnic.com",
+    packages=find_packages(include=["core*", "parser*", "local_sdk*"]),  # 自动发现包含的所有包
+    install_requires=[
+        "pyarrow==19.0.0",  # 指定依赖的 pyarrow 版本
+        "tabulate",
+        "pandas",
+        "netCDF4",
+        "rasterio",
+    ],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            # 可选：定义命令行工具
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
