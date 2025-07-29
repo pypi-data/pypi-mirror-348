8b45e0163777c700ce965cb8b0bbc2b197a14b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:11 +0000

    Add package stx-security-filter/0.1.0/stx_security_filter-0.1.0.tar.gz

---
+++ packages/stx-security-filter/stx_security_filter-0.1.0.tar.gz/stx_security_filter-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stx-security-filter',
+    version='0.1.0',
+    description='Simple Python utility to detect malicious GET input',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
