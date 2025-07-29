664f62a1efef5b3c7d908d125c109d74e6799ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:51 +0000

    Add package ankur/0.1/ankur-0.1.tar.gz

---
+++ packages/ankur/ankur-0.1.tar.gz/ankur-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ankur",
+    version="0.1",
+    packages=find_packages(),
+    description="A demo package by Ankur with social links!",
+    author="Ankur",
+    author_email="ankurkumar7753@gmail.com",
+    url="https://github.com/7007259Ankur",  # Main project URL
+    project_urls={
+        "GitHub": "https://github.com/yourusername",
+        "LinkedIn": "https://linkedin.com/in/yourusername",
+        "LeetCode": "https://leetcode.com/yourusername/",
+    },
+    install_requires=[],  # Add dependencies if needed
+)
