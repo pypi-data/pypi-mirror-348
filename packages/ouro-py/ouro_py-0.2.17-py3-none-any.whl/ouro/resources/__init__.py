55d264736f6fa1b55a1c5e4d5a189cc3d5260c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:57 +0000

    Add package ouro-py/0.2.17/ouro_py-0.2.17-py3-none-any.whl

---
+++ packages/ouro-py/ouro_py-0.2.17-py3-none-any.whl/ouro/resources/__init__.py
@@ -0,0 +1,41 @@
+# This file is used to import all the resources in the ouro package
+
+from .assets import Assets
+from .content import Content, Editor
+from .conversations import Conversations
+from .datasets import Datasets
+from .files import Files
+from .posts import Posts
+from .users import Users
+
+__all__ = [
+    "Content",
+    "Editor",
+    "Conversations",
+    "Datasets",
+    "Posts",
+    "Files",
+    "Users",
+    "Assets",
+]
+
+
+def EditorFactory(self, **kwargs) -> Editor:
+    return Editor(**kwargs)
+
+
+def ContentFactory(self, **kwargs) -> Content:
+    return Content(**kwargs)
+
+
+def __init__(self, ouro):
+    self.users = Users(ouro)
+    self.assets = Assets(ouro)
+
+    self.datasets = Datasets(ouro)
+    self.files = Files(ouro)
+    self.posts = Posts(ouro)
+    self.conversations = Conversations(ouro)
+
+    self.Editor = self.EditorFactory
+    self.Content = self.ContentFactory
