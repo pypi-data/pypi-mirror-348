a6d091edd43978b23b38c621e1ef495f24cd95b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package wagtailmenupage/1.1.0/wagtailmenupage-1.1.0-py3-none-any.whl

---
+++ packages/wagtailmenupage/wagtailmenupage-1.1.0-py3-none-any.whl/wagtailmenupage/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailmenupageConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wagtailmenupage"
+    verbose_name = "Wagtail Menu Page"
