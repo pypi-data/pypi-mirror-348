94800d63a31e2a2fd19f3b06567046491eafd68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:07:30 +0000

    Add package serieux/0.0.10/serieux-0.0.10-py3-none-any.whl

---
+++ packages/serieux/serieux-0.0.10-py3-none-any.whl/serieux/features/__init__.py
@@ -0,0 +1,20 @@
+from .clargs import FromArguments
+from .dotted import DottedNotation
+from .fromfile import FromFile, FromFileExtra
+from .interpol import Interpolation
+from .lazy import LazyDeserialization
+from .partial import PartialBuilding
+from .tagged import TaggedTypes
+from .tsubclass import TaggedSubclassFeature
+
+__all__ = [
+    "DottedNotation",
+    "FromArguments",
+    "FromFile",
+    "FromFileExtra",
+    "Interpolation",
+    "LazyDeserialization",
+    "PartialBuilding",
+    "TaggedSubclassFeature",
+    "TaggedTypes",
+]
