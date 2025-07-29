388499383f4699a3e85c5bfbcfd0cf40f3824036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:15 +0000

    Add package rest-core/0.1.14/rest_core-0.1.14.tar.gz

---
+++ packages/rest-core/rest_core-0.1.14.tar.gz/rest_core-0.1.14/todo_app/serializers/user_serializer.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+
+from rest_core.serializers import ModelSerializer
+
+User = get_user_model()
+
+
+class UserSerializer(ModelSerializer):
+    """Serializer class for User"""
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+        ]
+        read_only_fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+        ]
