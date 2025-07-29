87345cad9076f8b9c88e77c74739cc1659ae9bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:31 +0000

    Add package cuteagent/0.0.6/cuteagent-0.0.6-py2.py3-none-any.whl

---
+++ packages/cuteagent/cuteagent-0.0.6-py2.py3-none-any.whl/cuteagent/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for CuteAgent."""
+
+__author__ = """Masoud Jb"""
+__email__ = "eng@fintor.com"
+__version__ = "0.0.6"
+
+from .cuteagent import WindowsAgent
+
+__all__ = ["WindowsAgent"] # Optional: to define the public API
