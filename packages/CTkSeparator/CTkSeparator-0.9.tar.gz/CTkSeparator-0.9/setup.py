9defef523b20c3e8beb31614263f8cd39c2f6648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:28 +0000

    Add package CTkSeparator/0.9/CTkSeparator-0.9.tar.gz

---
+++ packages/CTkSeparator/CTkSeparator-0.9.tar.gz/CTkSeparator-0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CTkSeparator",
+    version="0.9",
+    author="AJ-cubes",
+    description="A customizable separator widget for CustomTkinter",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["customtkinter"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
