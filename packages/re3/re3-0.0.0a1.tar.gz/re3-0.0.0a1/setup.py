e0f931f0dc3ce903fd7d418632c073296ba8548d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:26 +0000

    Add package re3/0.0.0a1/re3-0.0.0a1.tar.gz

---
+++ packages/re3/re3-0.0.0a1.tar.gz/re3-0.0.0a1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='re3',
+    description="coming soon",
+    version="0.0.0a1",
+    author='Tim Hatch',
+    author_email='tim@timhatch.com',
+    license="MIT",
+)
