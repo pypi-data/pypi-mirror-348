2d785061979f07b1c4e7e62246cbd997c81c288d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:27 +0000

    Add package pyc-wheel/1.3.3/pyc_wheel-1.3.3.tar.gz

---
+++ packages/pyc-wheel/pyc_wheel-1.3.3.tar.gz/pyc_wheel-1.3.3/tests/test_main.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2019 Adam Karpierz
+# SPDX-License-Identifier: MIT
+
+import unittest
+
+import pyc_wheel
+from pyc_wheel import main
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
