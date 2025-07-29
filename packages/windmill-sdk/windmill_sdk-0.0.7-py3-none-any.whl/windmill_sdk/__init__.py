5794961fd9ab5ca28333c96a6dcd76879a56ed35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:42 +0000

    Add package windmill-sdk/0.0.7/windmill_sdk-0.0.7-py3-none-any.whl

---
+++ packages/windmill-sdk/windmill_sdk-0.0.7-py3-none-any.whl/windmill_sdk/__init__.py
@@ -0,0 +1,12 @@
+from . import clients
+from .models import Tenant, Service, Owner, Customer
+
+__version__ = "0.0.7"
+
+__all__ = [
+    "clients",
+    "Tenant",
+    "Service",
+    "Owner",
+    "Customer",
+]
