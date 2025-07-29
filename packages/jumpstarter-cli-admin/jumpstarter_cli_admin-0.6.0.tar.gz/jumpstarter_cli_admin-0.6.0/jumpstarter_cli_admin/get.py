2e040076ab7afa23fdee5fdd182ef23fb7cb03cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:18 +0000

    Add package jumpstarter-cli-admin/0.6.0/jumpstarter_cli_admin-0.6.0.tar.gz

---
+++ packages/jumpstarter-cli-admin/jumpstarter_cli_admin-0.6.0.tar.gz/jumpstarter_cli_admin-0.6.0/jumpstarter_cli_admin/get.py
@@ -0,0 +1,114 @@
+import logging
+from typing import Optional
+
+import asyncclick as click
+from jumpstarter_cli_common.alias import AliasedGroup
+from jumpstarter_cli_common.opt import (
+    OutputType,
+    opt_context,
+    opt_kubeconfig,
+    opt_log_level,
+    opt_namespace,
+    opt_output_all,
+)
+from jumpstarter_kubernetes import (
+    ClientsV1Alpha1Api,
+    ExportersV1Alpha1Api,
+    LeasesV1Alpha1Api,
+)
+from kubernetes_asyncio.client.exceptions import ApiException
+from kubernetes_asyncio.config.config_exception import ConfigException
+
+from .k8s import (
+    handle_k8s_api_exception,
+    handle_k8s_config_exception,
+)
+from .print import print_client, print_clients, print_exporter, print_exporters, print_lease, print_leases
+
+
+@click.group(cls=AliasedGroup)
+@opt_log_level
+def get(log_level: Optional[str]):
+    """Get Jumpstarter Kubernetes objects"""
+    if log_level:
+        logging.basicConfig(level=log_level.upper())
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+
+@get.command("client")
+@click.argument("name", type=str, required=False, default=None)
+@opt_namespace
+@opt_kubeconfig
+@opt_context
+@opt_output_all
+async def get_client(
+    name: Optional[str], kubeconfig: Optional[str], context: Optional[str], namespace: str, output: OutputType
+):
+    """Get the client objects in a Kubernetes cluster"""
+    try:
+        async with ClientsV1Alpha1Api(namespace, kubeconfig, context) as api:
+            if name is not None:
+                client = await api.get_client(name)
+                print_client(client, output)
+            else:
+                clients = await api.list_clients()
+                print_clients(clients, namespace, output)
+    except ApiException as e:
+        handle_k8s_api_exception(e)
+    except ConfigException as e:
+        handle_k8s_config_exception(e)
+
+
+@get.command("exporter")
+@click.argument("name", type=str, required=False, default=None)
+@opt_namespace
+@opt_kubeconfig
+@opt_context
+@opt_output_all
+@click.option("-d", "--devices", is_flag=True, help="Display the devices hosted by the exporter(s)")
+async def get_exporter(
+    name: Optional[str],
+    kubeconfig: Optional[str],
+    context: Optional[str],
+    namespace: str,
+    devices: bool,
+    output: OutputType,
+):
+    """Get the exporter objects in a Kubernetes cluster"""
+    try:
+        async with ExportersV1Alpha1Api(namespace, kubeconfig, context) as api:
+            if name is not None:
+                exporter = await api.get_exporter(name)
+                print_exporter(exporter, devices, output)
+            else:
+                exporters = await api.list_exporters()
+                print_exporters(exporters, namespace, devices, output)
+    except ApiException as e:
+        handle_k8s_api_exception(e)
+    except ConfigException as e:
+        handle_k8s_config_exception(e)
+
+
+@get.command("lease")
+@click.argument("name", type=str, required=False, default=None)
+@opt_namespace
+@opt_kubeconfig
+@opt_context
+@opt_output_all
+async def get_lease(
+    name: Optional[str], kubeconfig: Optional[str], context: Optional[str], namespace: str, output: OutputType
+):
+    """Get the lease objects in a Kubernetes cluster"""
+    try:
+        async with LeasesV1Alpha1Api(namespace, kubeconfig, context) as api:
+            if name is not None:
+                lease = await api.get_lease(name)
+                print_lease(lease, output)
+            else:
+                leases = await api.list_leases()
+                print_leases(leases, namespace, output)
+    except ApiException as e:
+        handle_k8s_api_exception(e)
+    except ConfigException as e:
+        handle_k8s_config_exception(e)
