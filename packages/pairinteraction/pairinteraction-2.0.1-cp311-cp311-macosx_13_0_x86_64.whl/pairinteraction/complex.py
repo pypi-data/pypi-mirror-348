0620498443bc2b6671b2907c11d3c9e93a9aa64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:08 +0000

    Add package pairinteraction/2.0.1/pairinteraction-2.0.1-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/pairinteraction/pairinteraction-2.0.1-cp311-cp311-macosx_13_0_x86_64.whl/pairinteraction/complex.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2025 Pairinteraction Developers
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+from pairinteraction._wrapped import (
+    BasisAtomComplex as BasisAtom,
+    BasisPairComplex as BasisPair,
+    Database,
+    KetAtom,
+    KetPairComplex as KetPair,
+    StateAtomComplex as StateAtom,
+    StatePairComplex as StatePair,
+    SystemAtomComplex as SystemAtom,
+    SystemPairComplex as SystemPair,
+    diagonalize,
+)
+
+__all__ = [
+    "BasisAtom",
+    "BasisPair",
+    "Database",
+    "KetAtom",
+    "KetPair",
+    "StateAtom",
+    "StatePair",
+    "SystemAtom",
+    "SystemPair",
+    "diagonalize",
+]
