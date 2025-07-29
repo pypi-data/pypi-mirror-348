e95b391f5d9f43220b8d2aae03559ebee42352d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:03 +0000

    Add package botocraft/0.15.0/botocraft-0.15.0-py3-none-any.whl

---
+++ packages/botocraft/botocraft-0.15.0-py3-none-any.whl/botocraft/cli/__init__.py
@@ -0,0 +1,5 @@
+from .cli import cli  # noqa:F401
+from .sync import *  # noqa:F403,F401
+from .botocore import *  # noqa:F403,F401
+from .data import *  # noqa:F403,F401
+from .shell import *  # noqa:F403,F401
