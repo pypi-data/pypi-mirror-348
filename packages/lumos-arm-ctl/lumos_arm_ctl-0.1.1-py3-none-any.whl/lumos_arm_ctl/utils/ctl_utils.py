dc578fffb03925b07234e965590079d2985e67b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:40 +0000

    Add package lumos-arm-ctl/0.1.1/lumos_arm_ctl-0.1.1-py3-none-any.whl

---
+++ packages/lumos-arm-ctl/lumos_arm_ctl-0.1.1-py3-none-any.whl/lumos_arm_ctl/utils/ctl_utils.py
@@ -0,0 +1,32 @@
+def init_keyboard_listener():
+    # Allow to exit early while recording an episode or resetting the environment,
+    # by tapping the right arrow key '->'. This might require a sudo permission
+    # to allow your terminal to monitor keyboard events.
+    events = {}
+    events["exit_early"] = False
+    events["rerecord_episode"] = False
+    events["stop_recording"] = False
+
+    # Only import pynput if not in a headless environment
+    from pynput import keyboard
+
+    def on_press(key):
+        try:
+            if key == keyboard.Key.right:
+                print("Right arrow key pressed. Exiting loop...")
+                events["exit_early"] = True
+            elif key == keyboard.Key.left:
+                print("Left arrow key pressed. Exiting loop and rerecord the last episode...")
+                events["rerecord_episode"] = True
+                events["exit_early"] = True
+            elif key == keyboard.Key.esc:
+                print("Escape key pressed. Stopping data recording...")
+                events["stop_recording"] = True
+                events["exit_early"] = True
+        except Exception as e:
+            print(f"Error handling key press: {e}")
+
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+
+    return listener, events
