07f17f186978dcb2c2bcdc650e7142b21b35de01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:26 +0000

    Add package PyDVP/3.2.0/pydvp-3.2.0-py3-none-any.whl

---
+++ packages/PyDVP/pydvp-3.2.0-py3-none-any.whl/pydvp/__init__.py
@@ -0,0 +1,7 @@
+from .core import version, open, close, setup, query_windows, take, take_close_all, Window
+
+__version__ = "3.2.0"
+__all__ = [
+    "Window",
+    "version", "open", "close", "setup", "query_windows", "take", "take_close_all"
+]
