f59ec27274bbc22e371cb1a8039d07eefefe1e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:59 +0000

    Add package PyDVP/3.1.1/pydvp-3.1.1.tar.gz

---
+++ packages/PyDVP/pydvp-3.1.1.tar.gz/pydvp-3.1.1/README.md
@@ -0,0 +1,7 @@
+An official DEXON DIVIP Python library
+
+## Installation
+
+Install the library using pip:
+
+pip install PyDVP 
---
+++ packages/PyDVP/pydvp-3.1.1.tar.gz/pydvp-3.1.1/pydvp/__init__.py
@@ -0,0 +1,4 @@
+from .core import version, open, close, setup, query_windows, take, take_close_all
+
+__version__ = "3.1.1"
+__all__ = ["version", "open", "close", "setup", "query_windows", "take", "take_close_all"]
