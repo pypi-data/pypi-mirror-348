4a3a2efcfc32041fc42087e60cc5bc4e80153a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:02 +0000

    Add package pairinteraction/2.0.1/pairinteraction-2.0.1-cp310-cp310-win_amd64.whl

---
+++ packages/pairinteraction/pairinteraction-2.0.1-cp310-cp310-win_amd64.whl/pairinteraction/real.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2025 Pairinteraction Developers
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+from pairinteraction._wrapped import (
+    BasisAtomReal as BasisAtom,
+    BasisPairReal as BasisPair,
+    Database,
+    KetAtom,
+    KetPairReal as KetPair,
+    StateAtomReal as StateAtom,
+    StatePairReal as StatePair,
+    SystemAtomReal as SystemAtom,
+    SystemPairReal as SystemPair,
+    diagonalize,
+)
+
+__all__ = [
+    "BasisAtom",
+    "BasisPair",
+    "Database",
+    "KetAtom",
+    "KetPair",
+    "StateAtom",
+    "StatePair",
+    "SystemAtom",
+    "SystemPair",
+    "diagonalize",
+]
