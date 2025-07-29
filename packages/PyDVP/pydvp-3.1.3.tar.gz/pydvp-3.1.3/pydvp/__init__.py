48a0689d2a3d3c1025182ba3b35ab7513a973c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:24 +0000

    Add package PyDVP/3.1.3/pydvp-3.1.3.tar.gz

---
+++ packages/PyDVP/pydvp-3.1.3.tar.gz/pydvp-3.1.3/pydvp/__init__.py
@@ -0,0 +1,7 @@
+from .core import version, open, close, setup, query_windows, take, take_close_all
+
+__version__ = "3.1.3"
+__all__ = [
+    "Window",
+    "version", "open", "close", "setup", "query_windows", "take", "take_close_all"
+]
