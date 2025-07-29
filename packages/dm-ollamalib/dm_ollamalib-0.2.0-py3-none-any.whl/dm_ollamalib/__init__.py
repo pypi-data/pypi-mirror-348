d1ff2ff7d6977f51f5a1691bcdfdf936ac9f2b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:24 +0000

    Add package dm-ollamalib/0.2.0/dm_ollamalib-0.2.0-py3-none-any.whl

---
+++ packages/dm-ollamalib/dm_ollamalib-0.2.0-py3-none-any.whl/dm_ollamalib/__init__.py
@@ -0,0 +1,8 @@
+"""__init__"""
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"  # Fallback for development mode
