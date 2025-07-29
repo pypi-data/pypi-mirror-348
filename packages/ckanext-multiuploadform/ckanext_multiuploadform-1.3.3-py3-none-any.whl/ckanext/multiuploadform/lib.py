4ec1e1412ed73ef20cd48b99e1a7d6d39f2ae0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:21 +0000

    Add package ckanext-multiuploadform/1.3.3/ckanext_multiuploadform-1.3.3-py3-none-any.whl

---
+++ packages/ckanext-multiuploadform/ckanext_multiuploadform-1.3.3-py3-none-any.whl/ckanext/multiuploadform/lib.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+
+import ckan.plugins.toolkit as toolkit
+class Helper():
+
+    def add_resource(package_name, request, active, isLink):
+        context = {'user': toolkit.g.user}
+        package = toolkit.get_action('package_show')(context, {'id': package_name})
+        description = request.form['description']
+        if isLink == 1: # resource is a link not file
+            resource_data = {}
+            resource_data = {
+                'package_id' : package['id'],
+                'url': request.form['url'],
+                'description': description,
+                'name': request.form['name'],
+                'url_type': '',                
+            }
+            resource = toolkit.get_action('resource_create')(context, resource_data)
+            package['resources'].append(resource)
+            if active:
+                package['state'] = 'active'
+            toolkit.get_action('package_update')(context,package)
+            return True
+
+
+        for resource in request.files.getlist('files'):
+            resource_data = {}
+            resource_data = {
+                'package_id' : package['id'],
+                'url': resource.filename,
+                'description': description,
+                'name': resource.filename,
+                'url_type': 'upload',
+                'upload': resource,
+            }
+            resource = toolkit.get_action('resource_create')(context, resource_data)
+            package = toolkit.get_action('package_show')(context, {'id': package_name})
+            package['resources'].append(resource)
+            if active:
+                package['state'] = 'active'
+            toolkit.get_action('package_update')(context,package)
+            
+        return True
+    
+    def check_plugin_enabled(plugin_name):
+        plugins = toolkit.config.get("ckan.plugins")
+        if plugin_name in plugins:
+            return True
+        return False
