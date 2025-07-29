5af30a8335b6dcfea82e3f9d3162812a2dc67b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:07:01 +0000

    Add package django-advanced-report-builder/0.9.8/django_advanced_report_builder-0.9.8.tar.gz

---
+++ packages/django-advanced-report-builder/django_advanced_report_builder-0.9.8.tar.gz/django_advanced_report_builder-0.9.8/advanced_report_builder/includes.py
@@ -0,0 +1,49 @@
+# version = pip_version('django-advanced-report-builder')
+from ajax_helpers.html_include import SourceBase
+
+
+class Dot(SourceBase):
+    static_path = 'advanced_report_builder/dot/'
+    js_filename = 'doT.js'
+
+
+class JQueryExtendext(SourceBase):
+    static_path = 'advanced_report_builder/jquery_extendext/'
+    js_filename = 'jQuery.extendext.js'
+
+
+class QueryBuilder(SourceBase):
+    static_path = 'advanced_report_builder/query_builder/'
+    css_filename = 'query-builder.default.css'
+    js_filename = 'query-builder.min.js'
+
+
+class DashboardInclude(SourceBase):
+    static_path = 'advanced_report_builder/dashboard/'
+    js_filename = 'dashboard.js'
+
+
+class ChartJS(SourceBase):
+    static_path = 'advanced_report_builder/chart-js/'
+    js_filename = [
+        'chart.min.js',
+        'chartjs-adapter-moment.min.js',  # this does require moment however it's already included
+        'chartjs-plugin-datalabels.min.js',
+    ]
+
+
+class D3(SourceBase):
+    static_path = 'advanced_report_builder/d3/'
+    js_filename = ['d3.min.js']
+
+
+class D3Funnel(SourceBase):
+    static_path = 'advanced_report_builder/d3-funnel/'
+    js_filename = ['d3-funnel.min.js']
+
+
+packages = {
+    'query_builder': [JQueryExtendext, Dot, QueryBuilder],
+    'dashboard': [DashboardInclude],
+    'funnel': [D3, D3Funnel],
+}
