f5cc93116b2d0cecf1de73fd210eabbb9ac2b8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:02 +0000

    Add package julep/2.0.0/julep-2.0.0-py3-none-any.whl

---
+++ packages/julep/julep-2.0.0-py3-none-any.whl/julep/types/users/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .doc_list_params import DocListParams as DocListParams
+from .doc_create_params import DocCreateParams as DocCreateParams
+from .doc_search_params import DocSearchParams as DocSearchParams
+from .doc_search_response import DocSearchResponse as DocSearchResponse
