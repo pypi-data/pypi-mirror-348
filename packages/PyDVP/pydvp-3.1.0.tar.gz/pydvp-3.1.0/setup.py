d27d111672527bb17e2c801190211d65a60044e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:50 +0000

    Add package PyDVP/3.1.0/pydvp-3.1.0.tar.gz

---
+++ packages/PyDVP/pydvp-3.1.0.tar.gz/pydvp-3.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PyDVP",  # Your package name
+    version="3.1.0",
+    author="Dexon Systems Ltd.",
+    author_email="sales@dexonsystems.com",
+    description="A DEXON DIVIP Python library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://dexonsystems.com/contact",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
