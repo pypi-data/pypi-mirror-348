64f4f1f71fbd89fffff9ec15f798d1150271dcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package finch-clust/0.2.1/finch-clust-0.2.1.tar.gz

---
+++ packages/finch-clust/finch-clust-0.2.1.tar.gz/finch-clust-0.2.1/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setuptools.setup(
+    name="finch-clust",
+    version="0.2.1",
+    author="Saquib Sarfraz",
+    author_email="saquibsarfraz@gmail.com",
+    description="FINCH - First Integer Neighbor Clustering Hierarchy: A parameter-free fast clustering algorithm.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['finch'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: Freely Distributable",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="finch, finch clustering, clustering, hierarchical clustering",
+    install_requires=[
+        "scipy",
+        "scikit-learn",
+        "numpy"
+    ],
+    extras_require={'ann': ["pynndescent"]},
+    project_urls={
+        "Repository": "https://github.com/ssarfraz/FINCH-Clustering",
+        "Publication": "https://openaccess.thecvf.com/content_CVPR_2019/html/Sarfraz_Efficient_Parameter-Free_Clustering_Using_First_Neighbor_Relations_CVPR_2019_paper.html"
+    }
+)
