ec1d1e19514c3ad7b77a0909a6c41f885a61adef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:52 +0000

    Add package python-1c-odata/0.1.2/python_1c_odata-0.1.2.tar.gz

---
+++ packages/python-1c-odata/python_1c_odata-0.1.2.tar.gz/python_1c_odata-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-1c-odata",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.8.0",
+    ],
+    author="Artem Guliaev",
+    author_email="itsuppartem@yandex.ri",
+    description="Библиотека для работы с 1С через OData",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/itsuppartem/python-1c-odata",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+) 
