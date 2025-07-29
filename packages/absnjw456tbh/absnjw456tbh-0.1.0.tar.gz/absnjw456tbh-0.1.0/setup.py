71df78730b9df8f33bd220b88187e658b46d6cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:06 +0000

    Add package absnjw456tbh/0.1.0/absnjw456tbh-0.1.0.tar.gz

---
+++ packages/absnjw456tbh/absnjw456tbh-0.1.0.tar.gz/absnjw456tbh-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="absnjw456tbh",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Lokesh Kumar Saini",
+    author_email="lokesh.saini@dianapps.com",
+    description="A small utility for string operations",
+    url="https://github.com/dianapps233lokesh/own_package",
+    python_requires='>=3.6',
+)
