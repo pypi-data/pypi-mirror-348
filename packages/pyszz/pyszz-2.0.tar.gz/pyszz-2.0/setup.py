e76eb1e0d809def1bd9da614bba4080b00c00976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package pyszz/2.0/pyszz-2.0.tar.gz

---
+++ packages/pyszz/pyszz-2.0.tar.gz/pyszz-2.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name='pyszz',         # How you named your package folder (MyLib)
+  packages=['pyszz'],   # Chose the same as "name"
+  version='2.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description='pyszz',   # Give a short description about your library
+  author='',                   # Type in your name
+  author_email='',      # Type in your E-Mail
+  url='',   # Provide either the link to your github or to your website
+  download_url='',    # I explain this later on
+  keywords=['pyszz'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+      ],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
