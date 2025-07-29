4f0d7d122516fad386124809567881f25f3633cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:10 +0000

    Add package thumbor/7.7.7/thumbor-7.7.7-cp39-abi3-macosx_11_0_arm64.whl

---
+++ packages/thumbor/thumbor-7.7.7-cp39-abi3-macosx_11_0_arm64.whl/thumbor/handler_lists/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com thumbor@googlegroups.com
+
+from tornado.routing import _RuleList
+
+BUILTIN_HANDLERS = [
+    "thumbor.handler_lists.healthcheck",
+    "thumbor.handler_lists.upload",
+    "thumbor.handler_lists.blacklist",
+]
+
+HandlerList = _RuleList
