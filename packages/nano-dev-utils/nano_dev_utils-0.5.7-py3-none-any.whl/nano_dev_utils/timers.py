0d7e3273354e85f7a81c170d62ab0602eb0b89c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:00 +0000

    Add package nano-dev-utils/0.5.7/nano_dev_utils-0.5.7-py3-none-any.whl

---
+++ packages/nano-dev-utils/nano_dev_utils-0.5.7-py3-none-any.whl/nano_dev_utils/timers.py
@@ -0,0 +1,38 @@
+from functools import wraps
+import time
+from typing import Callable, ParamSpec, TypeVar
+
+P = ParamSpec('P')
+R = TypeVar('R')
+
+
+class Timer:
+    def __init__(self, precision=4, verbose=False):
+        self.precision = precision
+        self.verbose = verbose
+        self.units = [(1e9, 's'), (1e6, 'ms'), (1e3, 'μs'), (1.0, 'ns')]
+
+    def timeit(self, func: Callable[P, R]) -> Callable[P, R]:
+        """Decorator that times function execution with automatic unit scaling."""
+
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            start = time.perf_counter_ns()
+            result = func(*args, **kwargs)
+            elapsed = time.perf_counter_ns() - start
+
+            value = elapsed
+            unit = 'ns'
+
+            for divisor, unit in self.units:
+                if elapsed >= divisor or unit == 'ns':
+                    value = elapsed / divisor
+                    break
+
+            extra_info = f'{args} {kwargs} ' if self.verbose else ''
+            print(
+                f'{func.__name__} {extra_info}took {value:.{self.precision}f} [{unit}]'
+            )
+            return result
+
+        return wrapper
