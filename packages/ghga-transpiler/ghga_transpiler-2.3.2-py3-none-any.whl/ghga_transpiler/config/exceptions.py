0fa2a217fe3b755ab50561e423178c0974cce877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:48 +0000

    Add package ghga-transpiler/2.3.2/ghga_transpiler-2.3.2-py3-none-any.whl

---
+++ packages/ghga-transpiler/ghga_transpiler-2.3.2-py3-none-any.whl/ghga_transpiler/config/exceptions.py
@@ -0,0 +1,30 @@
+# Copyright 2021 - 2025 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+"""Module to collect custom exceptions"""
+
+
+class DuplicatedName(ValueError):
+    """Raised when worksheet names are not unique in the config file"""
+
+
+class MissingWorkbookContent(KeyError):
+    """Raised when any worksheet given in the config yaml does not exist in the spreadsheet"""
+
+
+class UnknownVersionError(RuntimeError):
+    """Raised when the version encountered in the workbook is unknown"""
