104817900f2957da71cc2959fc3035a540338204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:59 +0000

    Add package cherab-lhd/0.2.0/cherab_lhd-0.2.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/cherab-lhd/cherab_lhd-0.2.0-cp312-cp312-macosx_11_0_arm64.whl/cherab/lhd/version.py
@@ -0,0 +1,8 @@
+
+version = "0.2.0"
+__version__ = version
+full_version = version
+
+git_revision = "0f661bf86ff4c5348e602b9f819c10fd971e30fc"
+release = "dev" not in version and "+" not in version
+short_version = version.split("+")[0]
