ff7c1fd10c7e4c2b577d51c91423e032fda4d488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package apns2-up/0.9.0/apns2_up-0.9.0.tar.gz

---
+++ packages/apns2-up/apns2_up-0.9.0.tar.gz/apns2_up-0.9.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='apns2-up',
+    version='0.9.0',
+    packages=['apns2'],
+    install_requires=[
+        'hyper-up>=0.9.0',
+        'PyJWT>=1.4.0',
+        'cryptography>=1.7.2',
+    ],
+    extras_require={
+        "tests": [
+            'freezegun',
+            'pytest',
+        ],
+    },
+    url='https://github.com/vasanski/PyAPNs2',
+    license='MIT',
+    author='Eduard Vasanski',
+    author_email='eduard.vasanski@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='A python library for interacting with the Apple Push Notification Service via HTTP/2 protocol'
+)
