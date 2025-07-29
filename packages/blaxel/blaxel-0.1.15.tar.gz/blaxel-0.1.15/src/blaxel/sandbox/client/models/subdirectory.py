215549f3310f34e786ba8f2a2bb9d8b3304fb426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:08 +0000

    Add package blaxel/0.1.15/blaxel-0.1.15.tar.gz

---
+++ packages/blaxel/blaxel-0.1.15.tar.gz/blaxel-0.1.15/src/blaxel/sandbox/client/models/subdirectory.py
@@ -0,0 +1,60 @@
+from typing import Any, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="Subdirectory")
+
+
+@_attrs_define
+class Subdirectory:
+    """
+    Attributes:
+        path (Union[Unset, str]):
+    """
+
+    path: Union[Unset, str] = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        path = self.path
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if path is not UNSET:
+            field_dict["path"] = path
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        if not src_dict:
+            return None
+        d = src_dict.copy()
+        path = d.pop("path", UNSET)
+
+        subdirectory = cls(
+            path=path,
+        )
+
+        subdirectory.additional_properties = d
+        return subdirectory
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
