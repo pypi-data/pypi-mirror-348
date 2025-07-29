d51d6ac2f149aff82c48b60c3c678ced5729fedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:50 +0000

    Add package ragformance/0.1.35/ragformance-0.1.35.tar.gz

---
+++ packages/ragformance/ragformance-0.1.35.tar.gz/ragformance-0.1.35/ragformance/config/loader.py
@@ -0,0 +1,30 @@
+import json
+import os
+import argparse
+
+
+def load_config(config_path: str):
+    with open(config_path) as file:
+        config = json.load(file)
+    # check is API_KEY is defined in the environment and ovveride it in the config
+    if "API_KEY" in os.environ:
+        config["llm"]["api_key"] = os.environ["API_KEY"]
+    # same for hf_token
+    if "HF_TOKEN" in os.environ:
+        config["hf"]["hf_token"] = os.environ["HF_TOKEN"]
+    return config
+
+
+def get_config():
+    """
+    Reads the -c input parameter and loads the configuration
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Path to the configuration file",
+        default=".config/config.json",
+    )
+    args = parser.parse_args()
+    return load_config(args.config)
