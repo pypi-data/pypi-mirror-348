c3f65571cb35f224f5b065e63536783cffcf1f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:07 +0000

    Add package hepevd/0.3.1/hepevd-0.3.1.tar.gz

---
+++ packages/hepevd/hepevd-0.3.1.tar.gz/hepevd-0.3.1/data/web/mc_data_state.js
@@ -0,0 +1,35 @@
+//
+// MC Data State
+//
+
+import { HitTypeState } from "./hit_type_state.js";
+
+export class MCDataState {
+  constructor(mcHits) {
+    this.mcHits = mcHits;
+    this.activeMC = [];
+  }
+
+  /**
+   * Get the active MC, if any.
+   *
+   * @returns {any} The active MC.
+   */
+  get mc() {
+    return this.activeMC;
+  }
+
+  /**
+   * Top level update function, to update what the active MC hits are.
+   *
+   * @param {HitTypeState} hitTypeState - The hit type state object.
+   */
+  updateActive(hitTypeState) {
+    this.activeMC = this.mcHits.filter((mcHit) => {
+      // Skip if hit type is not active
+      if (!hitTypeState.checkHitType(mcHit)) return false;
+
+      return true;
+    });
+  }
+}
