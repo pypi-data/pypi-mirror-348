5e2e017c960146a064bbf5ff1425553a0867a9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:58:06 +0000

    Add package mymathlib-test/0.1/mymathlib_test-0.1.tar.gz

---
+++ packages/mymathlib-test/mymathlib_test-0.1.tar.gz/mymathlib_test-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mymathlib_test',
+    version='0.1',
+    description='A simple math operations library for demo purposes',
+    author='nani123',
+    author_email='nanireddy8898@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+    license_files = "MIT",
+
+)
