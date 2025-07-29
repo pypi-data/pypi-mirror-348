923784360ca1ecb459eea6ead2a548cf1523e9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:57 +0000

    Add package autoskope-client/0.1.2/autoskope_client-0.1.2.tar.gz

---
+++ packages/autoskope-client/autoskope_client-0.1.2.tar.gz/autoskope_client-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autoskope_client",
+    version="0.1.2",
+    description="Python client library for the Autoskope API.",
+    author="Nico Liebeskind",
+    author_email="nico@autoskope.de",
+    url="https://github.com/mcisk/autoskope_client",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.8.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
