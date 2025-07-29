249fb26db8a639c8c7857df6ac235cfc59fd3228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:50 +0000

    Add package leneda-client/0.5.0/leneda_client-0.5.0-py3-none-any.whl

---
+++ packages/leneda-client/leneda_client-0.5.0-py3-none-any.whl/leneda/exceptions.py
@@ -0,0 +1,21 @@
+"""
+Custom exceptions for the Leneda API client.
+"""
+
+
+class LenedaException(Exception):
+    """Base exception for all Leneda API client exceptions."""
+
+    pass
+
+
+class UnauthorizedException(LenedaException):
+    """Raised when API authentication fails (401 Unauthorized)."""
+
+    pass
+
+
+class ForbiddenException(LenedaException):
+    """Raised when access is forbidden (403 Forbidden), typically due to geoblocking or other access restrictions."""
+
+    pass
