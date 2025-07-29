a59d239b3601954f2f645f27c84be353374dca48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:06 +0000

    Add package detectaicore/0.0.24/detectaicore-0.0.24.tar.gz

---
+++ packages/detectaicore/detectaicore-0.0.24.tar.gz/detectaicore-0.0.24/detectaicore/__init__.py
@@ -0,0 +1,5 @@
+from detectaicore.src.constants import *
+from detectaicore.src.constants_pii import *
+from detectaicore.src.utils import *
+from detectaicore.src.logger_util import *
+from detectaicore.classes.datadetectclasses import *
