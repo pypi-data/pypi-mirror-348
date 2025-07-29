ed28aa5e9c49c2cedec45cd90e36d41119f39c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:17 +0000

    Add package torchcrepe/0.0.24/torchcrepe-0.0.24.tar.gz

---
+++ packages/torchcrepe/torchcrepe-0.0.24.tar.gz/torchcrepe-0.0.24/torchcrepe/load.py
@@ -0,0 +1,31 @@
+import os
+
+import numpy as np
+import torch
+import torchaudio
+from scipy.io import wavfile
+
+import torchcrepe
+
+
+def audio(filename):
+    """Load audio from disk"""
+    return torchaudio.load(filename)
+
+
+def model(device, capacity='full'):
+    """Preloads model from disk"""
+    # Bind model and capacity
+    torchcrepe.infer.capacity = capacity
+    torchcrepe.infer.model = torchcrepe.Crepe(capacity)
+
+    # Load weights
+    file = os.path.join(os.path.dirname(__file__), 'assets', f'{capacity}.pth')
+    torchcrepe.infer.model.load_state_dict(
+        torch.load(file, map_location=device, weights_only=True))
+
+    # Place on device
+    torchcrepe.infer.model = torchcrepe.infer.model.to(torch.device(device))
+
+    # Eval mode
+    torchcrepe.infer.model.eval()
