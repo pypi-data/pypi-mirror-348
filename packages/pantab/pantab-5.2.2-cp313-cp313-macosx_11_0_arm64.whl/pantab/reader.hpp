b5d4b911e11a3b5872927c7d2dbdaf2967bd226b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:26 +0000

    Add package pantab/5.2.2/pantab-5.2.2-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/pantab/pantab-5.2.2-cp313-cp313-macosx_11_0_arm64.whl/pantab/reader.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/unordered_map.h>
+
+auto read_from_hyper_query(
+    const std::string &path, const std::string &query,
+    std::unordered_map<std::string, std::string> &&process_params,
+    size_t chunk_size) -> nanobind::capsule;
