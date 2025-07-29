eb5bca182ed1d02ad4cb958f29a707009742d3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:56 +0000

    Add package deepsearcher/0.0.2/deepsearcher-0.0.2-py3-none-any.whl

---
+++ packages/deepsearcher/deepsearcher-0.0.2-py3-none-any.whl/deepsearcher/embedding/__init__.py
@@ -0,0 +1,29 @@
+from .bedrock_embedding import BedrockEmbedding
+from .fastembed_embdding import FastEmbedEmbedding
+from .gemini_embedding import GeminiEmbedding
+from .glm_embedding import GLMEmbedding
+from .milvus_embedding import MilvusEmbedding
+from .novita_embedding import NovitaEmbedding
+from .ollama_embedding import OllamaEmbedding
+from .openai_embedding import OpenAIEmbedding
+from .ppio_embedding import PPIOEmbedding
+from .sentence_transformer_embedding import SentenceTransformerEmbedding
+from .siliconflow_embedding import SiliconflowEmbedding
+from .volcengine_embedding import VolcengineEmbedding
+from .voyage_embedding import VoyageEmbedding
+
+__all__ = [
+    "MilvusEmbedding",
+    "OpenAIEmbedding",
+    "VoyageEmbedding",
+    "BedrockEmbedding",
+    "SiliconflowEmbedding",
+    "GeminiEmbedding",
+    "PPIOEmbedding",
+    "VolcengineEmbedding",
+    "GLMEmbedding",
+    "OllamaEmbedding",
+    "FastEmbedEmbedding",
+    "NovitaEmbedding",
+    "SentenceTransformerEmbedding",
+]
