5030daf074331a4712dd52639547cdc17bc1f2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:57 +0000

    Add package notify-client-server/0.1.7/notify_client_server-0.1.7.tar.gz

---
+++ packages/notify-client-server/notify_client_server-0.1.7.tar.gz/notify_client_server-0.1.7/src/notify_client_server/build_info.py
@@ -0,0 +1,11 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.17'
+    os_name = 'posix:ubuntu'
+    version = '0.1.0'
+    git_sha = '38e897cf92f257ae48c609ae5f69287f5e4df92e'
+    git_branch = 'master'
+    git_uncommitted = [
+        ' M src/notify_client_server/build_info.py',
+    ]
+    git_unpushed = [
+    ]
