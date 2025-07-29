e4795922476d607c7539d09afb610685c0badaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:07:20 +0000

    Add package qciconnect/0.3.0/qciconnect-0.3.0.tar.gz

---
+++ packages/qciconnect/qciconnect-0.3.0.tar.gz/qciconnect-0.3.0/qciconnect/exceptions.py
@@ -0,0 +1,8 @@
+class KeycloakTokenManagerException(Exception):
+    """Exception class for exceptions from KeycloakTokenManager."""
+
+class QciConnectClientException(Exception):
+    """Exception class for exceptions from QciConnectClient."""
+
+class InvalidIdentifierException(Exception):
+    """Thrown when identifiers contain forbidden characters."""
---
+++ packages/qciconnect/qciconnect-0.3.0.tar.gz/qciconnect-0.3.0/qciconnect/utils.py
@@ -0,0 +1,9 @@
+class Utils:
+
+    def check_system(self):
+        # - run tests
+        # - help check local env with docker and so on ...
+        # see https://github.com/docker/docker-py
+        # see https://docker-py.readthedocs.io/en/stable/containers.html
+        print("nice_stats")
+    
