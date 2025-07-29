8b1a125a43a30ae406242fc8e3efda32b79b938d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:30 +0000

    Add package mud-git/1.0.25/mud_git-1.0.25.tar.gz

---
+++ packages/mud-git/mud_git-1.0.25.tar.gz/mud_git-1.0.25/src/mud/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from . import utils, settings
+from .app import App
+
+
+def run():
+	try:
+		utils.settings = settings.Settings(utils.SETTINGS_FILE_NAME, utils.OLD_SETTINGS_FILE_NAME)
+
+		app = App()
+		app.run()
+	except KeyboardInterrupt:
+		utils.print_error(0)
+
+
+if __name__ == '__main__':
+	run()
