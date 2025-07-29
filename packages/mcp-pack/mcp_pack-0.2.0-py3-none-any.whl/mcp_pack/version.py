2a4577dd49150582f917b0cdf226c4ec12f30f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:58 +0000

    Add package mcp-pack/0.2.0/mcp_pack-0.2.0-py3-none-any.whl

---
+++ packages/mcp-pack/mcp_pack-0.2.0-py3-none-any.whl/mcp_pack/version.py
@@ -0,0 +1,9 @@
+"""
+Version and license information.
+"""
+
+__all__ = ["__version__", "__versiondate__", "__license__"]
+
+__version__ = "0.2.0"
+__versiondate__ = '2025-05-15'
+__license__ = "Apache-2.0"
