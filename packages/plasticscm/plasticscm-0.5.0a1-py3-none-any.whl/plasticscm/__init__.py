9926f9039287aca3f542557baba9244ebbda61bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:09 +0000

    Add package plasticscm/0.5.0a1/plasticscm-0.5.0a1-py3-none-any.whl

---
+++ packages/plasticscm/plasticscm-0.5.0a1-py3-none-any.whl/plasticscm/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2019 Adam Karpierz
+# SPDX-License-Identifier: Zlib
+
+from .__about__ import * ; del __about__  # noqa
+
+from ._plastic   import * ; del _plastic  # noqa
+from .exceptions import *  # noqa
+from . import config ; del config
+from . import model  ; del model
