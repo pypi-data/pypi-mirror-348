747b1f99fd06e57a44bbb9f49b361adc931abeab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:41 +0000

    Add package centralcli/8.4.2/centralcli-8.4.2-py3-none-any.whl

---
+++ packages/centralcli/centralcli-8.4.2-py3-none-any.whl/centralcli/typedefs.py
@@ -0,0 +1,39 @@
+from yarl import URL
+from typing import Union, Literal, List, Dict  # future annotations does not work here, need to use Union to support py < 3.10
+
+
+
+StrOrURL = Union[str, URL]
+Method = Literal['GET', 'POST', 'PUT', 'DELETE']
+SiteData = Union[
+    List[
+        Dict[
+            str,
+            Union[
+                str, int, float
+            ]
+        ]
+    ],
+    Dict[
+        str,
+        Union[
+            str,
+            int,
+            float
+        ]
+    ]
+]
+PortalAuthType = Literal["user/pass", "anon", "self-reg"]
+PortalAuthTypes = List[PortalAuthType]
+CacheTableName = Literal["devices", "sites", "groups", "labels", "macs", "mpsk"]
+DynamicAntenna = Literal["narrow", "wide"]
+RadioType = Literal["2.4", "5", "6"]
+MPSKStatus = Literal["enabled", "disabled"]
+
+# StrEnum available python 3.11+
+try:
+    from enum import StrEnum
+except ImportError:
+    from enum import Enum
+    class StrEnum(Enum):
+        ...
