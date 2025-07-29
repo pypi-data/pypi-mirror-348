b80e4cd18e0cfb29d289539c209164a3c28b0663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:23 +0000

    Add package report-of-monaco-2018/0.0.9/report_of_monaco_2018-0.0.9-py3-none-any.whl

---
+++ packages/report-of-monaco-2018/report_of_monaco_2018-0.0.9-py3-none-any.whl/report_of_monaco_2018/main.py
@@ -0,0 +1,46 @@
+import argparse
+from pathlib import Path
+from report_of_monaco_2018.racing import RaceData
+
+
+def race_report() -> None:
+    DEFAULT_DATA_PATH = Path(__file__).parent / "data_example"
+
+    parser = argparse.ArgumentParser(
+        description="Monaco 2018 Q1 lap‐time report"
+    )
+    parser.add_argument(
+        "--file", "-f",
+        type=str,
+        required=True,
+        help="the way to file (start.log, end.log, abbreviations.txt)"
+    )
+    parser.add_argument(
+        "--driver", "-d",
+        type=str,
+        help="if True  print by driver"
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--asc", action="store_true", help="sorting asc")
+    group.add_argument("--desc", action="store_true", help="sorting desc")
+
+    args = parser.parse_args()
+    sort_order = "desc" if args.desc else "asc"
+    if args.file is None:
+        folder_path = DEFAULT_DATA_PATH
+    else:
+        folder_path = Path(__file__).parent / args.file
+    try:
+        RaceData().print_report(
+            folder=folder_path,
+            driver=args.driver,
+            sort_order=sort_order
+        )
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"[ERROR] {e}")
+    except ValueError as e:
+        raise ValueError(f"[ERROR] {e}")
+
+
+if __name__ == "__main__":
+   race_report()
