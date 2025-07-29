649998ef07618bb39951c5d6f05a95cd083a4451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:26 +0000

    Add package GuiGen/1.1.0/GuiGen-1.1.0-py3-none-any.whl

---
+++ packages/GuiGen/GuiGen-1.1.0-py3-none-any.whl/gui_gen/arguments/generic.py
@@ -0,0 +1,16 @@
+from gui_gen.meta.meta import MetaArg
+
+
+class Argument(metaclass=MetaArg):
+    maps_to: type = str
+    template_html: str = "templates/generic.jinja2"
+
+    def __init__(self, name, default):
+        self.name = name
+        self.default = default
+
+    def map(self, value):
+        try:
+            return self.maps_to(value)
+        except Exception as e:
+            return self.default
