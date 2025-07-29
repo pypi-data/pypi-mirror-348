de5e9bd138501204866ae948cde029f5125ecf86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:06 +0000

    Add package naxai/0.0.19/naxai-0.0.19-py3-none-any.whl

---
+++ packages/naxai/naxai-0.0.19-py3-none-any.whl/naxai/resources/__init__.py
@@ -0,0 +1,18 @@
+"""
+Resource classes for the Naxai SDK.
+
+This package provides access to various API resources including voice, calendars,
+email, and people management functionality.
+"""
+
+from .voice import VoiceResource
+from .calendars import CalendarsResource
+from .email import EmailResource
+from .people import PeopleResource
+
+_all_ = [
+    "VoiceResource",
+    "CalendarsResource",
+    "EmailResource",
+    "PeopleResource"
+]
