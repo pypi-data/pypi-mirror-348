db9f6ebe74251c6e96cc181ebf512e62a0404930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:58 +0000

    Add package maths-add/0.2.3/maths_add-0.2.3.tar.gz

---
+++ packages/maths-add/maths_add-0.2.3.tar.gz/maths_add-0.2.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup,find_packages
+
+setup(
+	name='maths_add',
+	version='0.2.3',
+	description='A extended math library',
+	long_description=open('README.md',encoding="utf-8").read(),
+	long_description_content_type='text/markdown',
+	author='fourth-dimensional_universe',
+	author_email='3817201131@qq.com',
+	url='https://github.com/fourth-dimensional/maths_add',
+	license='MIT',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+	packages=find_packages(),
+	python_requires='>=3.7',
+	install_requires=[
+		'pycryptodome',
+		'cryptography'
+	]
+	)
+
+
+
+
