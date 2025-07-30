446b03bda37dfc1bc0e6e896b647268b53fffc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:09:50 +0000

    Add package docker-migration/0.1.8/docker_migration-0.1.8.tar.gz

---
+++ packages/docker-migration/docker_migration-0.1.8.tar.gz/docker_migration-0.1.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-migration",
+    version="0.1.8",  # Increment version number
+    author="Anton Pavlenko",
+    author_email="apavlenko@hmcorp.fund",
+    description="A tool for migrating Docker applications between servers",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/HMCorp-Fund/docker_migration",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "docker",
+        "requests",
+        "paramiko",
+        "pyyaml>=6.0.1",
+        "zipfile36",
+    ],
+    entry_points={
+        'console_scripts': [
+            'docker-migration=docker_migration.main:main',
+        ],
+    },
+)
