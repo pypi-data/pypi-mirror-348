6adc3eafd3d21ece2e1c2dfada83a12efc97409f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:13:10 +0000

    Add package typically/0.1.2/typically-0.1.2.tar.gz

---
+++ packages/typically/typically-0.1.2.tar.gz/typically-0.1.2/typically/_model.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+import builtins
+import orjson
+from abc import ABC, abstractmethod
+from datetime import datetime
+from types import ModuleType
+from typing import Any, TypeVar, overload, ClassVar
+from typing import Generic, Iterable, TypeVar, Self
+from pydantic import BaseModel, ConfigDict, Json, Field
+from pydantic_core import core_schema, PydanticSerializationUnexpectedValue
+from pydantic.annotated_handlers import (
+    GetJsonSchemaHandler,
+    GetCoreSchemaHandler,
+)
+from pydantic.json_schema import JsonSchemaValue
+
+
+Model = BaseModel
+
+
+class Documented(Model):
+    model_config = ConfigDict(use_attribute_docstrings=True)
+
+
+class Blob(Model): ...
