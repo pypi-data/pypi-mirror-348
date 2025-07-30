53ea368702537080b7c7f7bbc769f1771f4b2ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:52 +0000

    Add package bilmb/0.1.0/bilmb-0.1.0-py3-none-any.whl

---
+++ packages/bilmb/bilmb-0.1.0-py3-none-any.whl/blimb/__init__.py
@@ -0,0 +1,11 @@
+def bilmb(name):
+    if name == "Dilda":
+        invite = (
+            "\u4f60"
+            "\u613f\u610f\u548c\u6211\u4e00\u8d77"
+            "\u53bb\u821e\u4f1a\u5417"
+            "\uff1f"
+        )
+        print(invite)
+    else:
+        print("hello")
