9503c33d7cdc7e169ae73bfe4ab1f01daed3097f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:11:28 +0000

    Add package terra-classic-data-analysis-sdk/0.1.6/terra_classic_data_analysis_sdk-0.1.6.tar.gz

---
+++ packages/terra-classic-data-analysis-sdk/terra_classic_data_analysis_sdk-0.1.6.tar.gz/terra_classic_data_analysis_sdk-0.1.6/terra_classic_sdk/util/converter.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+
+def to_isoformat(dt: datetime) -> str:
+    return (
+        dt.isoformat(timespec="microseconds")
+        .replace("+00:00", "Z")
+        .replace(".000Z", "Z")
+    )
