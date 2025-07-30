40d3a6fb57b026f0b5d9de5d35fef7a0fe3b9c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:34 +0000

    Add package rtctree-aist-test/4.2.5/rtctree_aist_test-4.2.5.tar.gz

---
+++ packages/rtctree-aist-test/rtctree_aist_test-4.2.5.tar.gz/rtctree_aist_test-4.2.5/README.md
@@ -0,0 +1,5 @@
+
+# rtctree
+
+rtctee is a Python library providing an easy-to-use API for interacting
+with running RT Components and RTM-based systems running on OpenRTM-aist.
---
+++ packages/rtctree-aist-test/rtctree_aist_test-4.2.5.tar.gz/rtctree_aist_test-4.2.5/rtctree/__init__.py
@@ -0,0 +1,35 @@
+# -*- Python -*-
+# -*- coding: utf-8 -*-
+
+'''rtctree
+
+Copyright (C) 2009-2015
+    Geoffrey Biggs
+    RT-Synthesis Research Group
+    Intelligent Systems Research Institute,
+    National Institute of Advanced Industrial Science and Technology (AIST),
+    Japan
+    All rights reserved.
+Licensed under the GNU Lesser General Public License version 3.
+http://www.gnu.org/licenses/lgpl-3.0.en.html
+
+Library for easy access to name servers running components, the components
+themselves, and managers.
+
+'''
+
+
+RTCTREE_VERSION = '4.2.5'
+#RTCTREE_VERSION = '0.0.1'
+NAMESERVERS_ENV_VAR = 'RTCTREE_NAMESERVERS'
+ORB_ARGS_ENV_VAR = 'RTCTREE_ORB_ARGS'
+ORB_SSL_ENABLE_ENV_VAR = 'RTCTREE_SSL_ENABLE'
+ORB_SSL_CAFILE_ENV_VAR = 'ORBsslCAFile'
+ORB_SSL_KEYFILE_ENV_VAR = 'ORBsslKeyFile'
+ORB_SSL_KEYPASSWORD_ENV_VAR = 'ORBsslKeyPassword'
+ORB_HTTP_ENABLE_ENV_VAR = 'RTCTREE_HTTP_ENABLE'
+ORB_HTTPS_CAFILE_ENV_VAR = 'ORBhttpsCAFile'
+ORB_HTTPS_KEYFILE_ENV_VAR = 'ORBhttpsKeyFile'
+ORB_HTTPS_KEYPASSWORD_ENV_VAR = 'ORBhttpsKeyPassword'
+
+# vim: set expandtab tabstop=8 shiftwidth=4 softtabstop=4 textwidth=79
