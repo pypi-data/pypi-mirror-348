6efc8757a7dff369935b3b8d5f9c2e8c3a009105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:44 +0000

    Add package vaapi/0.6.19/vaapi-0.6.19.tar.gz

---
+++ packages/vaapi/vaapi-0.6.19.tar.gz/vaapi-0.6.19/vaapi/__init__.py
@@ -0,0 +1,8 @@
+"""This package provides a client class that serves as wrapper for operations that
+you can perform with the API of the visual analytics tool by Berlin United for the Robocup SPL"""
+
+__version__ = "0.6.19"
+
+__pdoc__ = {
+    "core": False,  # Excludes the entire module/folder
+}
