1163bd95431d3fc4f9a69b23ec4f677b62232689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:19 +0000

    Add package watson-osint/0.1.5/watson_osint-0.1.5-py3-none-any.whl

---
+++ packages/watson-osint/watson_osint-0.1.5-py3-none-any.whl/watson/__init__.py
@@ -0,0 +1,12 @@
+"""
+Watson OSINT Tool
+================
+
+Un outil puissant pour rechercher des noms d'utilisateur sur de multiples plateformes.
+Développé par margoul1.
+"""
+
+__version__ = "0.1.0"
+__author__ = "margoul1"
+
+from watson.core.watson import main 
