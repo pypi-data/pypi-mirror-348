6e68e2632636ddacbd70b9336cbde588b3fd7e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:13 +0000

    Add package pyXLMS/0.0.3/pyxlms-0.0.3-py3-none-any.whl

---
+++ packages/pyXLMS/pyxlms-0.0.3-py3-none-any.whl/pyXLMS/transform.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# 2024 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+from .transform_util import modifications_to_str  # noqa: F401
+from .transform_to_dataframe import to_dataframe  # noqa: F401
