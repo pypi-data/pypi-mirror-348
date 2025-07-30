14e5490da50be7ed15bcf73c1640637d82190fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:39 +0000

    Add package predictrampyfinance/0.1.6/predictrampyfinance-0.1.6.tar.gz

---
+++ packages/predictrampyfinance/predictrampyfinance-0.1.6.tar.gz/predictrampyfinance-0.1.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='predictrampyfinance',
+    version='0.1.6',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    description='Access stock JSON financial data via a simple API',
+    author='PredictRAM Data',
+    author_email='you@example.com',
+    url='https://pypi.org/project/predictrampyfinance',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
