4aed17d455aa25cc0424a03c423d9ab089cf8cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:23 +0000

    Add package muspi-merol/0.2.2.7/muspi_merol-0.2.2.7.tar.gz

---
+++ packages/muspi-merol/muspi_merol-0.2.2.7.tar.gz/muspi_merol-0.2.2.7/m/cli/__init__.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+from ..utils.cmd import get_runner
+from ..utils.console import print_version
+
+if len(arg := argv[1:]) == 1 and arg[0] in {"-v", "--version"}:
+    print_version()
+    exit()  # early exit
+
+
+from ..config.load import load_config
+
+config = load_config()
+
+if len(argv) > 1:
+    for alias, item in config["aliases"].items():
+        if argv[1] == alias:
+            get_runner(item)()
+            # early exit
