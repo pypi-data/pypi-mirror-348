45879974d06afd45bd68f4e78fb52ca0190134c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:15:00 +0000

    Add package pyper-sdk/0.4.1/pyper_sdk-0.4.1-py3-none-any.whl

---
+++ packages/pyper-sdk/pyper_sdk-0.4.1-py3-none-any.whl/piper_sdk/__init__.py
@@ -0,0 +1,15 @@
+# piper_sdk/__init__.py
+
+"""
+Piper SDK Package.
+
+This file marks the directory 'piper_sdk' as a Python package,
+allowing modules within it (like client.py) to be imported.
+"""
+
+# Optionally, you can make classes/functions available directly when importing piper_sdk
+# For example:
+# from .client import PiperClient, PiperAuthError, PiperConfigError
+
+# However, it's often clearer to require users to import explicitly:
+# from piper_sdk.client import PiperClient
