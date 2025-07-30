6fd5bfbb6fae5843555afe0d25fb2cbced8f0048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:56 +0000

    Add package spokenpy/0.1.2/spokenpy-0.1.2.tar.gz

---
+++ packages/spokenpy/spokenpy-0.1.2.tar.gz/spokenpy-0.1.2/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="spokenpy",
+    version="0.1.2",
+    author="Arun",
+    description="Voice-driven programming language for accessibility",
+    packages=find_packages(),
+    install_requires=[
+    "SpeechRecognition",
+    "pyttsx3",
+    "pyaudio; platform_system!='Windows'",
+    "pywin32; platform_system=='Windows'",
+    "lark-parser",
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "spokenpy=spokenpy.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Education",
+        "Topic :: Software Development :: Interpreters",
+    ],
+    python_requires='>=3.7',
+)
