07cd4b68afb697c20e56b48ab878054aaf2e9c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:46 +0000

    Add package RclckSpace/0.2/rclckspace-0.2.tar.gz

---
+++ packages/RclckSpace/rclckspace-0.2.tar.gz/rclckspace-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RclckSpace",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=["requests"],
+    author="RostorVLasov",
+    description="API для сокращения ссылок через rclck.space",
+    url="https://github.com/your-repo/rclckspace",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
