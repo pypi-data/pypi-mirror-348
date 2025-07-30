8cf1ddfafd5a3ec31dce8bf865653909fc62c54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:55 +0000

    Add package polyglotdb/1.3.3/polyglotdb-1.3.3-py3-none-any.whl

---
+++ packages/polyglotdb/polyglotdb-1.3.3-py3-none-any.whl/polyglotdb/__init__.py
@@ -0,0 +1,15 @@
+__all__ = ['query', 'io', 'corpus', 'config', 'exceptions', 'CorpusContext', 'CorpusConfig']
+
+import polyglotdb.query.annotations as graph
+
+import polyglotdb.io as io
+
+import polyglotdb.corpus as corpus
+
+import polyglotdb.exceptions as exceptions
+
+import polyglotdb.config as config
+
+CorpusConfig = config.CorpusConfig
+
+CorpusContext = corpus.CorpusContext
