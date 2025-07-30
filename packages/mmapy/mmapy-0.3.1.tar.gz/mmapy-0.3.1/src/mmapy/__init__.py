b16a5b36c41c72bad54136cae7c5d221a6f29852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:08:02 +0000

    Add package mmapy/0.3.1/mmapy-0.3.1.tar.gz

---
+++ packages/mmapy/mmapy-0.3.1.tar.gz/mmapy-0.3.1/src/mmapy/__init__.py
@@ -0,0 +1,9 @@
+"""
+GCMMA-MMA-Python
+
+This file is part of GCMMA-MMA-Python. GCMMA-MMA-Python is licensed under the terms of GNU 
+General Public License as published by the Free Software Foundation. For more information and 
+the LICENSE file, see <https://github.com/arjendeetman/GCMMA-MMA-Python>. 
+"""
+
+from .mma import mmasub, gcmmasub, subsolv, kktcheck, raaupdate, concheck, asymp
