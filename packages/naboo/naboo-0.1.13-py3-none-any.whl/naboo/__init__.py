40d7e972d47717d333593de894115690d050ebc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:45 +0000

    Add package naboo/0.1.13/naboo-0.1.13-py3-none-any.whl

---
+++ packages/naboo/naboo-0.1.13-py3-none-any.whl/naboo/__init__.py
@@ -0,0 +1,2 @@
+from naboo.db import (Database, Field, ArrayField, BooleanField, CharField, DateField, DateTimeField, # NOQA: F401
+    FloatField, ForeignKeyField, IntField, TextField, TimeField, UUIDField, Query, Model)
