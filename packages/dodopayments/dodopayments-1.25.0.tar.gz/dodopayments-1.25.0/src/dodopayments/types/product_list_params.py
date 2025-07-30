9b0ce82979f2c4d506016a44e61ff6a15c4348eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:16 +0000

    Add package dodopayments/1.25.0/dodopayments-1.25.0.tar.gz

---
+++ packages/dodopayments/dodopayments-1.25.0.tar.gz/dodopayments-1.25.0/src/dodopayments/types/product_list_params.py
@@ -0,0 +1,27 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import TypedDict
+
+__all__ = ["ProductListParams"]
+
+
+class ProductListParams(TypedDict, total=False):
+    archived: bool
+    """List archived products"""
+
+    page_number: Optional[int]
+    """Page number default is 0"""
+
+    page_size: Optional[int]
+    """Page size default is 10 max is 100"""
+
+    recurring: Optional[bool]
+    """Filter products by pricing type:
+
+    - `true`: Show only recurring pricing products (e.g. subscriptions)
+    - `false`: Show only one-time price products
+    - `null` or absent: Show both types of products
+    """
