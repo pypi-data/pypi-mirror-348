548e46fc8c3c755cb2bdc02d77650975f796787b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:31 +0000

    Add package py-contract-codegen/0.1.3/py_contract_codegen-0.1.3-py3-none-any.whl

---
+++ packages/py-contract-codegen/py_contract_codegen-0.1.3-py3-none-any.whl/py_contract_codegen/modules/enums.py
@@ -0,0 +1,27 @@
+from enum import Enum, auto
+
+
+class TargetLib(str, Enum):
+    web3_v7 = "web3_v7"
+    web3_v6 = "web3_v6"
+
+
+class ABIType(Enum):
+    function = auto()
+    event = auto()
+    constructor = auto()
+    fallback = auto()
+    receive = auto()
+    error = auto()
+
+
+class StateMutability(str, Enum):
+    pure = "pure"
+    view = "view"
+    nonpayable = "nonpayable"
+    payable = "payable"
+
+
+class Network(str, Enum):
+    mainnet = "mainnet"
+    sepolia = "sepolia"
