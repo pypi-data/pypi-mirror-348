8678162f74c9a86780db6cf1a93f08a5c43372ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:57 +0000

    Add package mrblack/0.2.4/mrblack-0.2.4-py3-none-any.whl

---
+++ packages/mrblack/mrblack-0.2.4-py3-none-any.whl/mrblack/__version__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File: __version__.py
+# Author: Wadih Khairallah
+# Description: 
+# Created: 2025-05-17 20:35:57
+# Modified: 2025-05-18 01:27:41
+
+__version__ = "0.2.4"
