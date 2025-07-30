3d1e95e338e2051456c9c71f95379287f21044a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:10 +0000

    Add package haystack-experimental/0.10.0/haystack_experimental-0.10.0-py3-none-any.whl

---
+++ packages/haystack-experimental/haystack_experimental-0.10.0-py3-none-any.whl/haystack_experimental/core/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .pipeline.base import ComponentPriority, PipelineBase
+
+__all__ = ["PipelineBase"]
