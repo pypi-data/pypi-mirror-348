d5189887dbdcb0e3e33263c7da37bebdbb16b4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:47 +0000

    Add package evlib/0.1.24/evlib-0.1.24-cp310-cp310-manylinux_2_34_x86_64.whl

---
+++ packages/evlib/evlib-0.1.24-cp310-cp310-manylinux_2_34_x86_64.whl/evlib/__init__.py
@@ -0,0 +1,4 @@
+# This file is required for Python to recognize this directory as a package
+# Import the actual functionality from the Rust extension module
+# ruff: noqa: F403
+from evlib.evlib import *
