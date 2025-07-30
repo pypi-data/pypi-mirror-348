175f4dc20ae7e03c8ffe56403a4ba96c980d7693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:42 +0000

    Add package cibuildwheel/3.0.0b1/cibuildwheel-3.0.0b1-py3-none-any.whl

---
+++ packages/cibuildwheel/cibuildwheel-3.0.0b1-py3-none-any.whl/cibuildwheel/extra.py
@@ -0,0 +1,32 @@
+"""
+These are utilities for the `/bin` scripts, not for the `cibuildwheel` program.
+"""
+
+from collections.abc import Mapping, Sequence
+from io import StringIO
+from typing import Protocol
+
+__all__ = ("Printable", "dump_python_configurations")
+
+
+class Printable(Protocol):
+    def __str__(self) -> str: ...
+
+
+def dump_python_configurations(
+    inp: Mapping[str, Mapping[str, Sequence[Mapping[str, Printable]]]],
+) -> str:
+    output = StringIO()
+    for header, values in inp.items():
+        output.write(f"[{header}]\n")
+        for inner_header, listing in values.items():
+            output.write(f"{inner_header} = [\n")
+            for item in listing:
+                output.write("  { ")
+                dict_contents = (f'{key} = "{value}"' for key, value in item.items())
+                output.write(", ".join(dict_contents))
+                output.write(" },\n")
+            output.write("]\n")
+        output.write("\n")
+    # Strip the final newline, to avoid two blank lines at the end.
+    return output.getvalue()[:-1]
