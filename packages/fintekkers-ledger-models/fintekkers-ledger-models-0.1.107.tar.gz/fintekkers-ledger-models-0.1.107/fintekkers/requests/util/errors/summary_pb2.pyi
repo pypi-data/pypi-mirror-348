776797d37f05f8c3c1f99e3299483d9b0b44dcd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:05 +0000

    Add package fintekkers-ledger-models/0.1.107/fintekkers-ledger-models-0.1.107.tar.gz

---
+++ packages/fintekkers-ledger-models/fintekkers-ledger-models-0.1.107.tar.gz/fintekkers-ledger-models-0.1.107/fintekkers/requests/util/errors/summary_pb2.pyi
@@ -0,0 +1,15 @@
+from fintekkers.requests.util.errors import error_pb2 as _error_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class SummaryProto(_message.Message):
+    __slots__ = ("errors", "warnings")
+    ERRORS_FIELD_NUMBER: _ClassVar[int]
+    WARNINGS_FIELD_NUMBER: _ClassVar[int]
+    errors: _containers.RepeatedCompositeFieldContainer[_error_pb2.ErrorProto]
+    warnings: _containers.RepeatedCompositeFieldContainer[_error_pb2.WarningProto]
+    def __init__(self, errors: _Optional[_Iterable[_Union[_error_pb2.ErrorProto, _Mapping]]] = ..., warnings: _Optional[_Iterable[_Union[_error_pb2.WarningProto, _Mapping]]] = ...) -> None: ...
