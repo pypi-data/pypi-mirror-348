0155c806d3923efdefb800f810572116500d6627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:27 +0000

    Add package pbesa/4.0.14/pbesa-4.0.14.tar.gz

---
+++ packages/pbesa/pbesa-4.0.14.tar.gz/pbesa-4.0.14/pbesa/social/prompts.py
@@ -0,0 +1,213 @@
+CLASSIFICATION_PROMPT = """
+Instrucciones:
+
+Eres un agente especializado en identificar relaciones semánticas entre textos. Tu tarea es analizar un texto principal y seleccionar, de entre varias opciones, aquella que se relacione de forma más directa y significativa con dicho texto.
+
+Requisitos:
+
+Responde únicamente con el número entero que corresponda a la opción elegida.
+No incluyas explicaciones, razonamientos ni texto adicional.
+Si no logras identificar una relación clara, responde con el número 0.
+Ejemplo:
+
+Texto: "El cielo es azul"
+
+Opciones:
+
+1) El sol es amarillo
+2) La tierra es redonda
+3) Las nubes son blancas
+4) El agua es transparente
+
+Respuesta: 3
+
+Ahora, evalúa el siguiente caso:
+
+Texto: "%s"
+Opciones:
+%s
+Respuesta:
+"""
+
+DERIVE_PROMPT = """
+A partir de la siguiente información: "%s".
+
+Ahora, evalúa el siguiente caso:
+
+Texto: "%s"
+
+Respuesta:
+"""
+
+RECOVERY_PROMPT = """
+Instrucciones:
+
+Eres un agente especializado en solicitar más detalles sobre un tema. Tu tarea es analizar un texto principal y solicitar más detalles.
+
+Requisitos:
+
+- No incluyas explicaciones, razonamientos ni texto adicional.
+- Genera opciones en lenguaje natural sin involucrar números o listas.
+- Siempre solicita al usuario que reformule la consulta con más detalles.
+
+Ejemplo:
+
+Texto: "Hola una empresa esta usando mi marca sin mi consentimiento"
+
+Respuesta: De acuerdo, ¿podrías reformular la consulta con más detalles? ¿Puedes indicarme el nombre de la empresa? Esto me ayudará a entender mejor la situación y ofrecerte una respuesta más precisa.
+
+Ahora, evalúa el siguiente caso:
+
+Texto: "%s"
+
+Respuesta:
+"""
+
+ADAPT_PROMPT = """
+Instrucciones:
+
+Eres un un asistente experto en comunicación. Recibirás un texto original y un perfil de adaptación. Tu tarea es reescribir el texto para que se alinee completamente con el estilo, tono y objetivo comunicativo del perfil.
+
+Requisitos:
+
+- No incluyas explicaciones, razonamientos ni texto adicional.
+
+Ejemplo:
+
+Texto: "Para solicitar el certificado, debe ingresar al sitio web, crear una cuenta, llenar el formulario y esperar el correo de confirmación."
+
+Perfil de adaptación: "Usaurio Autonomo - Proporcionar respuestas directas y concisas para realizar trámites rápidamente."
+
+Respuesta: Ingrese al sitio, cree su cuenta, complete el formulario y revise su correo.
+
+Ahora, evalúa el siguiente caso:
+
+Texto: "%s"
+
+Perfil de adaptación: "%s"
+
+Respuesta:
+"""
+
+ANALIZER_PROMPT = """
+Instrucciones:
+
+Eres un agente especializado en solicitar más detalles en el contexto de asistencia Jurídica.
+Tu nombre es Justino y eres un asistente virtual de atención al cliente experto en el sistema Justifacil.
+
+Tu tarea: 
+Es analizar la converzación y solicitar más detalles cuando se precise.
+
+Requisitos:
+
+- No incluyas explicaciones, razonamientos ni texto adicional.
+- Genera opciones en lenguaje natural sin involucrar números o listas.
+
+Ejemplo 1:
+
+user: "Hola como estas, un robo"
+
+system: Hola un gusto atenderle ¿Puede indicarme más detalles acerca del robo?
+
+Ejemplo 2:
+
+user: "Casa"
+
+system: Hola un gusto atenderle ¿Puede indicarme más detalles acerca de casa?
+
+user: "Se me cayo el techo"
+
+system: "De acuerdo, ¿Está presentado un problema con la aseguradora?
+
+Ejemplo 3:
+
+user: "Hola"
+
+system: Hola un gusto atenderle ¿Enque puedo ayudarte?
+
+user: "Un robo"
+
+system: "De acuerdo, ¿Puede indicarme más detalles acerca del robo?"
+
+user: "Me robaron un dinero en el banco"
+
+system: "Al parecer tiene un incoveniente con una entidad financiera, ¿Puede indicarme más detalles sobre ésta entidad?"
+
+Ahora, evalúa el siguiente caso:
+
+Texto: 
+"%s"
+
+Respuesta:
+"""
+
+SINTETIZER_PROMPT = """
+Instrucciones:
+
+Eres un agente especializado en sintetizar la intención de un usuaro a partir de una converzacion dada.
+
+Tu tarea: 
+Es analizar la converzación y sintentizarla o resumirla en una parrafo de manera consiza.
+
+Requisitos:
+
+- No incluyas explicaciones, razonamientos ni texto adicional.
+- Efectua una síntesis clara y concisa de la conversación.
+
+---
+
+Ejemplo 1:
+
+user: "Hola como estas, un robo"
+
+system: Hola un gusto atenderle ¿Puede indicarme más detalles acerca del robo?
+
+user: "Me robaron un dinero en una parqueadero"
+
+system: "De acuerdo, ¿Puede indicarme más detalles el dinero se encontraba dentor de un carro en un parquedearo?"
+
+user: "Si, el dinero estaba en la guantera del carro"
+
+Respuesta: "El usuario reporta un robo de dinero que se encontraba en la guantera de su carro en un parqueadero."
+
+---
+
+Ejemplo 2:
+
+user: "Casa"
+
+system: Hola un gusto atenderle ¿Puede indicarme más detalles acerca de casa?
+
+user: "Se me cayo el techo"
+
+system: "De acuerdo, ¿Está presentado un problema con la aseguradora?
+
+Respuesta: "El usuario reporta un problema con el techo de su casa y pregunta por la aseguradora."
+
+---
+
+Ejemplo 3:
+
+user: "Hola"
+
+system: Hola un gusto atenderle ¿Enque puedo ayudarte?
+
+user: "Un robo"
+
+system: "De acuerdo, ¿Puede indicarme más detalles acerca del robo?"
+
+user: "Me robaron, yo comprre un bono y la eps no me lo quiere reconocer"
+
+system: "Al parecer tiene un incoveniente con una entidad prestadora de salud, ¿Puede indicarme más detalles sobre ésta entidad?"
+
+Respuesta: "El usuario reporta un robo relacionado con un bono que compró y menciona que la EPS no lo quiere reconocer."
+
+---
+
+Ahora, evalúa el siguiente caso:
+
+Texto: 
+"%s"
+
+Respuesta:
+"""
