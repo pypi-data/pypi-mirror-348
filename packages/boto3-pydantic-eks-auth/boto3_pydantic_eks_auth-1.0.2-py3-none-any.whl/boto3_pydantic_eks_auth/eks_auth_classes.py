fa49547388adb2f37af9c03ffdf84efb7445ec99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:33 +0000

    Add package boto3-pydantic-eks-auth/1.0.2/boto3_pydantic_eks_auth-1.0.2-py3-none-any.whl

---
+++ packages/boto3-pydantic-eks-auth/boto3_pydantic_eks_auth-1.0.2-py3-none-any.whl/boto3_pydantic_eks_auth/eks_auth_classes.py
@@ -0,0 +1,54 @@
+from typing import Any, Dict, IO, List, Literal, Mapping, Optional, Sequence, Union
+from datetime import datetime
+from pydantic import BaseModel, Field
+from botocore.response import StreamingBody
+from aws_resource_validator.pydantic_models.eks_auth.eks_auth_constants import *
+from ..base_validator_model import BaseValidatorModel, EventStream
+
+
+
+
+# This class is the input for the 'assume_role_for_pod_identity' function.
+class AssumeRoleForPodIdentityRequest(BaseValidatorModel):
+    clusterName: str
+    token: str
+
+
+class AssumedRoleUser(BaseValidatorModel):
+    arn: str
+    assumeRoleId: str
+
+
+class Credentials(BaseValidatorModel):
+    sessionToken: str
+    secretAccessKey: str
+    accessKeyId: str
+    expiration: datetime
+
+
+class PodIdentityAssociation(BaseValidatorModel):
+    associationArn: str
+    associationId: str
+
+
+class ResponseMetadata(BaseValidatorModel):
+    RequestId: str
+    HTTPStatusCode: int
+    HTTPHeaders: Dict[str, str]
+    RetryAttempts: int
+    HostId: Optional[str] = None
+
+
+class Subject(BaseValidatorModel):
+    namespace: str
+    serviceAccount: str
+
+
+# This class is the output for the 'assume_role_for_pod_identity' function.
+class AssumeRoleForPodIdentityResponse(BaseValidatorModel):
+    subject: Subject
+    audience: str
+    podIdentityAssociation: PodIdentityAssociation
+    assumedRoleUser: AssumedRoleUser
+    credentials: Credentials
+    ResponseMetadata: ResponseMetadata
