c77d54be73c6229e4f53efa81180eab91ada7c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:37:03 +0000

    Add package MetaRagTool/0.3.8/metaragtool-0.3.8.tar.gz

---
+++ packages/MetaRagTool/metaragtool-0.3.8.tar.gz/metaragtool-0.3.8/MetaRagTool/Constants.py
@@ -0,0 +1,16 @@
+﻿use_wandb = False
+local_mode = False
+trust_remote_code = True
+lang='multi'
+
+
+
+WandbToken = None
+HFToken = None
+GitHubToken = None
+
+API_KEY_GEMINI = None
+API_KEY_OPENAI = None
+API_KEY_OPENROUTER = None
+
+
