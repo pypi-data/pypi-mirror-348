e6deda694fb9f3a3645517c28e03a31461fa20e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:37:03 +0000

    Add package thrads-sdk/0.1.2/thrads_sdk-0.1.2-py3-none-any.whl

---
+++ packages/thrads-sdk/thrads_sdk-0.1.2-py3-none-any.whl/thrads/__init__.py
@@ -0,0 +1,7 @@
+"""Thrads SDK for accessing the Thrads ad platform."""
+
+from .client import ThradsClient
+from .exceptions import ThradsError, AuthenticationError, APIError
+from .models.ad import Ad
+
+__version__ = "0.1.2"
