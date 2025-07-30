de8d1b18995a89e66bb638933234cb81e1c477bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:28 +0000

    Add package NuPlone/4.0.2/NuPlone-4.0.2-py3-none-any.whl

---
+++ packages/NuPlone/NuPlone-4.0.2-py3-none-any.whl/plonetheme/nuplone/tiles/tales.py
@@ -0,0 +1,26 @@
+from chameleon.utils import Markup
+from plonetheme.nuplone.tiles.tile import getTile
+from Products.PageTemplates.Expressions import getTrustedEngine
+from zope.tales.expressions import StringExpr
+
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class TileExpression(StringExpr):
+    def __call__(self, econtext):
+        name = super().__call__(econtext)
+        context = econtext.vars["context"]
+        request = econtext.vars["request"]
+
+        tile = getTile(context, request, name)
+        if tile is None:  # XXX Use custom exception?
+            log.warning("Request for unknown tile %s", name)
+            return ""
+        return Markup(tile())
+
+
+engine = getTrustedEngine()
+engine.registerType("tile", TileExpression)
