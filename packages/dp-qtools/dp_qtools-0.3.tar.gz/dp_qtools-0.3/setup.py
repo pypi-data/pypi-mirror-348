a3e4cbb128425954f2f5dbff0b87143435ea3366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:24 +0000

    Add package dp-qtools/0.3/dp_qtools-0.3.tar.gz

---
+++ packages/dp-qtools/dp_qtools-0.3.tar.gz/dp_qtools-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dp-qtools',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    long_description="qtools is a library of functions used in the Datapod framework"
+)
