bb25ef512ea58aa22065fe8f56337c1011278177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:22 +0000

    Add package devpi-server/6.15.0/devpi-server-6.15.0.tar.gz

---
+++ packages/devpi-server/devpi-server-6.15.0.tar.gz/devpi-server-6.15.0/devpi_server/middleware.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlparse
+
+
+class OutsideURLMiddleware(object):
+    def __init__(self, app, xom):
+        self.app = app
+        self.xom = xom
+
+    def __call__(self, environ, start_response):
+        outside_url = environ.get('HTTP_X_OUTSIDE_URL')
+        if not outside_url:
+            outside_url = self.xom.config.args.outside_url
+        if outside_url:
+            outside_url = urlparse(outside_url)
+            environ['wsgi.url_scheme'] = outside_url.scheme
+            environ['HTTP_HOST'] = outside_url.netloc
+            if outside_url.path:
+                environ['SCRIPT_NAME'] = outside_url.path
+                if environ['PATH_INFO'].startswith(outside_url.path):
+                    environ['PATH_INFO'] = environ['PATH_INFO'][len(outside_url.path):]
+        return self.app(environ, start_response)
