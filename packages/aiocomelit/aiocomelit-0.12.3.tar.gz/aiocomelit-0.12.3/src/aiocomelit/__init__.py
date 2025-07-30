1cddef2a95bcf211ad9f7cd1289cd20821be88ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:43 +0000

    Add package aiocomelit/0.12.3/aiocomelit-0.12.3.tar.gz

---
+++ packages/aiocomelit/aiocomelit-0.12.3.tar.gz/aiocomelit-0.12.3/src/aiocomelit/__init__.py
@@ -0,0 +1,29 @@
+"""aiocomelit library."""
+
+__version__ = "0.12.3"
+
+from .api import (
+    ComeliteSerialBridgeApi,
+    ComelitSerialBridgeObject,
+    ComelitVedoApi,
+    ComelitVedoAreaObject,
+    ComelitVedoZoneObject,
+)
+from .exceptions import (
+    CannotAuthenticate,
+    CannotConnect,
+    CannotRetrieveData,
+    ComelitError,
+)
+
+__all__ = [
+    "CannotAuthenticate",
+    "CannotConnect",
+    "CannotRetrieveData",
+    "ComelitError",
+    "ComelitSerialBridgeObject",
+    "ComelitVedoApi",
+    "ComelitVedoAreaObject",
+    "ComelitVedoZoneObject",
+    "ComeliteSerialBridgeApi",
+]
