dc3eb2cea43a52649797c6a624f6a1d052fc7789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:53 +0000

    Add package datachampsai/0.1/datachampsai-0.1.tar.gz

---
+++ packages/datachampsai/datachampsai-0.1.tar.gz/datachampsai-0.1/chatApp/__init__.py
@@ -0,0 +1,7 @@
+"""
+ChatApp Package
+
+A Streamlit-based app that refines user queries using prior chat history
+and a reference instruction document. Powered by OpenAI and LangChain.
+"""
+__version__ = "0.1.0"
