dc3964d1bc1088a6756a5c16565ef22153644ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:41 +0000

    Add package djwsbridge/0.1.3/djwsbridge-0.1.3.tar.gz

---
+++ packages/djwsbridge/djwsbridge-0.1.3.tar.gz/djwsbridge-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="djwsbridge",
+    version="0.1.3",
+    description="Real-time WebSocket bridge for Django (using Channels)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Asadbek",
+    author_email="email@asadbektuygunov9@gmail.com",
+    url="https://github.com/asadbek000002/djwsbridge",  # bo‘sh qoldirsa ham bo‘ladi
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "Django>=4.0",
+        "channels>=4.0",
+        "asgiref>=3.5",
+        "channels_redis>=4.0",
+        "daphne>=4.0"
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
