57327fe690777577ae63cad687f75b3b8e80537a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:35:54 +0000

    Add package backup-rsync-base/0.1.0/backup_rsync_base-0.1.0.tar.gz

---
+++ packages/backup-rsync-base/backup_rsync_base-0.1.0.tar.gz/backup_rsync_base-0.1.0/src/backup_rsync_base/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.17'
+    os_name = 'posix:ubuntu'
+    version = '0.1.0'
+    git_sha = '20234fc5664393ed1460bb3974c83da53f4886c0'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
