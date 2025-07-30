3fe549d1d42a4df8185f7a39cf4c3c5a5510b1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:38 +0000

    Add package fnsignal/1.0.0/fnsignal-1.0.0.tar.gz

---
+++ packages/fnsignal/fnsignal-1.0.0.tar.gz/fnsignal-1.0.0/fnsignal/__init__.py
@@ -0,0 +1,9 @@
+from .fnsignal import (
+    send_signal,
+    receive_signal,
+    receive_signal_async,
+    wait_for_signals
+)
+
+__version__ = "1.0.0"
+__all__ = ["send_signal", "receive_signal", "receive_signal_async", "wait_for_signals"] 
