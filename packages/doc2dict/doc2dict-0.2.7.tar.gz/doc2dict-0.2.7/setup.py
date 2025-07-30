9e238d7cc3ee623229eb23d4c9a1495cf3332039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:55 +0000

    Add package doc2dict/0.2.7/doc2dict-0.2.7.tar.gz

---
+++ packages/doc2dict/doc2dict-0.2.7.tar.gz/doc2dict-0.2.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="doc2dict",
+    version="0.2.7",
+    packages=find_packages(),
+    install_requires=['selectolax','xmltodict'
+    ],
+    python_requires=">=3.8"
+)
