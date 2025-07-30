28a91bd81c959f0ee337b7cd3c1657c7f8e8c2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:11 +0000

    Add package artemis-model/0.1.107/artemis_model-0.1.107-py3-none-any.whl

---
+++ packages/artemis-model/artemis_model-0.1.107-py3-none-any.whl/artemis_model/banned_tracks.py
@@ -0,0 +1,21 @@
+"""Banned Tracks per Zone Model"""
+
+from sqlalchemy import UUID, Column, Integer
+
+from artemis_model.base import AuditMixin, TimeStampMixin, CustomSyncBase, CustomBase
+
+
+class BannedTracksMixin(TimeStampMixin, AuditMixin):
+    """Banned Tracks per Zone Model"""
+    zone_id: int = Column(Integer, primary_key=True)
+    track_id: UUID = Column(UUID(as_uuid=True), primary_key=True)
+
+
+class BannedTracksSync(CustomSyncBase, BannedTracksMixin):
+    """Banned Tracks per Zone Model"""
+    pass
+
+
+class BannedTracks(CustomBase, BannedTracksMixin):
+    """Banned Tracks per Zone Model"""
+    pass
