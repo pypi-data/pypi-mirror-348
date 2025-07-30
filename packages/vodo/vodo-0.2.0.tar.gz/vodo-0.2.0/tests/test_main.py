ad03e2794fe5148492be9b8e88c4c5a78638e6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:22 +0000

    Add package vodo/0.2.0/vodo-0.2.0.tar.gz

---
+++ packages/vodo/vodo-0.2.0.tar.gz/vodo-0.2.0/tests/test_main.py
@@ -0,0 +1,20 @@
+from typer.testing import CliRunner
+from vodo.main import app
+from vodo.api import list_tasks
+
+runner = CliRunner()
+
+
+def test_tasks():
+    result = runner.invoke(app, ["tasks", "--plaintext"])
+    assert result.exit_code == 0
+    assert "priority:" in result.stdout
+
+
+def test_add():
+    result = runner.invoke(
+        app,
+        ["add", "-d", "test", "-p", 3, "--due", "2025-01-01", "test"],
+    )
+    assert result.exit_code == 0
+    assert "test" in result.stdout
