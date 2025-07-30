516316f2de93f0b75ee3eadd454cedcf6ec9246e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:41 +0000

    Add package openlineage-python/1.33.0/openlineage_python-1.33.0-py3-none-any.whl

---
+++ packages/openlineage-python/openlineage_python-1.33.0-py3-none-any.whl/openlineage/client/generated/documentation_dataset.py
@@ -0,0 +1,17 @@
+# Copyright 2018-2025 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import attr
+from openlineage.client.generated.base import DatasetFacet
+
+
+@attr.define
+class DocumentationDatasetFacet(DatasetFacet):
+    description: str
+    """The description of the dataset."""
+
+    @staticmethod
+    def _get_schema() -> str:
+        return "https://openlineage.io/spec/facets/1-0-1/DocumentationDatasetFacet.json#/$defs/DocumentationDatasetFacet"
---
+++ packages/openlineage-python/openlineage_python-1.33.0-py3-none-any.whl/openlineage/client/generated/documentation_job.py
@@ -0,0 +1,19 @@
+# Copyright 2018-2025 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import attr
+from openlineage.client.generated.base import JobFacet
+
+
+@attr.define
+class DocumentationJobFacet(JobFacet):
+    description: str
+    """The description of the job."""
+
+    @staticmethod
+    def _get_schema() -> str:
+        return (
+            "https://openlineage.io/spec/facets/1-0-1/DocumentationJobFacet.json#/$defs/DocumentationJobFacet"
+        )
