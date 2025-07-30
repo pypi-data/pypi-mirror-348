0afbccd688cfd0f150c021b5bdaefeb4ac0a0000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:34 +0000

    Add package vcmp-python-plugin/1.2.5/vcmp_python_plugin-1.2.5.tar.gz

---
+++ packages/vcmp-python-plugin/vcmp_python_plugin-1.2.5.tar.gz/vcmp_python_plugin-1.2.5/vcmp/events/vehicle.py
@@ -0,0 +1,34 @@
+from ..__export import vcmpVehicleUpdate
+from .abc import Event
+from ..instance import get_vehicle_from_id
+
+class VehicleEvent(Event):
+    def __init__(
+        self,
+        vehicle_id: int,
+        *args,
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.vehicle_id = vehicle_id
+        
+        vehicle = get_vehicle_from_id(vehicle_id)
+        assert vehicle is not None, f"Vehicle id {vehicle_id} does not exists."
+
+        self.vehicle = vehicle
+
+class VehicleUpdateEvent(VehicleEvent):
+    def __init__(
+        self,
+        vehicle_id: int,
+        type: int
+    ):
+        super().__init__(vehicle_id)
+
+        self.type = vcmpVehicleUpdate(type)
+
+class VehicleExplodeEvent(VehicleEvent):
+    ...
+
+class VehicleRespawnEvent(VehicleEvent):
+    ...
