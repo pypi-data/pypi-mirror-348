12ccf09c809ba9f16d3af1644ea919ad51ec80d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:50 +0000

    Add package docker-migration/0.1.6/docker_migration-0.1.6.tar.gz

---
+++ packages/docker-migration/docker_migration-0.1.6.tar.gz/docker_migration-0.1.6/docker_migration/__init__.py
@@ -0,0 +1,15 @@
+"""
+Docker migration module.
+
+This package provides functionality for migrating Docker containers and images.
+"""
+
+__version__ = '0.1.6'
+__author__ = 'Anton Pavlenko'
+
+# Import and expose key components that should be available when importing the package
+# from .core import migrate_container, migrate_image
+# from .utils import DockerClient
+
+# Define what gets imported with `from docker_migration import *`
+# __all__ = ['migrate_container', 'migrate_image', 'DockerClient']
