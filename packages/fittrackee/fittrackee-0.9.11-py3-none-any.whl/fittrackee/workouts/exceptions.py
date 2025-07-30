a83066bd2f03f934ae4c54f9d298e1f088b70b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:49 +0000

    Add package fittrackee/0.9.11/fittrackee-0.9.11-py3-none-any.whl

---
+++ packages/fittrackee/fittrackee-0.9.11-py3-none-any.whl/fittrackee/workouts/exceptions.py
@@ -0,0 +1,23 @@
+from fittrackee.exceptions import GenericException
+
+
+class InvalidDurationException(Exception):
+    def __init__(self) -> None:
+        super().__init__("invalid duration")
+
+
+class InvalidGPXException(GenericException):
+    pass
+
+
+class WorkoutException(GenericException):
+    pass
+
+
+class WorkoutGPXException(GenericException):
+    pass
+
+
+class WorkoutForbiddenException(GenericException):
+    def __init__(self) -> None:
+        super().__init__("error", "you do not have permissions")
