e995c4e55bfb92b10e1a5fc7e54dd24c8f23e452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:11 +0000

    Add package ghostmark/0.0.1/ghostmark-0.0.1-py3-none-any.whl

---
+++ packages/ghostmark/ghostmark-0.0.1-py3-none-any.whl/ghostmark/styles.py
@@ -0,0 +1,9 @@
+from rich.style import Style
+
+SYSTEM_COLOR = "magenta2"
+
+WHITESPACE_STYLE = Style(color="slate_blue1", dim=True)
+INVISIBLE_STYLE = Style(color="gold3", dim=True)
+TRANSLATION_STYLE = Style(color="green_yellow")
+NORMAL_STYLE = Style(color="white")
+SYSTEM_STYLE = Style(color=SYSTEM_COLOR, bold=True)
