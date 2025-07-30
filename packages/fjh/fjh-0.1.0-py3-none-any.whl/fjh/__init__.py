915f5b29878daf4b719ebddb6172adee4f494a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:30 +0000

    Add package fjh/0.1.0/fjh-0.1.0-py3-none-any.whl

---
+++ packages/fjh/fjh-0.1.0-py3-none-any.whl/fjh/__init__.py
@@ -0,0 +1,3 @@
+from .fjh import setTimeout, setInterval, timeIt, catch, catchAll, callLimit, retry, memoize
+
+__all__ = ["setTimeout", "setInterval", "timeIt", "catch", "catchAll", "callLimit", "retry", "memoize"]
