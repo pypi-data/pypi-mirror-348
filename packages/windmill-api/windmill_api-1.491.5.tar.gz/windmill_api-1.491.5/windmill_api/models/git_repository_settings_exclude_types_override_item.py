9009db563f744b9a114b7e5327176a7e5876e30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:49:28 +0000

    Add package windmill-api/1.491.5/windmill_api-1.491.5.tar.gz

---
+++ packages/windmill-api/windmill_api-1.491.5.tar.gz/windmill_api-1.491.5/windmill_api/models/git_repository_settings_exclude_types_override_item.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class GitRepositorySettingsExcludeTypesOverrideItem(str, Enum):
+    APP = "app"
+    FLOW = "flow"
+    FOLDER = "folder"
+    GROUP = "group"
+    RESOURCE = "resource"
+    RESOURCETYPE = "resourcetype"
+    SCHEDULE = "schedule"
+    SCRIPT = "script"
+    SECRET = "secret"
+    USER = "user"
+    VARIABLE = "variable"
+
+    def __str__(self) -> str:
+        return str(self.value)
