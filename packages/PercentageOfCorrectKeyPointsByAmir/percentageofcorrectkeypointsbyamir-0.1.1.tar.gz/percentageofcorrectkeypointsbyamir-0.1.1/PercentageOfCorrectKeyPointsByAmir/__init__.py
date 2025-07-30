86f332840e43b87149a67c4f3b799975896fdca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:37:54 +0000

    Add package PercentageOfCorrectKeyPointsByAmir/0.1.1/percentageofcorrectkeypointsbyamir-0.1.1.tar.gz

---
+++ packages/PercentageOfCorrectKeyPointsByAmir/percentageofcorrectkeypointsbyamir-0.1.1.tar.gz/percentageofcorrectkeypointsbyamir-0.1.1/PercentageOfCorrectKeyPointsByAmir/__init__.py
@@ -0,0 +1,1 @@
+from .PercentageOfCorrectKeyPoints import PercentageOfCorrectKeyPoints
---
+++ packages/PercentageOfCorrectKeyPointsByAmir/percentageofcorrectkeypointsbyamir-0.1.1.tar.gz/percentageofcorrectkeypointsbyamir-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PercentageOfCorrectKeyPointsByAmir',  
+    version='0.1.1',
+    packages=find_packages(),    
+    author='Amir Dalili',
+    description='A sample package taht calcualtes Percentage Of CorrectKey Points',
+    url='https://github.com/yourusername/my_package',   
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
