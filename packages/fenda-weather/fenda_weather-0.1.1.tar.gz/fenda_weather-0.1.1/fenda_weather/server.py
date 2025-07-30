f248aa7444acfcfef4b6ddba6ca9b53ccdb5208f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:48 +0000

    Add package fenda-weather/0.1.1/fenda_weather-0.1.1.tar.gz

---
+++ packages/fenda-weather/fenda_weather-0.1.1.tar.gz/fenda_weather-0.1.1/fenda_weather/server.py
@@ -0,0 +1,19 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("fenda-weather")
+
+
+@mcp.tool()
+def query_weather(city_name: str) -> str:
+    """ 根据城市名称获取天气信息
+
+    :param city_name: 城市名称
+    :return: 天气信息
+    """
+    return "你好，%s的天气很好~ 27度不冷不热" % city_name
+
+def main():
+    mcp.run()
+
+if __name__ == "__main__":
+    mcp.run(transport='stdio')
