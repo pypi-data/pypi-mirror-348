fd54092dd87f97a2bc5437e9994acad40193ec70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:30 +0000

    Add package contsys/1.0.1/contsys-1.0.1.tar.gz

---
+++ packages/contsys/contsys-1.0.1.tar.gz/contsys-1.0.1/contsys/__init__.py
@@ -0,0 +1,13 @@
+from .fonction import fonction
+
+
+class CMD:
+    clear = fonction.clear
+    title = fonction.title
+
+class system:
+    iswin32 = fonction.iswin32
+    islinux = fonction.islinux
+    isdarwin = fonction.isdarwin
+    recupmode = fonction.recupmode
+    reboot = fonction.reboot
