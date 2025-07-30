69596e8c43d655ca7a6154253bb36dc66ff88a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:09 +0000

    Add package chart-studio-figlinq/0.2.5/chart_studio_figlinq-0.2.5-py3-none-any.whl

---
+++ packages/chart-studio-figlinq/chart_studio_figlinq-0.2.5-py3-none-any.whl/chart_studio/figlinq/__init__.py
@@ -0,0 +1,10 @@
+"""
+figlinq
+======
+
+This module defines functionality that requires interaction between your
+local machine and Figlinq.
+
+"""
+
+from .figlinq import upload, download, get_plot_template, apply_plot_template
