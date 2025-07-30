368f83a5f937a205e6c3243eb7ea24bb9da72e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:12 +0000

    Add package niamkeltd-pylib/1.0.0/niamkeltd_pylib-1.0.0-py3-none-any.whl

---
+++ packages/niamkeltd-pylib/niamkeltd_pylib-1.0.0-py3-none-any.whl/niamkeltd_pylib/models/emojis.py
@@ -0,0 +1,17 @@
+# https://www.iemoji.com/view/emoji/535/symbols/white-heavy-check-mark
+class Emojis:
+    # ℹ️
+    INFORMATION = "\u2139"
+
+    # ⚠️
+    WARNING = "\u260A0"
+
+    # ❌
+    RED_CROSS = "\u274C"
+
+    # ✅
+    TICK_GREEN = "\u2705"
+
+    # ☑️
+    TICK_GREY = "\u2611\uFE0F"
+    VARIENT_SELECTOR = "\uFE0F"
