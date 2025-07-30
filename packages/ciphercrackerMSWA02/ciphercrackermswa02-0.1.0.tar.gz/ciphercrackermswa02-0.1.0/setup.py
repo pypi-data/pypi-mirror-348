547a717a933ae2e283dfc3745141a7c553d18f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:16 +0000

    Add package ciphercrackerMSWA02/0.1.0/ciphercrackermswa02-0.1.0.tar.gz

---
+++ packages/ciphercrackerMSWA02/ciphercrackermswa02-0.1.0.tar.gz/ciphercrackermswa02-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+import io
+with io.open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="ciphercrackerMSWA02",
+    version="0.1.0",
+    package_dir={"": "src"},
+    py_modules=["cipherCracker"],
+    author="Oskar Šefců et al.",
+    author_email="oskysef@gmail.com",
+    description="monoalphabetic substitution cipher cracking",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MSWA-02/python_semestral.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
