ea0c5bc760a274c37771b84e6bc40942fe09fc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:11:22 +0000

    Add package ardhi-framework/0.0.1a0/ardhi_framework-0.0.1a0-py3-none-any.whl

---
+++ packages/ardhi-framework/ardhi_framework-0.0.1a0-py3-none-any.whl/ardhi_framework/mfa/fingerprint.py
@@ -0,0 +1,7 @@
+from .base_mfa import AuthBase
+
+class AuthenticateFingerprint(AuthBase):
+
+    def has_fingerprint(self):
+        """Filter signatures by is fingerprinted"""
+        return self.application_instance.fingerprints.filter(user_id=self.user_id).exists()
