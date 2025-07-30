134e5beefc4018a4646b946c0340fc391d0d79f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:43 +0000

    Add package tonnelmp/1.0.6.1/tonnelmp-1.0.6.1-py3-none-any.whl

---
+++ packages/tonnelmp/tonnelmp-1.0.6.1-py3-none-any.whl/tonnelmp/__init__.py
@@ -0,0 +1,44 @@
+from tonnelmp.marketapi import (
+    getGifts,
+    getAuctions,
+    saleHistory,
+    listForSale,
+    cancelSale,
+    buyGift,
+    createAuction,
+    cancelAuction,
+    info,
+    placeBid,
+    returnGift,
+    withdraw,
+    switchTransfer,
+    mintGift,
+    myGifts,
+    joinGiveaway,
+    giveawayInfo,
+    Gift
+)
+
+from tonnelmp.wtf import generate_wtf
+
+__all__ = [
+    "getGifts",
+    "getAuctions",
+    "saleHistory",
+    "listForSale",
+    "cancelSale",
+    "buyGift",
+    "createAuction",
+    "cancelAuction",
+    "generate_wtf",
+    "info",
+    "placeBid",
+    "returnGift",
+    "withdraw",
+    "switchTransfer",
+    "mintGift",
+    "joinGiveaway",
+    "giveawayInfo",
+    "myGifts",
+    "Gift"
+]
