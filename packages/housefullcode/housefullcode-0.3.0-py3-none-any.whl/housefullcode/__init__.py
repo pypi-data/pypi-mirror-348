6f4ec4d1e3eae7592f25304e69d927883613084b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:29 +0000

    Add package housefullcode/0.3.0/housefullcode-0.3.0-py3-none-any.whl

---
+++ packages/housefullcode/housefullcode-0.3.0-py3-none-any.whl/housefullcode/__init__.py
@@ -0,0 +1,5 @@
+# my_package/__init__.py
+
+from .core import exp1,exp2,exp3,exp4,exp5,exp6,exp7,exp8,exp9,exp10
+
+__all__ = ["exp1", "exp2" , "exp3" , "exp4","exp5","exp6","exp7","exp8","exp9","exp10"]  
