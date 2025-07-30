5794cd4bc5bc8c873305fb729822c21158d2fcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:11:23 +0000

    Add package griboedov/0.1/griboedov-0.1.tar.gz

---
+++ packages/griboedov/griboedov-0.1.tar.gz/griboedov-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="griboedov", # имя модуля
+    version="0.1", # версия модуля
+    description="...", # описание проекта
+    packages=["griboedov"], # список с названиями папок
+    author_email="panufriev258@gmail.com", # почта автора
+    zip_safe=False, # пароль автора
+    install_requires=[] # список дополнительных модулей    
+)
