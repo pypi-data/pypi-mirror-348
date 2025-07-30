a33bf55f1e33ecdb27987dd6237163525de32f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:55 +0000

    Add package kap-sdk/0.1.19/kap_sdk-0.1.19.tar.gz

---
+++ packages/kap-sdk/kap_sdk-0.1.19.tar.gz/kap_sdk-0.1.19/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kap_sdk',
+    version='0.1.19',
+    description='Kap Data Scrapping',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pyppeteer',
+        "diskcache"
+    ],
+)
