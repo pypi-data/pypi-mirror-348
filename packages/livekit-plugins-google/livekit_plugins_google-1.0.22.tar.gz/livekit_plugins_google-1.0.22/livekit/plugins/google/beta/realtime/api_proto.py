9f1fb1a12fab0961fa445ac08920973cbfacf76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:36:55 +0000

    Add package livekit-plugins-google/1.0.22/livekit_plugins_google-1.0.22.tar.gz

---
+++ packages/livekit-plugins-google/livekit_plugins_google-1.0.22.tar.gz/livekit_plugins_google-1.0.22/livekit/plugins/google/beta/realtime/api_proto.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Literal, Union
+
+from google.genai import types
+
+LiveAPIModels = Literal["gemini-2.0-flash-exp", "gemini-2.0-flash-live-001"]
+
+Voice = Literal["Puck", "Charon", "Kore", "Fenrir", "Aoede"]
+
+
+ClientEvents = Union[
+    types.ContentListUnion,
+    types.ContentListUnionDict,
+    types.LiveClientContentOrDict,
+    types.LiveClientRealtimeInput,
+    types.LiveClientRealtimeInputOrDict,
+    types.LiveClientToolResponseOrDict,
+    types.FunctionResponseOrDict,
+    Sequence[types.FunctionResponseOrDict],
+]
