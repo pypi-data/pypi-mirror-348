4d52053d8e295758a759d8cebbde900a4a546167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:16 +0000

    Add package yjviewer/0.2.4/yjviewer-0.2.4-py3-none-any.whl

---
+++ packages/yjviewer/yjviewer-0.2.4-py3-none-any.whl/yjviewer/locales.py
@@ -0,0 +1,19 @@
+import ygojson
+
+LOCALE_TRANSLATED = {
+    ygojson.Locale.ENGLISH: "English",
+    ygojson.Locale.ITALIAN: "Italian",
+    ygojson.Locale.JAPANESE: "Japanese",
+    ygojson.Locale.CHINESE_SIMPLIFIED: "Simplified Chinese",
+    ygojson.Locale.CHINESE_TRADITIONAL: "Traditional Chinese",
+    ygojson.Locale.ASIAN_ENGLISH: "Asian-English",
+    ygojson.Locale.ENGLISH_EUROPE: "English (Europe)",
+    ygojson.Locale.ENGLISH_AMERICA: "English (North America)",
+    ygojson.Locale.ENGLISH_OCEANIA: "English (Oceania)",
+    ygojson.Locale.FRENCH: "French",
+    ygojson.Locale.FRENCH_CANADA: "French (Canada)",
+    ygojson.Locale.SPANISH: "Spanish",
+    ygojson.Locale.GERMAN: "German",
+    ygojson.Locale.PORTUGESE: "Portugese",
+    ygojson.Locale.KOREAN: "Korean",
+}
