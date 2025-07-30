7a6438c2ccd25691938d4cd5e25c299dfd8c136e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:34 +0000

    Add package filter-persist/0.1.1/filter_persist-0.1.1.tar.gz

---
+++ packages/filter-persist/filter_persist-0.1.1.tar.gz/filter_persist-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+here = Path(__file__).parent
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="filter-persist",  # The name to install via pip
+    version="0.1.1",         # 🚨 Increment this every release!
+    description="Custom Streamlit AgGrid component",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Parshav Shivnani",
+    packages=find_packages(),  # Automatically find all packages including 'filter_persist'
+    include_package_data=True,
+    install_requires=[
+        "streamlit>=1.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
