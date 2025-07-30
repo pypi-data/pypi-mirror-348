ba075d214d6bbf507c363d2a622a1eb0dbaa92bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:44 +0000

    Add package thenextlei-nester/1.0/thenextlei_nester-1.0.tar.gz

---
+++ packages/thenextlei-nester/thenextlei_nester-1.0.tar.gz/thenextlei_nester-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup (
+
+		name = 'thenextlei_nester',
+		version = '1.0',
+		packages = ['thenextlei_nester'],
+		author = 'lei',
+		author_email = 'thenextlei@gmail.com',
+		description = 'A simple printer of nested lines'
+
+	)
