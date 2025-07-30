b9d482032f1506f89f50ca0d2a054cf387391dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:10:17 +0000

    Add package haiway/0.20.1/haiway-0.20.1-py3-none-any.whl

---
+++ packages/haiway/haiway-0.20.1-py3-none-any.whl/haiway/types/__init__.py
@@ -0,0 +1,14 @@
+from haiway.types.default import Default, DefaultValue
+from haiway.types.frozen import frozenlist
+from haiway.types.missing import MISSING, Missing, is_missing, not_missing, when_missing
+
+__all__ = (
+    "MISSING",
+    "Default",
+    "DefaultValue",
+    "Missing",
+    "frozenlist",
+    "is_missing",
+    "not_missing",
+    "when_missing",
+)
