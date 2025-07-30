e02f66347c86e134234a3054a24ca69ccedf7baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:09:14 +0000

    Add package passagemath-fricas/10.5.35/passagemath_fricas-10.5.35-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/passagemath-fricas/passagemath_fricas-10.5.35-cp310-cp310-macosx_14_0_arm64.whl/sage_wheels/lib/fricas/target/aarch64-apple-darwin23.6.0/algebra/OMSAGG.c
@@ -0,0 +1,6 @@
+/*      Compiler: ECL 24.5.10                                         */
+/*      Date: 2025/5/18 23:35 (yyyy/mm/dd)                            */
+/*      Machine: Darwin 23.6.0 arm64                                  */
+/*      Source: /Users/runner/sage-local/var/tmp/sage/build/fricas-1.3.11/src/pre-generated/src/algebra/OMSAGG.lsp */
+#include <ecl/ecl-cmp.h>
+#include "/Users/runner/sage-local/var/tmp/sage/build/fricas-1.3.11/src/_build/target/aarch64-apple-darwin23.6.0/algebra/OMSAGG.eclh"
