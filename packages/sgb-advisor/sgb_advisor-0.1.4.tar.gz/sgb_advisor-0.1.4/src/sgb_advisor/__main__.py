7e6a3bc511cb6d16acc599e679ad2200328b5ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:50 +0000

    Add package sgb-advisor/0.1.4/sgb_advisor-0.1.4.tar.gz

---
+++ packages/sgb-advisor/sgb_advisor-0.1.4.tar.gz/sgb_advisor-0.1.4/src/sgb_advisor/__main__.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+
+# Need to load dotenv before importing/running any file, since they use API keys from env modules
+load_dotenv(".env")
+
+
+def main():
+    "Entry fuction for the script"
+    from sgb_advisor import get_sgbs, notify
+
+    sgbs = get_sgbs()
+    notify(sgbs)
+
+
+if __name__ == "__main__":
+    main()
