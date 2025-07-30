22c453fb34f0343f276f9bc024a36388e0105599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:05 +0000

    Add package session-sampling-simulator/0.2.1/session_sampling_simulator-0.2.1.tar.gz

---
+++ packages/session-sampling-simulator/session_sampling_simulator-0.2.1.tar.gz/session_sampling_simulator-0.2.1/README.md
@@ -0,0 +1,102 @@
+# session-sampling-simulator
+
+An application that generates synthetic database sessions and allows you to simulate the effects of sampling those sessions to understand the error characteristics.
+
+## Installing
+
+The application is distributed as a Python package. 
+The recommended way to use it is by installing from PyPI using either pipx or uv.
+
+    pipx install session_sampling_simulator
+
+or
+
+    uv tool install session_sampling_simulator
+
+Equivalently you can download the wheel or sdist from the GitHub releases page and substitute the path to the file for `session_sampling_simulator` in either command.
+
+### Warning for uv users!
+
+The standalone Python versions that uv uses don't place nicely with tkinter and matplotlib.
+The GUI itself works ok, but GUI and charts together are a no-no right now
+which means the `gui analyzer` command is broken. 
+I'm working on a 'compatibility mode' that will allow this to work.
+
+### Manual installation
+
+Of course, you can also just create a virtual environment manually, activate it and install the package.
+
+    python -m venv .venv
+    source .venv/bin/activate
+    pip install session_sampling_simulator
+
+## Using
+
+The package installs a CLI script `sesasim` which you can invoke to access various functionality. 
+It's recommended to run `sesasim setup` first. 
+This creates a directory in your home directory which will be used for saving the GUI state. 
+It's also the default location for input files and the `setup` command populates it with some default query files.
+
+You can start the GUI mode with `sesasim gui <mode>`. To get a feel for what sesasim is all about I suggest you start
+with `sesasim gui single-session`. This mode generates a single session according to the parameters you enter, 
+then samples it and presents the true run times of each query and those estimated by sampling.
+
+The `gui analyzer` mode does the same thing, except over many sessions with a range of sampling frequencies and generates
+a chart of the error as a function of sampling frequency.
+
+## Detailed instructions
+
+The CLI has `--help` for every command, so you can access a quick description of each parameter. Some more detail is
+provided below.
+
+### Single-session GUI mode
+
+```commandline
+Usage: sesasim gui single-session [OPTIONS]
+
+ A GUI mode for simulating a single session
+╭─ Options ────────────────────────────────────────────────────────────────────╮
+│ --query-file                     TEXT     Path to YAML file defining the     │
+│                                           queries to simulate                │
+│                                           [default: None]                    │
+│ --num-queries                    INTEGER  Number of query inputs to render   │
+│                                           in the GUI                         │
+│                                           [default: None]                    │
+│ --restore        --no-restore             Restore previous inputs, ignores   │
+│                                           any query file specified.          │
+│                                           [default: no-restore]              │
+│ --help                                    Show this message and exit.        │
+╰──────────────────────────────────────────────────────────────────────────────╯
+```
+
+If you have run `sesasim setup` you can run this command with no options and it will use a default set of queries.
+If you have not run `setup` you must provide a query file and the `--restore` option will not work because your GUI inputs
+will not be saved.
+
+If you provide a query file _and_ specify `--num-queries n` only the first `n` queries of the query file will be used.
+If `n` is greater than the number of queries in the file, the last query will be repeated to reach `n`.
+
+#### GUI inputs: Global Settings
+
+ - Duration: the duration of the session to generate in milliseconds
+ - Sample Period: the average interval between samples in milliseconds
+ - Randomize Session: if unchecked, query times will not be randomized but instead will start with exactly the specified periodicity (unless another query is already running)
+ - Sampling Strategy: determines how the simulation decides when to take a sample.
+   - u == uniform: a sample will be taken every _P_ milliseconds exactly, where _P_ is the sample period.
+   - e == exponential: the probability of a given millisecond being sampled is 1/_P_ resulting in randomly spaced samples with an average period of _P_.
+   - j == jittered: like uniform, but with a random jitter applied to each sample. This means the sample period is uniformly distributed between 1 and 2*P*-1.
+   - phr == post-hoc random: a fraction 1/_P_ of all the milliseconds in the session are selected to be sampled. This is mathematically equivalent to exponential, but unlike the other methods, couldn't be applied to streaming data in real-time as you need to have the whole session.
+ - Use Calculated Sample Weights: if checked, the weight of each sample will be calculated as half the elapsed time since the previous sample, plus half the time until the next one. If not checked each sample is equally weighted.
+
+#### GUI inputs: Queries
+
+For each query, you can specify the following:
+
+ - Mean Duration: the mean duration of that query in milliseconds
+ - Duration Spread: a measure of how much the duration varies, how this is used depends on _Duration Distribution_.
+ - Average Periodicity: the average interval between executions of the query in milliseconds. This interval is generated from an exponential distribution function.
+ - Wait-state Ratios: a ratio describing how much of the query's execution is spent in each wait state. For example `2:0:1` means that the query spends two thirds of its time in wait 1, none in wait 2 and one third it wait 3.
+ - Duration Distribution: can be set to 'uniform', 'lognormal' or 'exponential'. 
+   - If set to 'uniform' the duration of each execution is drawn from a uniform distribution of width 2 x _Duration Spread_ around _Average Periodicity_.
+   - If set to 'uniform' the duration of each execution is drawn from a lognormal distribution with mean _Average Periodicity and standard deviation _Duration Spread_.
+   - If set to 'exponential', it is drawn from an exponential distribution of mean _Average Periodicity_.
---
+++ packages/session-sampling-simulator/session_sampling_simulator-0.2.1.tar.gz/session_sampling_simulator-0.2.1/session_sampling_simulator/gui.py
@@ -0,0 +1,307 @@
+import json
+import pickle
+import tkinter as tk
+from pathlib import Path
+from tkinter import ttk
+
+from session_sampling_simulator.session_simulator import (
+    Query,
+    compare_true_and_resampled,
+    generate_session,
+)
+import logging
+from session_sampling_simulator.analysis.total_duration import (
+    analyze_many_and_chart,
+    get_sample_period_range,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+def add_header(root: tk.Tk, text: str, row: int, col: int) -> None:
+    ttk.Label(root, text=text, font=("Helvetica", 14, "bold")).grid(
+        row=row, column=col, padx=5, pady=5, sticky="e"
+    )
+
+
+def add_labeled_field(
+    root: tk.Tk, label: str, var, row: int, col: int, checkbox: bool = False
+):
+    ttk.Label(root, text=label).grid(row=row, column=col, padx=5, pady=5, sticky="e")
+    if checkbox:
+        ttk.Checkbutton(root, variable=var).grid(
+            row=row, column=col + 1, padx=5, pady=5
+        )
+    else:
+        ttk.Entry(root, textvariable=var).grid(row=row, column=col + 1, padx=5, pady=5)
+
+
+def make_dict_from_ratios(ratios: str) -> dict[int, int]:
+    raw_ratios = [int(x) for x in ratios.split(":")]
+    return {w + 1: r for w, r in enumerate(raw_ratios)}
+
+
+def make_ratios_from_dict(d: dict[int:int]) -> str:
+    # note this won't preserve the keys, just their order
+    return ":".join([str(v) for k, v in sorted(d.items(), key=lambda x: x[0])])
+
+
+def extract_queries_from_vars(tk_vars: dict) -> list[Query]:
+    return [
+        Query(
+            id=int(k[6:]),
+            mean_duration=v["Mean Duration (ms)"].get(),
+            duration_spread=v["Duration Spread (ms)"].get(),
+            target_periodicity=v["Av. Periodicity (ms)"].get(),
+            wait_state_ratios=make_dict_from_ratios(v["Wait-state Ratios"].get()),
+            duration_distribution=v["Duration Distribution"].get(),
+        )
+        for k, v in tk_vars.items()
+        if k[:5] == "Query" and v["Av. Periodicity (ms)"].get() > 0
+    ]
+
+
+def dump_gui_inputs(
+    tk_vars: dict, path: Path, queries: list[Query] | None = None
+) -> None:
+    with open(path / "settings.json", "w") as f:
+        # noinspection PyTypeChecker
+        json.dump({k: v.get() for k, v in tk_vars["Global settings"].items()}, f)
+
+    if not queries:
+        queries = extract_queries_from_vars(tk_vars)
+
+    with open(path / "queries.pickle", "wb") as f:
+        # noinspection PyTypeChecker
+        pickle.dump(queries, f)
+
+
+def load_gui_inputs(path: Path) -> tuple[dict, list[Query]]:
+    with open(path / "settings.json", "r") as f:
+        # noinspection PyTypeChecker
+        global_settings = json.load(f)
+
+    with open(path / "queries.pickle", "rb") as f:
+        # noinspection PyTypeChecker
+        queries = pickle.load(f)
+
+    return global_settings, queries
+
+
+def on_calculate_button_click(
+    tk_vars: dict, result_text: tk.Text, save_dir: Path | None = None
+) -> None:
+    queries = extract_queries_from_vars(tk_vars)
+
+    if save_dir:
+        dump_gui_inputs(tk_vars, save_dir, queries)
+
+    result = compare_true_and_resampled(
+        session=generate_session(
+            window_duration=tk_vars["Global settings"]["Duration (ms)"].get(),
+            queries=queries,
+            random_starts=tk_vars["Global settings"]["Randomize Session"].get(),
+        ),
+        sample_period=tk_vars["Global settings"]["Sample Period (ms)"].get(),
+        sampling_strategy=tk_vars["Global settings"]["Sampling Strategy"].get(),
+        use_calculated_sample_weights=tk_vars["Global settings"][
+            "Use Calculated Sample Weights"
+        ].get(),
+    )
+
+    result_text.config(state=tk.NORMAL)
+    result_text.delete("1.0", tk.END)
+    result_text.insert(tk.END, str(result))
+    result_text.config(state=tk.DISABLED)
+
+
+def init_input_fields(
+    widget: tk.Tk, field_definitions: dict, section_height: int
+) -> dict:
+    row = 0
+    col = 0
+
+    tk_vars = {}
+
+    # Iterate over the input template and create a labelled input field for each item
+    for group, fields in field_definitions.items():
+        tk_vars[group] = {}
+        add_header(widget, group, row, col)
+        row += 1
+
+        for label, default, var_class in fields:
+            tk_vars[group][label] = var_class(value=default)
+            add_labeled_field(
+                widget,
+                label,
+                tk_vars[group][label],
+                row,
+                col,
+                checkbox=(var_class == tk.BooleanVar),
+            )
+            row += 1
+        # this doesn't actually enforce section_height rigidly, it always lets the group stay together
+        if row > section_height:
+            row = 0
+            col += 3
+
+    return tk_vars
+
+
+def generate_query_field_defs(queries: list[Query]) -> dict:
+    return {
+        f"Query {q.id}": [
+            ("Mean Duration (ms)", q.mean_duration, tk.IntVar),
+            ("Duration Spread (ms)", q.duration_spread, tk.IntVar),
+            ("Av. Periodicity (ms)", q.target_periodicity, tk.IntVar),
+            (
+                "Wait-state Ratios",
+                make_ratios_from_dict(q.wait_state_ratios),
+                tk.StringVar,
+            ),
+            ("Duration Distribution", q.duration_distribution, tk.StringVar),
+        ]
+        for q in queries
+    }
+
+
+def on_analyze_button_click(tk_vars: dict, save_dir: Path | None = None) -> None:
+    queries = extract_queries_from_vars(tk_vars)
+
+    if save_dir:
+        dump_gui_inputs(tk_vars, save_dir, queries)
+
+    sample_periods = get_sample_period_range(
+        max_sample_period=tk_vars["Global settings"]["Max Sampling Period (ms)"].get(),
+        min_sample_period=tk_vars["Global settings"]["Min Sampling Period (ms)"].get(),
+        num_steps=tk_vars["Global settings"]["Number of Steps"].get(),
+    )
+
+    # Save these in the hope of finding some way to pass them outside of tk scope and make uv python work :(
+    analyze_params = {
+        "queries": queries,
+        "durations": [tk_vars["Global settings"]["Duration (ms)"].get()],
+        "sampling_strategy": tk_vars["Global settings"]["Sampling Strategy"].get(),
+        "show_calculated": tk_vars["Global settings"]["Show calculated error"].get(),
+        "target_error": tk_vars["Global settings"]["Target error (%)"].get(),
+        "sample_periods": sample_periods,
+        "num_sessions": tk_vars["Global settings"]["Sessions Per Step"].get(),
+        "use_calculated_sample_weights": tk_vars["Global settings"][
+            "Use Calculated Sample Weights"
+        ].get(),
+    }
+
+    analyze_many_and_chart(**analyze_params)
+
+
+def run_single_session_gui(
+    initial_queries: list[Query] | None = None,
+    save_dir: Path | None = None,
+    restore: bool = False,
+) -> None:
+    if not initial_queries and not (restore and save_dir):
+        raise RuntimeError(
+            "You must provide either some initial queries or set restore and specify a save path"
+        )
+
+    input_section_height = 7
+
+    root = tk.Tk()
+    root.title("Sampling Simulator")
+
+    default_global_settings = [
+        ("Duration (ms)", 3600000, tk.IntVar),
+        ("Sample Period (ms)", 1000, tk.IntVar),
+        ("Randomize Session", True, tk.BooleanVar),
+        ("Sampling Strategy", "u", tk.StringVar),
+        ("Use Calculated Sample Weights", False, tk.BooleanVar),
+    ]
+
+    if restore:
+        restored_gs, restored_queries = load_gui_inputs(save_dir)
+        if not initial_queries:
+            initial_queries = restored_queries
+        global_settings = [
+            (x[0], restored_gs.get(x[0], x[1]), x[2]) for x in default_global_settings
+        ]
+    else:
+        global_settings = default_global_settings
+
+    input_template = {
+        "Global settings": global_settings,
+    } | generate_query_field_defs(initial_queries)
+
+    tk_vars = init_input_fields(
+        widget=root,
+        field_definitions=input_template,
+        section_height=input_section_height,
+    )
+
+    calculate_button = ttk.Button(
+        root,
+        text="Calculate",
+        command=lambda: on_calculate_button_click(tk_vars, result_text, save_dir),
+    )
+    calculate_button.grid(
+        row=input_section_height + 5, column=0, columnspan=10, pady=10
+    )
+
+    result_text = tk.Text(
+        root, height=32, width=150, state=tk.DISABLED, font=("Courier", 16)
+    )
+    result_text.grid(row=input_section_height + 6, column=0, columnspan=10, pady=10)
+
+    root.mainloop()
+
+
+# todo: consider factoring out GUI into a class to avoid repetition
+def run_analyzer_gui(
+    initial_queries: list[Query] | None, save_dir: Path | None, restore: bool = False
+) -> None:
+    input_section_height = 7
+
+    root = tk.Tk()
+    root.title("Sampling Analyzer")
+    root.tk.call("tk", "scaling", 2.0)
+
+    default_global_settings = [
+        ("Duration (ms)", 3600000, tk.IntVar),
+        ("Sampling Strategy", "u", tk.StringVar),
+        ("Use Calculated Sample Weights", False, tk.BooleanVar),
+        ("Max Sampling Period (ms)", 2000, tk.IntVar),
+        ("Min Sampling Period (ms)", 200, tk.IntVar),
+        ("Number of Steps", 10, tk.IntVar),
+        ("Sessions Per Step", 10, tk.IntVar),
+        ("Target error (%)", 10, tk.IntVar),
+        ("Show calculated error", False, tk.BooleanVar),
+    ]
+
+    if restore:
+        restored_gs, restored_queries = load_gui_inputs(save_dir)
+        if not initial_queries:
+            initial_queries = restored_queries
+        global_settings = [
+            (x[0], restored_gs.get(x[0], x[1]), x[2]) for x in default_global_settings
+        ]
+    else:
+        global_settings = default_global_settings
+
+    input_template = {
+        "Global settings": global_settings,
+    } | generate_query_field_defs(initial_queries)
+
+    tk_vars = init_input_fields(
+        widget=root,
+        field_definitions=input_template,
+        section_height=input_section_height,
+    )
+
+    analyze_button = ttk.Button(
+        root,
+        text="Analyze",
+        command=lambda: on_analyze_button_click(tk_vars, save_dir),
+    )
+    analyze_button.grid(row=input_section_height + 5, column=0, columnspan=10, pady=10)
+
+    root.mainloop()
