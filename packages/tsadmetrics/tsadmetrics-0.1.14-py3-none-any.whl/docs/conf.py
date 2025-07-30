b6d88f9068f61f880cece7a65777b338625b5ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:39 +0000

    Add package tsadmetrics/0.1.14/tsadmetrics-0.1.14-py3-none-any.whl

---
+++ packages/tsadmetrics/tsadmetrics-0.1.14-py3-none-any.whl/docs/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+
+project = 'TSADmetrics'
+copyright = '2025, Pedro Rafael Velasco Priego'
+author = 'Pedro Rafael Velasco Priego'
+release = 'MIT'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+epub_show_urls = 'footnote'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
