c653402c44e4627835fe4f6325fd3649aad04fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:15 +0000

    Add package opennote/0.1.0/opennote-0.1.0-py3-none-any.whl

---
+++ packages/opennote/opennote-0.1.0-py3-none-any.whl/opennote/sync_sdk.py
@@ -0,0 +1,46 @@
+import requests
+from opennote.api_types import *
+
+
+class Video:
+    def __init__(self, client):
+        self._client = client
+
+    def make(self, 
+        sections: int = 5, 
+        model: MODEL_CHOICES = "feynman2", 
+        messages: list[dict[str, str]] = [], 
+        script: list[str] = [],
+    ) -> VideoCreateResponse:
+        headers = {
+            "Authorization": f"Bearer {self._client._api_key}",
+            "Content-Type": "application/json",
+        }
+        url = f"{self._client._base_url}/video/create"
+        payload = {
+            "sections": sections,
+            "model": model,
+            "messages": messages,
+            "script": script,
+        }
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+        return VideoCreateResponse(**response.json())
+    
+
+    def status(self, video_id: str) -> VideoStatusAPIResponse:
+        url = f"{self._client._base_url}/video/{video_id}"
+        headers = {
+            "Content-Type": "application/json",
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return VideoStatusAPIResponse(**response.json())
+
+
+
+class OpennoteVideoClient:
+    def __init__(self, api_key: str, base_url: str | None = None):
+        self._api_key = api_key
+        self._base_url = base_url or OPENNOTE_BASE_URL
+        self.video = Video(self)
