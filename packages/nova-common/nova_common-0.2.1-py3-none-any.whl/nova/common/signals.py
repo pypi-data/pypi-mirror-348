4beaaf3ad2b8525b51afef8f8ef2aa1c668a6a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:19 +0000

    Add package nova-common/0.2.1/nova_common-0.2.1-py3-none-any.whl

---
+++ packages/nova-common/nova_common-0.2.1-py3-none-any.whl/nova/common/signals.py
@@ -0,0 +1,25 @@
+"""Signal related classes/enums."""
+
+from enum import Enum
+
+
+class Signal(str, Enum):
+    """Available signal prefixes."""
+
+    PROGRESS = "progress"
+    OUTPUTS = "outputs"
+    TOOL_COMMAND = "tool_command"
+    ERROR_MESSAGE = "error_message"
+
+
+def get_signal_id(id: str, signal: Signal) -> str:
+    return f"{id}_{signal}"
+
+
+class ToolCommand(str, Enum):
+    """Available commands for tools."""
+
+    STOP = "stop"
+    START = "start"
+    CANCEL = "cancel"
+    GET_RESULTS = "get_results"
