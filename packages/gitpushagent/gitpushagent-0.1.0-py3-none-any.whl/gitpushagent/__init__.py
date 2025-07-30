4b6f51286684dabce095ddf351c9c27958964c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:18 +0000

    Add package gitpushagent/0.1.0/gitpushagent-0.1.0-py3-none-any.whl

---
+++ packages/gitpushagent/gitpushagent-0.1.0-py3-none-any.whl/gitpushagent/__init__.py
@@ -0,0 +1,7 @@
+from .git_operations import push, GitPushError
+
+__version__ = "0.1.0"
+__author__ = "Alapati Sree Harsha"
+__author_email__ = "sreeharsha.alapati@gmail.com"
+__url__ = "https://github.com/AlapatiSreeHarsha/gitpushagent"
+__all__ = ["push", "GitPushError"] 
