956397fc0db0a61de9b507c649fcdd9ca2737d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:36 +0000

    Add package freeai-utils/0.2.8/freeai_utils-0.2.8-py3-none-any.whl

---
+++ packages/freeai-utils/freeai_utils-0.2.8-py3-none-any.whl/freeai_utils/text_to_speech_pyttsx3.py
@@ -0,0 +1,39 @@
+import pyttsx3 #need pip install pyttsx3
+import traceback
+
+class Text_To_Speech_Pyttsx3:
+    def __init__(self, rate=170, volume=1.0, voice_index=0) -> None:
+        self._engine = pyttsx3.init()
+        self.config_voice(rate, volume, voice_index)
+
+    def config_voice(self, rate: float = 170, volume: float = 1.0, voice_index: int = 0) -> None:
+        if not isinstance(rate, (int, float)):
+            raise TypeError("Rate must be a number.")
+        if not (0.0 <= volume <= 1.0):
+            raise ValueError("Volume must be between 0.0 and 1.0.")
+        
+        voices = self._engine.getProperty('voices')
+        if not isinstance(voice_index, int) or not (0 <= voice_index < len(voices)):
+            raise ValueError(f"Voice index must be between 0 and {len(voices)-1}")
+        
+        self._engine.setProperty('rate', rate)
+        self._engine.setProperty('volume', volume)
+        self._engine.setProperty('voice', voices[voice_index].id)
+
+    def speak(self, text: str) -> None:
+        if not isinstance(text, str):
+            raise ValueError(f"The provided text is not string!")
+        self._engine.say((text))
+        self._engine.runAndWait()
+
+    def stop(self) -> None:
+        self._engine.stop()
+        
+if __name__ == "__main__":
+    _speaking = Text_To_Speech_Pyttsx3()
+    _speaking.speak("Hello there my friend")
+    _speaking.config_voice(165, 0.8) #the first number is for rate, second is the volume, and last is the voice_id
+    try:
+        _speaking.speak(56) #this would raise valueError, dont worry, the code is fine :)
+    except ValueError as e:
+        traceback.print_exc() 
---
+++ packages/freeai-utils/freeai_utils-0.2.8-py3-none-any.whl/freeai_utils/wrapper.py
@@ -0,0 +1,25 @@
+import time
+from functools import wraps
+
+""" Copy part """
+
+#  A decorator that measures and prints the execution time of a function.
+def time_it(func):
+    @wraps(func) 
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {execution_time:.4f} seconds.")
+        return result
+    return wrapper
+
+""" Copy part """
+
+#how to use
+if __name__== "__main__":
+    @time_it
+    def say_hello():
+        print("Hello world!")  
+    say_hello()
