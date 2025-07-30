e7e3a882821728c480b7cdcc6089c9b142e7c2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:28 +0000

    Add package sunandmoonpdf/1.0.3/sunandmoonpdf-1.0.3-py3-none-any.whl

---
+++ packages/sunandmoonpdf/sunandmoonpdf-1.0.3-py3-none-any.whl/sunandmoonpdf/pdf2text.py
@@ -0,0 +1,20 @@
+"""
+This module represents a multitude of functions designated to convert PDF files into text.
+"""
+
+
+class Converter:
+    """
+    A simple converter to convert PDFs to text
+    """
+
+    def convert(self, path):
+        """
+        This function will convert the given PDF to a text
+
+        Parameters: path (str): The path to a pdf file
+
+        Returns: str: The content of the PDF file as text
+        """
+        print("Converting PDF to text...")
+        return path
