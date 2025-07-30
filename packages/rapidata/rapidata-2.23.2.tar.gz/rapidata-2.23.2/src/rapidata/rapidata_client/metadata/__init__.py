9ed0821db1b1cd2cdfbd43c0982004aae260bdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:40 +0000

    Add package rapidata/2.23.2/rapidata-2.23.2.tar.gz

---
+++ packages/rapidata/rapidata-2.23.2.tar.gz/rapidata-2.23.2/src/rapidata/rapidata_client/metadata/__init__.py
@@ -0,0 +1,5 @@
+from ._base_metadata import Metadata
+from ._private_text_metadata import PrivateTextMetadata
+from ._public_text_metadata import PublicTextMetadata
+from ._prompt_metadata import PromptMetadata
+from ._select_words_metadata import SelectWordsMetadata
