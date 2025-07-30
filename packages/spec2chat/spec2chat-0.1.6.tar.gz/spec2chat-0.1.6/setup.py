29a8fce7edb3d536e4e42e984a6bec9a3c283e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:24 +0000

    Add package spec2chat/0.1.6/spec2chat-0.1.6.tar.gz

---
+++ packages/spec2chat/spec2chat-0.1.6.tar.gz/spec2chat-0.1.6/setup.py
@@ -0,0 +1,42 @@
+"""
+spec2chat - A Python library for building task-oriented conversational systems from OpenAPI service specifications.
+
+Author: M. Jesús Rodríguez
+License: Apache 2.0 License
+Version: 0.1.6
+Repository: https://github.com/mjesusrodriguez/spec2chat
+Created on 17/05/2025 by M. Jesús Rodríguez
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="spec2chat",
+    version="0.1.6",
+    author="María Jesús Rodríguez Sánchez",
+    author_email="mjesusrodriguez@ugr.es",
+    description="A Python library for generating task-oriented dialogue systems from service specifications (PPTalk).",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mjesusrodriguez/spec2chat",  # Replace with your real GitHub URL
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "openai>=0.27.0",
+        "pymongo>=4.0.0,<5.0.0",
+        "spacy==3.7.2",
+        "nltk>=3.7",
+        "python-dotenv>=0.21.0",
+        "dnspython",
+        "setuptools>=65.0.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    license="Apache-2.0",
+    python_requires='>=3.8',
+)
