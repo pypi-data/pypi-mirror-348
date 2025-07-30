053faa3f10bcaf87d1898ca2fdf2bdbda9603d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:08 +0000

    Add package kanmanipriya-ml-lab/0.1/kanmanipriya_ml_lab-0.1-py3-none-any.whl

---
+++ packages/kanmanipriya-ml-lab/kanmanipriya_ml_lab-0.1-py3-none-any.whl/kanmanipriya_ml_lab/ex1.py
@@ -0,0 +1,27 @@
+# exercise1.py
+import inspect
+import sys
+
+print("=== Code of samu_ml_lab.exercise1 ===")
+print(inspect.getsource(sys.modules[__name__]))
+
+def run():
+    print("Running Decision Tree on Iris Dataset...")
+
+    from sklearn.datasets import load_iris
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = DecisionTreeClassifier()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {accuracy * 100:.2f}%")
