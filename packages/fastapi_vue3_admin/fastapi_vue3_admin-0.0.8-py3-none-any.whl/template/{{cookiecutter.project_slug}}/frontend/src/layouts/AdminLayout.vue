bd6a2108a6324a785ad5e46474a6a4223f7fd593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:35 +0000

    Add package fastapi_vue3_admin/0.0.8/fastapi_vue3_admin-0.0.8-py3-none-any.whl

---
+++ packages/fastapi_vue3_admin/fastapi_vue3_admin-0.0.8-py3-none-any.whl/template/{{cookiecutter.project_slug}}/frontend/src/layouts/AdminLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app permanent>
+      <v-list>
+        <v-list-item-title class="text-h6 pa-4">Admin</v-list-item-title>
+        <v-list-item link to="/">
+          <v-list-item-title>Dashboard</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/staff/users">
+          <v-list-item-title>Users</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template> 
