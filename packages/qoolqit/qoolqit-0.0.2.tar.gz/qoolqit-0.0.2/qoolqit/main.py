e7ee619428dc7244cf930e228a82eecb6bcdfa24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:12:03 +0000

    Add package qoolqit/0.0.2/qoolqit-0.0.2.tar.gz

---
+++ packages/qoolqit/qoolqit-0.0.2.tar.gz/qoolqit-0.0.2/qoolqit/main.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+
+def hello(name: str) -> str:
+    """
+    Returns a greeting message.
+
+    Arguments:
+        name: The name to greet
+
+    Returns
+        The greeting message.
+    """
+    return "Hello " + name
