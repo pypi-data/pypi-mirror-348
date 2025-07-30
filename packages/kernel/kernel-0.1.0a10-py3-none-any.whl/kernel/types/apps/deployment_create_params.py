c4d4bfa3cb3f7240082e7151ab7d2194884337fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:44 +0000

    Add package kernel/0.1.0a10/kernel-0.1.0a10-py3-none-any.whl

---
+++ packages/kernel/kernel-0.1.0a10-py3-none-any.whl/kernel/types/apps/deployment_create_params.py
@@ -0,0 +1,26 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, Required, TypedDict
+
+from ..._types import FileTypes
+
+__all__ = ["DeploymentCreateParams"]
+
+
+class DeploymentCreateParams(TypedDict, total=False):
+    entrypoint_rel_path: Required[str]
+    """Relative path to the entrypoint of the application"""
+
+    file: Required[FileTypes]
+    """ZIP file containing the application source directory"""
+
+    force: bool
+    """Allow overwriting an existing app version"""
+
+    region: Literal["aws.us-east-1a"]
+    """Region for deployment. Currently we only support "aws.us-east-1a" """
+
+    version: str
+    """Version of the application. Can be any string."""
