0768a1a7ba944020c87d60157e9142b1e87782f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:47 +0000

    Add package ddeutil-workflow/0.0.67/ddeutil_workflow-0.0.67-py3-none-any.whl

---
+++ packages/ddeutil-workflow/ddeutil_workflow-0.0.67-py3-none-any.whl/ddeutil/workflow/api/routes/__init__.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2022 Korawich Anuttra. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for
+# license information.
+# ------------------------------------------------------------------------------
+from .job import job_route as job
+from .logs import log_route as log
+from .workflows import workflow_route as workflow
