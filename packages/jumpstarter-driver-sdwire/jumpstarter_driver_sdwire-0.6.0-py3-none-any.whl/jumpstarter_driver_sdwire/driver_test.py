acc13a32efdc8ba1e539c585da6dca9a0081ea37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:23:12 +0000

    Add package jumpstarter-driver-sdwire/0.6.0/jumpstarter_driver_sdwire-0.6.0-py3-none-any.whl

---
+++ packages/jumpstarter-driver-sdwire/jumpstarter_driver_sdwire-0.6.0-py3-none-any.whl/jumpstarter_driver_sdwire/driver_test.py
@@ -0,0 +1,23 @@
+import pytest
+import usb
+
+from jumpstarter_driver_sdwire.driver import SDWire
+
+from jumpstarter.common.utils import serve
+
+
+def test_drivers_sdwire():
+    try:
+        instance = SDWire()
+    except FileNotFoundError:
+        pytest.skip("sd-wire not available")
+    except usb.core.USBError:
+        pytest.skip("USB not available")
+    except usb.core.NoBackendError:
+        pytest.skip("No USB backend")
+
+    with serve(instance) as client:
+        client.host()
+        assert instance.query() == "host"
+        client.dut()
+        assert instance.query() == "dut"
