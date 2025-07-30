407883272ea532217d1985443997e18efcf6d185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:23 +0000

    Add package pipfixer/1.0.1/pipfixer-1.0.1.tar.gz

---
+++ packages/pipfixer/pipfixer-1.0.1.tar.gz/pipfixer-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pipfixer",
+    version="1.0.1",  # versiyonu arttır
+    description="Auto-installs missing Python imports from a script",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="PozStudio",
+    author_email="you@example.com",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pipfixer=cli:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
