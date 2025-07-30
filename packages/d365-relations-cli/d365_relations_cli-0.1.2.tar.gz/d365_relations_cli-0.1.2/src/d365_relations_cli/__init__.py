c629e359437d60c8521c28ae557186c77a118355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:09:50 +0000

    Add package d365-relations-cli/0.1.2/d365_relations_cli-0.1.2.tar.gz

---
+++ packages/d365-relations-cli/d365_relations_cli-0.1.2.tar.gz/d365_relations_cli-0.1.2/src/d365_relations_cli/__init__.py
@@ -0,0 +1,12 @@
+"""
+Dynamics 365 Table Relationship Finder
+
+A package for finding relationships between Dynamics 365 tables.
+"""
+
+__version__ = '0.1.0'
+
+from .table_relationships import AdvancedTableRelationshipFinder
+from .optimize_json import optimize_relationship_file
+
+__all__ = ['AdvancedTableRelationshipFinder', 'optimize_relationship_file'] 
