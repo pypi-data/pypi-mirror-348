0b7d326e0bac2547a3901e2ee584f3a0009f21b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:06 +0000

    Add package SpatioloJI/0.1.1/spatioloji-0.1.1.tar.gz

---
+++ packages/SpatioloJI/spatioloji-0.1.1.tar.gz/spatioloji-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='SpatioloJI',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    author='Ji Wang',
+    author_email='gynecoloji@gmail.com',
+    description='A spatial transcriptomics toolkit',
+    url='https://github.com/gynecoloji/SpatioloJI',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
