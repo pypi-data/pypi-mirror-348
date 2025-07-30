9e4ebfdc5523bca730ac857eaf3857bc3c43785f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:33 +0000

    Add package ideal6/1.0.2/ideal6-1.0.2.tar.gz

---
+++ packages/ideal6/ideal6-1.0.2.tar.gz/ideal6-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ideal6",
+    version="1.0.2",
+    description="Multi-layer AES encryption library with HMAC integrity verification",
+    long_description="Ideal Encryption is a secure, multi-layer encryption library that uses AES-CBC with HMAC-SHA256 to provide confidentiality and integrity for sensitive data.",
+    author="GroupIDEAL",
+    author_email="idealencryption@gmail.com",
+    packages=find_packages(include=["bitcrypt", "bitcrypt.", "tests", "tests."]),
+    install_requires=[
+        "pycryptodome"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Security :: Cryptography",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers"
+    ],
+    python_requires=">=3.6",
+)
