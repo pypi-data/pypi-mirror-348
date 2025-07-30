00abc35d91b2fb9f1f8c294454c53cb110277f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:23 +0000

    Add package tea-chainquery/0.1.0/tea_chainquery-0.1.0.tar.gz

---
+++ packages/tea-chainquery/tea_chainquery-0.1.0.tar.gz/tea_chainquery-0.1.0/README.md
@@ -0,0 +1,27 @@
+# Sonia
+A CLI tool for querying blockchain data (Ethereum/Base).
+
+## Installation
+```bash
+pip install sonia
+```
+
+# Usage
+```bash
+sonia balance 0xYourAddress
+sonia tx_count 0xYourAddress
+```
+
+# Contributing
+
+Fork the repo, submit PRS to
+https://github.com/idyweb/sonia
+
+- Commit and push:
+```bash
+git init
+git add .
+git commit -m "Initial Sonia release"
+git remote add origin https://github.com/idyweb/sonia.git
+git push -u origin main
+```
---
+++ packages/tea-chainquery/tea_chainquery-0.1.0.tar.gz/tea_chainquery-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tea-chainquery",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["web3>=5.0.0", "click>=8.0.0", "requests>=2.0.0"],
+    entry_points={"console_scripts": ["sonia=sonia.cli:cli"]},
+    author="Idongesit Inyang",
+    author_email="inyangidongesit22@gmail.com",
+    description="Sonia: A CLI tool for querying blockchain data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/idyweb/sonia",
+    project_urls={"Source": "https://github.com/idyweb/sonia"},
+    license="MIT",
+)
