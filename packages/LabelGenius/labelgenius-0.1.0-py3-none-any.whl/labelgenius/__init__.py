30176ca6bb6d867cfbb10adaa455bdc2cd54ce5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:08:15 +0000

    Add package LabelGenius/0.1.0/labelgenius-0.1.0-py3-none-any.whl

---
+++ packages/LabelGenius/labelgenius-0.1.0-py3-none-any.whl/labelgenius/__init__.py
@@ -0,0 +1,11 @@
+
+from .LabelGenius import (
+    classification_CLIP_0_shot,
+    classification_CLIP_finetuned,
+    finetune_CLIP,
+    auto_verification,
+    classification_GPT,
+    generate_GPT_finetune_jsonl,
+    finetune_GPT,
+    price_estimation,
+)
