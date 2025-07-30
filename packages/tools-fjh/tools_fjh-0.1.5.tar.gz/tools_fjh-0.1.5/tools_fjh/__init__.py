aff3d503fdac0d42a6dc0a8aec0eaf771972de29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package tools-fjh/0.1.5/tools_fjh-0.1.5.tar.gz

---
+++ packages/tools-fjh/tools_fjh-0.1.5.tar.gz/tools_fjh-0.1.5/tools_fjh/__init__.py
@@ -0,0 +1,3 @@
+from .tools_fjh import setTimeout, setInterval, timeIt, catch, catchAll, callLimit, retry
+
+__all__ = ["setTimeout", "setInterval", "timeIt", "catch", "catchAll", "callLimit", "retry"]
