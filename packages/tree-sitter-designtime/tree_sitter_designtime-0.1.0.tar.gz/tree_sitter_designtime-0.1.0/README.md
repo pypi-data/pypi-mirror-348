c2acf008ef27f23de98a87f2bb70fd6ebe0de239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:54 +0000

    Add package tree-sitter-designtime/0.1.0/tree_sitter_designtime-0.1.0.tar.gz

---
+++ packages/tree-sitter-designtime/tree_sitter_designtime-0.1.0.tar.gz/tree_sitter_designtime-0.1.0/README.md
@@ -0,0 +1,5 @@
+# Tree-sitter Design Time
+
+A Tree-sitter grammar for parsing DesignTime files.
+
+This a draft but can be used to parse DesignTime files.
---
+++ packages/tree-sitter-designtime/tree_sitter_designtime-0.1.0.tar.gz/tree_sitter_designtime-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from setuptools.command.build import build
+from os.path import dirname, join
+import subprocess
+
+class TreesitterBuild(build):
+    def run(self):
+        try:
+            subprocess.run(['tree-sitter', 'generate'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Warning: Failed to generate parser: {e}")
+        build.run(self)
+
+setup(
+    name="tree-sitter-designtime",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    author="Preston Arnold",
+    description="Tree-sitter grammar for the DesignTime language",
+    long_description=open(join(dirname(__file__), "README.md")).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/preston/tree-sitter-designtime",
+    include_package_data=True,
+    cmdclass={
+        'build': TreesitterBuild,
+    },
+)
