cf9ee37e531689c5a449a8d2a56b9de01b62d4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:12 +0000

    Add package polyglotdb/1.3.2/polyglotdb-1.3.2-py3-none-any.whl

---
+++ packages/polyglotdb/polyglotdb-1.3.2-py3-none-any.whl/tests/test_utils.py
@@ -0,0 +1,7 @@
+from polyglotdb.utils import get_corpora_list
+
+
+def test_corpora_list(acoustic_config):
+    corpora_list = get_corpora_list(acoustic_config)
+    assert 'acoustic' in corpora_list
+
