2cf23d61fdea5ce1fa01936662c6188b5d6406e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:28 +0000

    Add package ardhi-framework/0.1.0/ardhi_framework-0.1.0-py3-none-any.whl

---
+++ packages/ardhi-framework/ardhi_framework-0.1.0-py3-none-any.whl/ardhi_framework/req_res.py
@@ -0,0 +1,12 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+
+class ArdhiRequest(Request):
+    pass
+
+
+class ArdhiResponse(Response):
+    pass
+
+
