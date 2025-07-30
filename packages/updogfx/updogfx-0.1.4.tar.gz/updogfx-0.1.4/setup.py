4800027c19fde8f448ea2c296dc4377c35289de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:39 +0000

    Add package updogfx/0.1.4/updogfx-0.1.4.tar.gz

---
+++ packages/updogfx/updogfx-0.1.4.tar.gz/updogfx-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="updogfx",
+    version="0.1.4",
+    packages=find_packages(),
+    include_package_data=True,  # This enables inclusion of data files as per MANIFEST.in
+    install_requires=[
+        "Flask",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "updogfx = updogfx.app:main",
+        ],
+    },
+    package_data={
+        "updogfx": ["templates/*.html"],  # Include HTML templates
+    },
+)
