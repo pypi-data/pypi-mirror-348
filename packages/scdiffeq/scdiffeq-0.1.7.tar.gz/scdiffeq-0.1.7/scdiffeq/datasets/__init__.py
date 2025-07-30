d5a994e8f4dfe678a0ecb54a71d88bb983c2c591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:04 +0000

    Add package scdiffeq/0.1.7/scdiffeq-0.1.7.tar.gz

---
+++ packages/scdiffeq/scdiffeq-0.1.7.tar.gz/scdiffeq-0.1.7/scdiffeq/datasets/__init__.py
@@ -0,0 +1,5 @@
+from ._human_hematopoiesis import human_hematopoiesis
+from ._pancreatic_endocrinogenesis import pancreatic_endocrinogenesis
+from ._larry_in_vitro import larry
+
+__all__ = ["pancreatic_endocrinogenesis", "larry", "human_hematopoiesis"]
