94de97f8e408f6285bb65b433222bf8e58220b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:50 +0000

    Add package avocado-framework/111.0/avocado_framework-111.0.tar.gz

---
+++ packages/avocado-framework/avocado_framework-111.0.tar.gz/avocado_framework-111.0/selftests/unit/runner_asset.py
@@ -0,0 +1,86 @@
+import unittest
+from unittest.mock import patch
+
+from avocado.core.nrunner.runnable import Runnable
+from avocado.plugins.runners.asset import AssetRunner
+
+
+class BasicTests(unittest.TestCase):
+    """Basic unit tests for the AssetRunner class"""
+
+    def test_no_kwargs(self):
+        runnable = Runnable(kind="asset", uri=None)
+        runner = AssetRunner()
+        status = runner.run(runnable)
+        messages = []
+        while True:
+            try:
+                messages.append(next(status))
+            except StopIteration:
+                break
+        self.assertEqual(messages[-1]["result"], "error")
+        stderr = b"At least name should be passed as kwargs"
+        self.assertIn(stderr, messages[-2]["log"])
+
+    def test_wrong_name(self):
+        runnable = Runnable(kind="asset", uri=None, **{"name": "foo"})
+        runner = AssetRunner()
+        status = runner.run(runnable)
+        messages = []
+        while True:
+            try:
+                messages.append(next(status))
+            except StopIteration:
+                break
+        self.assertEqual(messages[-1]["result"], "error")
+        stderr = b"Failed to fetch foo ("
+        self.assertIn(stderr, messages[-2]["log"])
+
+
+class FetchTests(unittest.TestCase):
+    """Unit tests for the actions on RequirementPackageRunner class"""
+
+    def setUp(self):
+        """Mock SoftwareManager"""
+
+        self.asset_patcher = patch("avocado.plugins.runners.asset.Asset", autospec=True)
+        self.mock_asset = self.asset_patcher.start()
+        self.addCleanup(self.asset_patcher.stop)
+
+    def test_success_fetch(self):
+
+        self.mock_asset.return_value.fetch.return_value = "/tmp/asset.txt"
+        runnable = Runnable(kind="asset", uri=None, **{"name": "asset.txt"})
+        runner = AssetRunner()
+        status = runner.run(runnable)
+        messages = []
+        while True:
+            try:
+                messages.append(next(status))
+            except StopIteration:
+                break
+        self.assertEqual(messages[-1]["result"], "pass")
+        stdout = b"File fetched at /tmp/asset.txt"
+        self.assertIn(stdout, messages[-3]["log"])
+
+    def test_fail_fetch(self):
+
+        self.mock_asset.return_value.fetch = lambda: (_ for _ in ()).throw(
+            OSError("Failed to fetch asset.txt")
+        )
+        runnable = Runnable(kind="asset", uri=None, **{"name": "asset.txt"})
+        runner = AssetRunner()
+        status = runner.run(runnable)
+        messages = []
+        while True:
+            try:
+                messages.append(next(status))
+            except StopIteration:
+                break
+        self.assertEqual(messages[-1]["result"], "error")
+        stderr = b"Failed to fetch asset.txt"
+        self.assertIn(stderr, messages[-2]["log"])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/avocado-framework/avocado_framework-111.0.tar.gz/avocado_framework-111.0/selftests/unit/utils/network.py
@@ -0,0 +1,70 @@
+import unittest.mock
+
+from avocado.utils.network import ports
+
+try:
+    import netifaces
+
+    HAS_NETIFACES = True
+except ImportError:
+    HAS_NETIFACES = False
+
+
+class PortTrackerTest(unittest.TestCase):
+    def test_register_port(self):
+        tracker = ports.PortTracker()
+        ports.is_port_free = unittest.mock.MagicMock(return_value=True)
+        self.assertNotIn(22, tracker.retained_ports)
+        tracker.register_port(22)
+        ports.is_port_free.assert_called_once_with(22, tracker.address)
+        self.assertIn(22, tracker.retained_ports)
+
+    def test_release_port_does_not_poke_system(self):
+        tracker = ports.PortTracker()
+        tracker.release_port = unittest.mock.MagicMock()
+        ports.is_port_free = unittest.mock.MagicMock()
+        tracker.release_port(22)
+        tracker.release_port.assert_called_once_with(22)
+        ports.is_port_free.assert_not_called()
+
+    def test_release_port(self):
+        tracker = ports.PortTracker()
+        tracker.retained_ports = [22]
+        tracker.release_port(22)
+        self.assertNotIn(22, tracker.retained_ports)
+
+
+def get_all_local_addrs():
+    """
+    Returns all ipv4/ipv6 addresses that are associated with this machine
+    """
+    ipv4_addrs = []
+    ipv6_addrs = []
+    for interface in netifaces.interfaces():
+        ifaddresses = netifaces.ifaddresses(interface)
+        ipv4_addrs += [_["addr"] for _ in ifaddresses.get(netifaces.AF_INET, [])]
+        ipv6_addrs += [_["addr"] for _ in ifaddresses.get(netifaces.AF_INET6, [])]
+    if ipv4_addrs:
+        ipv4_addrs += ["localhost", ""]
+    if ipv6_addrs:
+        ipv6_addrs += ["localhost", ""]
+    return ipv4_addrs, ipv6_addrs
+
+
+class FreePort(unittest.TestCase):
+    def test_is_port_available(self):
+        port = ports.find_free_port(sequent=False)
+        result = ports.is_port_available(port, "localhost")
+        self.assertTrue(result)
+
+    def test_find_free_port(self):
+        port = ports.find_free_port(sequent=False)
+        self.assertEqual(type(port), int)
+
+    def test_find_free_ports(self):
+        port = ports.find_free_ports(1000, 2000, 10)
+        self.assertEqual(type(port), list)
+
+
+if __name__ == "__main__":
+    unittest.main()
