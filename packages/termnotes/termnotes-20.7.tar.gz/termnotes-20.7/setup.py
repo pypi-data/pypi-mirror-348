79aee64b8c0e25761c0444fe5f4b2a609c7a1843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package termnotes/20.7/termnotes-20.7.tar.gz

---
+++ packages/termnotes/termnotes-20.7.tar.gz/termnotes-20.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="termnotes",  # This is your package name
+  version="20.7",
+  packages=find_packages(),
+  include_package_data=True,
+  package_data={'termnotes': ['data.json']},
+  install_requires=[
+    "appdirs",
+    "gnureadline",
+    "pyperclip",
+    "rich",
+    "termcolor",  # Keep this if you actually use it in your code elsewhere
+  ],
+  entry_points={
+    'console_scripts': [
+    'tn=termnotes.main:run',  # 'termnotes' will call the `run` function from termnotes.main
+    ],
+  },
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
