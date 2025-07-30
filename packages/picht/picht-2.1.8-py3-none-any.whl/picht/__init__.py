4233f5bd6f0d42e04786747fe69921e3091b5f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:30 +0000

    Add package picht/2.1.8/picht-2.1.8-py3-none-any.whl

---
+++ packages/picht/picht-2.1.8-py3-none-any.whl/picht/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "2.1.3"
+
+from .core import (
+    ElectricField,
+    ParticleTracer,
+    ElectrodeConfig,
+    EinzelLens,
+    ElectronOptics,
+    MagneticField,
+    MagneticLensConfig,
+    Export,
+)
+
+__author__ = "Rishiit Sharma"
+__email__ = "rishiitsharma@gmail.com"
