d64c7aa8e7889507006eacde499683e5ad5924d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:03 +0000

    Add package pyvcon-async/0.1.0/pyvcon_async-0.1.0.tar.gz

---
+++ packages/pyvcon-async/pyvcon_async-0.1.0.tar.gz/pyvcon_async-0.1.0/README.md
@@ -0,0 +1,9 @@
+# pyvcon
+
+Minimal async RCON client for Pavlov VR and Source engine servers.
+
+## Installation
+
+Clone this repository and install locally:
+```bash
+pip install .
---
+++ packages/pyvcon-async/pyvcon_async-0.1.0.tar.gz/pyvcon_async-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyvcon-async",
+    version="0.1.0",
+    description="Minimal async RCON client for Pavlov VR and Source engine servers",
+    author="Your Name",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
