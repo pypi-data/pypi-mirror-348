87e51232505c4b0049eb6a6d1baf478d1b4e92a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:50 +0000

    Add package fintekkers-ledger-models/0.1.108/fintekkers_ledger_models-0.1.108-py3-none-any.whl

---
+++ packages/fintekkers-ledger-models/fintekkers_ledger_models-0.1.108-py3-none-any.whl/fintekkers/models/util/local_timestamp_pb2.pyi
@@ -0,0 +1,14 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class LocalTimestampProto(_message.Message):
+    __slots__ = ("timestamp", "time_zone")
+    TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
+    TIME_ZONE_FIELD_NUMBER: _ClassVar[int]
+    timestamp: _timestamp_pb2.Timestamp
+    time_zone: str
+    def __init__(self, timestamp: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., time_zone: _Optional[str] = ...) -> None: ...
