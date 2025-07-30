de864d8add641e402cba69e1db5b87640a5260aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:36:22 +0000

    Add package allianceauth-afat/3.8.2/allianceauth_afat-3.8.2-py3-none-any.whl

---
+++ packages/allianceauth-afat/allianceauth_afat-3.8.2-py3-none-any.whl/afat/auth_hooks.py
@@ -0,0 +1,66 @@
+"""
+Auth hooks
+"""
+
+# Alliance Auth
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+# Alliance Auth AFAT
+from afat import __title__, urls
+from afat.constants import APP_BASE_URL
+
+
+class AaAfatMenuItem(MenuItemHook):  # pylint: disable=too-few-public-methods
+    """
+    This class ensures only authorized users will see the menu entry
+    """
+
+    def __init__(self):
+        # Setup menu entry for sidebar
+        MenuItemHook.__init__(
+            self,
+            text=__title__,
+            classes="fa-solid fa-space-shuttle",
+            url_name="afat:dashboard",
+            navactive=["afat:"],
+        )
+
+    def render(self, request):
+        """
+        Only if the user has access to this app
+
+        :param request:
+        :type request:
+        :return:
+        :rtype:
+        """
+
+        if request.user.has_perm(perm="afat.basic_access"):
+            return MenuItemHook.render(self, request=request)
+
+        return ""
+
+
+@hooks.register(name="menu_item_hook")
+def register_menu():
+    """
+    Register our menu
+
+    :return:
+    :rtype:
+    """
+
+    return AaAfatMenuItem()
+
+
+@hooks.register(name="url_hook")
+def register_url():
+    """
+    Register our menu link
+
+    :return:
+    :rtype:
+    """
+
+    return UrlHook(urls=urls, namespace="afat", base_url=rf"^{APP_BASE_URL}/")
