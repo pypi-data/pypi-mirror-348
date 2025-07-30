bc272110f91bd550a645f35c8619b815e784a740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:11:23 +0000

    Add package pymammotion/0.4.47/pymammotion-0.4.47.tar.gz

---
+++ packages/pymammotion/pymammotion-0.4.47.tar.gz/pymammotion-0.4.47/pymammotion/data/model/device_info.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, field
+
+from mashumaro.mixins.orjson import DataClassORJSONMixin
+
+from pymammotion.data.model.mowing_modes import TraversalMode, TurningMode
+
+
+@dataclass
+class SideLight(DataClassORJSONMixin):
+    operate: int = 0
+    enable: int = 0
+    start_hour: int = 0
+    start_min: int = 0
+    end_hour: int = 0
+    end_min: int = 0
+    action: int = 0
+
+
+@dataclass
+class MowerInfo(DataClassORJSONMixin):
+    blade_status: bool = False
+    rain_detection: bool = False
+    traversal_mode: TraversalMode = TraversalMode.direct
+    turning_mode: TurningMode = TurningMode.zero_turn
+    side_led: SideLight = field(default_factory=SideLight)
+    collector_installation_status: bool = False
+    model: str = ""
+    swversion: str = ""
+    product_key: str = ""
+    model_id: str = ""
+    sub_model_id: str = ""
+
+
+@dataclass
+class DeviceFirmwares(DataClassORJSONMixin):
+    device_version: str = ""
+    left_motor_driver: str = ""
+    lora_version: str = ""
+    main_controller: str = ""
+    model_name: str = ""
+    right_motor_driver: str = ""
+    rtk_rover_station: str = ""
+    rtk_version: str = ""
