bb85e6d5e66fc2a0c08d2fb52dd57fa44cc5ddd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:08:00 +0000

    Add package celao/0.3/celao-0.3.tar.gz

---
+++ packages/celao/celao-0.3.tar.gz/celao-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CELAO",
+    version="0.3",
+    author="Good",
+    packages=find_packages(include=['celao', 'celao.*', 'lib', 'lib.*']),
+    description="Visualization tool for correlation n-ptychs of linguistic attributes",
+    url="https://github.com/Good03/celao/",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "networkx",
+        "gensim"
+    ]
+)
