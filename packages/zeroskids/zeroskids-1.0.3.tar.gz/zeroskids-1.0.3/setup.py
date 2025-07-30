433e274dd3bde93b187cfa4eb0ae65354608cc47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:07 +0000

    Add package zeroskids/1.0.3/zeroskids-1.0.3.tar.gz

---
+++ packages/zeroskids/zeroskids-1.0.3.tar.gz/zeroskids-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="zeroskids",
+    version="1.0.3",
+    author="SkitDev",
+    description="Truly skid-proof Python obfuscator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SkitDev/ZeroSkids",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[],
+)
