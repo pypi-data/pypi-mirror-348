e3c01d63213e446846149c4ee283a3812eb15258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:18 +0000

    Add package MetaRagTool/0.3.9.3/metaragtool-0.3.9.3.tar.gz

---
+++ packages/MetaRagTool/metaragtool-0.3.9.3.tar.gz/metaragtool-0.3.9.3/MetaRagTool/Constants.py
@@ -0,0 +1,25 @@
+﻿use_wandb = False
+local_mode = False
+trust_remote_code = True
+lang='multi'
+
+
+
+WandbToken = None
+HFToken = None
+GitHubToken = None
+
+API_KEY_GEMINI = None
+API_KEY_OPENAI = None
+API_KEY_OPENROUTER = None
+
+import nltk
+try:
+    nltk.data.find('tokenizers/punkt')
+except BaseException:
+    try:
+        nltk.download('punkt')
+    except BaseException as e:
+        print(f"Error downloading 'punkt' for nltk: {e}")
+
+
