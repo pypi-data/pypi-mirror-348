b5ed0c933c3cadb348d55568a29d0c3d06747c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:41 +0000

    Add package betterproto-fw/2.0.1/betterproto_fw-2.0.1.tar.gz

---
+++ packages/betterproto-fw/betterproto_fw-2.0.1.tar.gz/betterproto_fw-2.0.1/tests/inputs/bool/test_bool.py
@@ -0,0 +1,24 @@
+import pytest
+
+from tests.output_betterproto.bool import Test
+from tests.output_betterproto_pydantic.bool import Test as TestPyd
+
+
+def test_value():
+    message = Test()
+    assert not message.value, "Boolean is False by default"
+
+
+def test_pydantic_no_value():
+    message = TestPyd()
+    assert not message.value, "Boolean is False by default"
+
+
+def test_pydantic_value():
+    message = TestPyd(value=False)
+    assert not message.value
+
+
+def test_pydantic_bad_value():
+    with pytest.raises(ValueError):
+        TestPyd(value=123)
