121a989c424ad9edfc7322ec1e373d768d5f0ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:13 +0000

    Add package cmsgpack/0.1.2/cmsgpack-0.1.2-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/cmsgpack/cmsgpack-0.1.2-cp313-cp313-musllinux_1_2_x86_64.whl/cmsgpack/__init__.py
@@ -0,0 +1,5 @@
+__author__ = "Sven Boertjens"
+__license__ = "MIT License"
+__url__ = "https://github.com/svenboertjens/cmsgpack"
+
+from .cmsgpack import encode, decode, Extensions, extensions, Stream, FileStream
