7b9769d1d11b300ba22a8dab06908decac1deea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:35 +0000

    Add package shearwater/0.2/shearwater-0.2-py3-none-any.whl

---
+++ packages/shearwater/shearwater-0.2-py3-none-any.whl/shearwater/__init__.py
@@ -0,0 +1,40 @@
+#    This file is part of Shearwater.
+#    Copyright (C) 2025  Carine Dengler
+#
+#    Puffin is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+:synopsis:
+"""
+
+
+# standard library imports
+# third party imports
+# library specific imports
+
+
+METADATA = {
+    "name": "shearwater",
+    "version": "v0.2",
+    "description": "",
+    "author": "Carine Dengler",
+    "author_email": "",
+    "url": "",
+    "classifiers": [
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.13",
+    ],
+}
