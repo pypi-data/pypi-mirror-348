957c8ec66f7adf110ef112757f898d39da91f717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:22 +0000

    Add package django-classy-views/0.0.1/django_classy_views-0.0.1.tar.gz

---
+++ packages/django-classy-views/django_classy_views-0.0.1.tar.gz/django_classy_views-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-classy-views',
+    version='0.0.1',
+    author='Guangye Bai',
+    author_email='2253637@tongji.edu.cn',
+    description='An academic experiment package',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://pypi.org/project/redis-py/',
+    packages=find_packages(),
+    license='MIT',
+    keywords=['django', 'django-classy-views', 'experiment', 'supply chain', 'security'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Database :: Front-Ends'
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
