d26854feb29e746588272227557c0abe85dba678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:08 +0000

    Add package CircuitNotionAI/0.1.1/circuitnotionai-0.1.1.tar.gz

---
+++ packages/CircuitNotionAI/circuitnotionai-0.1.1.tar.gz/circuitnotionai-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CircuitNotionAI",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+)
