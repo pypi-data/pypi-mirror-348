e86268bfbca545674113b2c35a98e7001f19b2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:45 +0000

    Add package first-weekday-scheduler/0.1.2/first_weekday_scheduler-0.1.2.tar.gz

---
+++ packages/first-weekday-scheduler/first_weekday_scheduler-0.1.2.tar.gz/first_weekday_scheduler-0.1.2/first_weekday_scheduler/config.py
@@ -0,0 +1,7 @@
+# scheduler/config.py
+
+# Scheduler parameters
+FIRST_WEEKDAY = 0               # Monday
+FIRST_WEEK_DAY_THRESHOLD = 7    # Day <= 7 => First week
+AFTER_HOUR = 19                 # 7 PM or later
+TIMEZONE = "America/New_York"   # Configurable timezone
