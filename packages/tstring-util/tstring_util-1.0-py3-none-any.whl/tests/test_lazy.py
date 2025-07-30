d2c44dc17eed7da3655dab9701753897346ec924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:59 +0000

    Add package tstring-util/1.0/tstring_util-1.0-py3-none-any.whl

---
+++ packages/tstring-util/tstring_util-1.0-py3-none-any.whl/tests/test_lazy.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from tstring import render
+
+
+def hello(name):
+    print(f"hello {name}")
+
+def test_lazy():
+    who = 'bob'
+    flavor = 'spicy'
+    embedx = t'Call function {hello:!fn} {who} {flavor}'
+    who = 'jane'
+
+
+    r = render(embedx)
+    assert r ==  "Call function hello jane spicy"
+
