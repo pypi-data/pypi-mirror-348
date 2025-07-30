0960b7c29125d22bf8882ffc0e1080587087d6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:01 +0000

    Add package neverraise/0.1.1/neverraise-0.1.1.tar.gz

---
+++ packages/neverraise/neverraise-0.1.1.tar.gz/neverraise-0.1.1/noxfile.py
@@ -0,0 +1,48 @@
+import typing as t
+
+import nox
+
+
+def nox_session(
+    **kwargs: t.Any,
+) -> t.Callable[[t.Callable[[nox.Session], None]], t.Callable[[nox.Session], None]]:
+    kwargs.setdefault("venv_backend", "uv")
+    kwargs.setdefault("reuse_venv", True)
+
+    def inner(func: t.Callable[[nox.Session], None]) -> t.Callable[[nox.Session], None]:
+        return nox.session(**kwargs)(func)
+
+    return inner
+
+
+@nox_session()
+def test(session: nox.Session) -> None:
+    _ = session.run_install(
+        "uv",
+        "sync",
+        "--group=test",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    _ = session.run("python", "-m", "pytest", "--capture=no")
+
+
+@nox_session()
+def lint(session: nox.Session) -> None:
+    _ = session.run_install(
+        "uv",
+        "sync",
+        "--group=lint",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    _ = session.run("python", "-m", "ruff", "check", "--fix", "src", "tests")
+
+
+@nox_session()
+def typecheck(session: nox.Session) -> None:
+    _ = session.run_install(
+        "uv",
+        "sync",
+        "--group=typecheck",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    _ = session.run("python", "-m", "pyright", "src")
