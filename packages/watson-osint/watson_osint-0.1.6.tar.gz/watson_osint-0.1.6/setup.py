0d10878b849ea3ae94a204eb7fe40fb01537fd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:52 +0000

    Add package watson-osint/0.1.6/watson_osint-0.1.6.tar.gz

---
+++ packages/watson-osint/watson_osint-0.1.6.tar.gz/watson_osint-0.1.6/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="watson-osint",
+    version="0.1.6",
+    author="margoul1",
+    author_email="github.pydoctor@gmail.com",
+    description="Watson OSINT - A powerful tool to search for usernames on multiple platforms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/margoul1Malin/watson",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "colorama",
+        "beautifulsoup4",
+        "fake_useragent",
+        "dnspython",
+        "google-api-python-client",
+        "cloudscraper",
+        "urllib3",
+    ],
+    entry_points={
+        "console_scripts": [
+            "watson=watson.__main__:main",
+        ],
+    },
+)
