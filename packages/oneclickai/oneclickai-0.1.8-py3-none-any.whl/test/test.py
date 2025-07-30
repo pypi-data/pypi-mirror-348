443f3fe1fa8da0a5d28a05c4381e3d3c7dfdf7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:17 +0000

    Add package oneclickai/0.1.8/oneclickai-0.1.8-py3-none-any.whl

---
+++ packages/oneclickai/oneclickai-0.1.8-py3-none-any.whl/test/test.py
@@ -0,0 +1,11 @@
+
+
+class_id = 1
+class_names = ['person', 'bicycle']
+
+
+for i in range(10):
+    # check if class id is valid
+    if (class_id != None) and (class_id >= len(class_names)):
+        continue
+    print("gggg")
