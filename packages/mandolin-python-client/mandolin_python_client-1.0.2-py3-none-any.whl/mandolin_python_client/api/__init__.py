0880b52fc093fca651b68f504055203b28b86d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:24 +0000

    Add package mandolin-python-client/1.0.2/mandolin_python_client-1.0.2-py3-none-any.whl

---
+++ packages/mandolin-python-client/mandolin_python_client-1.0.2-py3-none-any.whl/mandolin_python_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from mandolin_python_client.api.analyzers_api import AnalyzersApi
+from mandolin_python_client.api.converters_api import ConvertersApi
+from mandolin_python_client.api.default_api import DefaultApi
+
