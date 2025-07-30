c3983af3e3ba02fa8019eb902d1e6608bd647c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:41 +0000

    Add package anyunit/0.1.0/anyunit-0.1.0-py3-none-any.whl

---
+++ packages/anyunit/anyunit-0.1.0-py3-none-any.whl/anyunit/__init__.py
@@ -0,0 +1,2 @@
+from .converter import convert_length, convert_weight
+__all__ = ['convert_length', 'convert_weight', 'convert_temperature']
---
+++ packages/anyunit/anyunit-0.1.0-py3-none-any.whl/anyunit/converter.py
@@ -0,0 +1,31 @@
+from .units import LENGTH, WEIGHT
+
+
+def convert_length(value : float, from_unit : str, to_unit : str) -> float:
+    """
+    Converts units of length.
+    Parameters:
+        value (float): Numeric value for conversion
+        from_unit (str): Reference unit of measure (e.g. 'mile')
+        to_unit (str): Target unit of measure (e.g. 'kilometer')
+    Returns:
+        float: Conversion result
+    Examples:
+        >>> convert_length(1, "mile", "kilometer")
+        1.609344
+        >>> convert_length(100, "meter", "foot")
+        328.084
+    """
+    if from_unit not in LENGTH:
+        raise ValueError(f"'{from_unit}' is unknown unit. Available: {", ".join(LENGTH.keys())}")
+    if to_unit not in LENGTH:
+        raise ValueError(f"{to_unit} is unknown unit. Available: {", ".join(LENGTH.keys())}")
+    return value * LENGTH[from_unit] / LENGTH[to_unit]
+
+
+def convert_weight(value: float, from_unit : str, to_unit : str) -> float:
+    if from_unit not in WEIGHT:
+        raise ValueError(f"'{from_unit}' is unknown unit. Available: {", ".join(WEIGHT.keys())}")
+    if to_unit not in WEIGHT:
+        raise ValueError(f"{to_unit} is unknown unit. Available: {", ".join(WEIGHT.keys())}")
+    return value * WEIGHT[from_unit] / WEIGHT[to_unit]
