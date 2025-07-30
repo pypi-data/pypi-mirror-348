626cfcfb0ba82adf9829d07f1a2d89668d39f430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:14 +0000

    Add package webdevaccess/0.1.0/webdevaccess-0.1.0-py3-none-any.whl

---
+++ packages/webdevaccess/webdevaccess-0.1.0-py3-none-any.whl/webdevaccess/__init__.py
@@ -0,0 +1,7 @@
+## src/webdevaccess/__init__.py 
+
+## import webdevaccess.htmlElements
+from webdevaccess import htmlElements 
+
+
+## end of file 
---
+++ packages/webdevaccess/webdevaccess-0.1.0-py3-none-any.whl/webdevaccess/htmlElements/__main__.py
@@ -0,0 +1,9 @@
+## src/webdevaccess/htmlElements/__main__.py 
+
+
+from webdevaccess.htmlElements import run
+
+if __name__ == "__main__":
+    run()
+
+## end of file 
