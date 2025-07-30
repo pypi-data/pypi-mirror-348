4db8883f26cc29e2eed9aacea69d07d7700ce181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:51 +0000

    Add package python-test-aviz/1.0.3/python_test_aviz-1.0.3-py3-none-any.whl

---
+++ packages/python-test-aviz/python_test_aviz-1.0.3-py3-none-any.whl/python_test_aviz/aviz_test.py
@@ -0,0 +1,26 @@
+import logging
+from custom_python_logger.logger import get_logger
+
+
+class AvizTest:
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.name = "Aviz"
+
+    def print_name(self):
+        self.logger.info(f"Test name: {self.name}")
+
+
+def main():
+    _ = get_logger(
+        project_name='Logger Project Test',
+        log_level=logging.DEBUG,
+        extra={'user': 'test_user'}
+    )
+
+    a = AvizTest()
+    a.print_name()
+
+
+if __name__ == '__main__':
+    main()
