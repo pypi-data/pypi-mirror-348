aee7d0f09bc91850966241e02b780d5089f2af01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:11:27 +0000

    Add package draive/0.64.0/draive-0.64.0-py3-none-any.whl

---
+++ packages/draive/draive-0.64.0-py3-none-any.whl/draive/resources/state.py
@@ -0,0 +1,94 @@
+from collections.abc import Sequence
+from typing import Any, Literal, overload
+
+from haiway import State, ctx
+
+from draive.commons import META_EMPTY, Meta
+from draive.resources.types import (
+    Resource,
+    ResourceDeclaration,
+    ResourceFetching,
+    ResourceListFetching,
+    ResourceMissing,
+    ResourceUploading,
+)
+
+__all__ = ("Resources",)
+
+
+class Resources(State):
+    @classmethod
+    async def fetch_list(
+        cls,
+        **extra: Any,
+    ) -> Sequence[ResourceDeclaration]:
+        return await ctx.state(cls).list_fetching(**extra)
+
+    @overload
+    @classmethod
+    async def fetch(
+        cls,
+        resource: ResourceDeclaration | str,
+        /,
+        **extra: Any,
+    ) -> Resource | None: ...
+
+    @overload
+    @classmethod
+    async def fetch(
+        cls,
+        resource: ResourceDeclaration | str,
+        /,
+        *,
+        default: Resource,
+        **extra: Any,
+    ) -> Resource: ...
+
+    @overload
+    @classmethod
+    async def fetch(
+        cls,
+        resource: ResourceDeclaration | str,
+        /,
+        *,
+        required: Literal[True],
+        **extra: Any,
+    ) -> Resource: ...
+
+    @classmethod
+    async def fetch(
+        cls,
+        resource: ResourceDeclaration | str,
+        /,
+        *,
+        default: Resource | None = None,
+        required: bool = True,
+        **extra: Any,
+    ) -> Resource | None:
+        uri: str = resource if isinstance(resource, str) else resource.uri
+
+        if fetched := await ctx.state(cls).fetching(
+            uri,
+            **extra,
+        ):
+            return fetched
+
+        elif required and default is None:
+            raise ResourceMissing(f"Missing resource: '{uri}'")
+
+        else:
+            return default
+
+    @classmethod
+    async def upload(
+        cls,
+        resource: Resource,
+        /,
+        **extra: Any,
+    ) -> None:
+        return await ctx.state(cls).uploading(resource, **extra)
+
+    list_fetching: ResourceListFetching
+    fetching: ResourceFetching
+    uploading: ResourceUploading
+    meta: Meta = META_EMPTY
---
+++ packages/draive/draive-0.64.0-py3-none-any.whl/draive/resources/types.py
@@ -0,0 +1,163 @@
+from collections.abc import Sequence
+from typing import Any, Protocol, Self, overload, runtime_checkable
+
+from haiway import Default, State
+
+from draive.commons import META_EMPTY, Meta
+from draive.multimodal import (
+    MediaData,
+    MediaReference,
+    MultimodalContentElement,
+    TextContent,
+    # validated_media_type,
+)
+from draive.parameters import DataModel
+
+__all__ = (
+    "Resource",
+    "ResourceContent",
+    "ResourceDeclaration",
+    "ResourceException",
+    "ResourceFetching",
+    "ResourceListFetching",
+    "ResourceMissing",
+    "ResourceUploading",
+)
+
+
+class ResourceException(Exception):
+    pass
+
+
+class ResourceMissing(ResourceException):
+    pass
+
+
+class ResourceDeclaration(DataModel):
+    uri: str
+    mime_type: str | None
+    name: str
+    description: str | None = None
+    meta: Meta = Default(META_EMPTY)
+
+
+# TODO: add ResourceTemplateDeclaration for ResourceTemplates support
+# https://modelcontextprotocol.io/docs/concepts/resources#resource-templates
+
+
+class ResourceContent(State):
+    @overload
+    @classmethod
+    def of(
+        cls,
+        content: bytes,
+        /,
+        *,
+        mime_type: str,
+    ) -> Self: ...
+
+    @overload
+    @classmethod
+    def of(
+        cls,
+        content: MultimodalContentElement | str,
+        /,
+        *,
+        mime_type: str | None = None,
+    ) -> Self: ...
+
+    @classmethod
+    def of(
+        cls,
+        content: MultimodalContentElement | str | bytes,
+        /,
+        *,
+        mime_type: str | None = None,
+    ) -> Self:
+        match content:
+            case str() as text:
+                return cls(
+                    mime_type=mime_type or "text/plain",
+                    blob=text.encode(),
+                )
+
+            case bytes() as data:
+                return cls(
+                    mime_type=mime_type or "application/octet-stream",
+                    blob=data,
+                )
+
+            case TextContent() as text_content:
+                return cls(
+                    mime_type=mime_type or "text/plain",
+                    blob=text_content.text.encode(),
+                )
+
+            case MediaData() as media_data:
+                return cls(
+                    mime_type=mime_type or media_data.media,
+                    blob=media_data.data,
+                )
+
+            case MediaReference():
+                raise ValueError("Resource can't use a media reference")
+
+            case other:
+                return cls(
+                    mime_type=mime_type or "application/json",
+                    blob=other.to_json().encode(),
+                )
+
+        return cls()
+
+    mime_type: str
+    blob: bytes
+
+    def to_multimodal(self) -> MultimodalContentElement:
+        match self.mime_type:
+            case "text/plain":
+                return TextContent(text=self.blob.decode())
+
+            case "application/json":
+                return DataModel.from_json(self.blob.decode())
+
+            case other:
+                # try to match supported media
+                return MediaData.of(
+                    self.blob,
+                    media=other,
+                )
+
+
+class Resource(State):
+    uri: str
+    name: str
+    description: str | None
+    content: Sequence[Self] | ResourceContent
+    meta: Meta = Default(META_EMPTY)
+
+
+@runtime_checkable
+class ResourceFetching(Protocol):
+    async def __call__(
+        self,
+        uri: str,
+        **extra: Any,
+    ) -> Resource | None: ...
+
+
+@runtime_checkable
+class ResourceListFetching(Protocol):
+    async def __call__(
+        self,
+        **extra: Any,
+    ) -> Sequence[ResourceDeclaration]: ...
+
+
+@runtime_checkable
+class ResourceUploading(Protocol):
+    async def __call__(
+        self,
+        resource: Resource,
+        **extra: Any,
+    ) -> None: ...
