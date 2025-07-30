5a3197848ef834a07156c547f0fe62add3223929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:08:16 +0000

    Add package voice-use/0.0.1/voice_use-0.0.1.tar.gz

---
+++ packages/voice-use/voice_use-0.0.1.tar.gz/voice_use-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="voice-use",  # Replace with your package name
+    version="0.0.1",
+    author="Elio Assaf",
+    description="Placeholder package.",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
