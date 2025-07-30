31f9de15341720aa75767c0cc06147a85075d583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:43 +0000

    Add package mqttsensor/0.2.0/mqttsensor-0.2.0.tar.gz

---
+++ packages/mqttsensor/mqttsensor-0.2.0.tar.gz/mqttsensor-0.2.0/src/mqttsensor/main.py
@@ -0,0 +1,19 @@
+from mqttsensor.config import Config
+from mqttsensor.mqtt.client import MqttClient
+from loguru import logger
+
+from mqttsensor import __version__ as version
+
+
+def init_client(config_file: str = "config.toml") -> tuple[Config, MqttClient]:
+    """
+    Main function to initialize the configuration and MQTT client.
+    """
+    config = Config(config_file)
+    mqtt_client = MqttClient(**config.get(["mqtt"]))
+    return config, mqtt_client
+
+
+def main():
+    logger.info("Downloaded module correctly")
+    logger.info(f"Version: {version}")
