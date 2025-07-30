27efc52e79380baab46061b5b7808d4a4c80ce16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:13:43 +0000

    Add package neurop/0.0.3/neurop-0.0.3-py3-none-any.whl

---
+++ packages/neurop/neurop-0.0.3-py3-none-any.whl/neurop/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.3"
+__authors__ = [
+    "John Doe"
+]
+__author_emails__ = [
+    "johndoe@gmail.com",
+]
+__url__ = "https://github.com/lonelyneutrin0/neurop"
+
+from . import src as src
