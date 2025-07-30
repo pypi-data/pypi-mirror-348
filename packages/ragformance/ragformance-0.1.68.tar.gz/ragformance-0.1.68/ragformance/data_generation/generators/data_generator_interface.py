37edf80c42ff0b06843d5e28c714069b9aa993f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:53 +0000

    Add package ragformance/0.1.68/ragformance-0.1.68.tar.gz

---
+++ packages/ragformance/ragformance-0.1.68.tar.gz/ragformance-0.1.68/ragformance/data_generation/generators/data_generator_interface.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Dict
+
+from ragformance.models.answer import AnnotatedQueryModel
+from ragformance.models.corpus import DocModel
+
+
+class QueryGeneratorInterface(ABC):
+    @abstractmethod
+    def generate_synthetic_queries(
+        self, corpus: list[DocModel], config: Dict = {}
+    ) -> list[AnnotatedQueryModel]:
+        raise NotImplementedError
+
+
+class AnswerGeneratorInterface(ABC):
+    @abstractmethod
+    def generate_answers(
+        self,
+        corpus: list[DocModel],
+        queries: list[AnnotatedQueryModel],
+        config: Dict = {},
+    ) -> list[AnnotatedQueryModel]:
+        raise NotImplementedError
+
+
+class CorpusGeneratorInterface(ABC):
+    @abstractmethod
+    def generate_corpus(self, docs_folder: Path, config: Dict = {}) -> list[DocModel]:
+        raise NotImplementedError
