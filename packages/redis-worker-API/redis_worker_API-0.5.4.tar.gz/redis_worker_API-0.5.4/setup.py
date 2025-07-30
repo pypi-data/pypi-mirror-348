1252e3f375fe03c3d0bc7fc7f1984bdb4b6f6c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:33 +0000

    Add package redis-worker-API/0.5.4/redis_worker_API-0.5.4.tar.gz

---
+++ packages/redis-worker-API/redis_worker_API-0.5.4.tar.gz/redis_worker_API-0.5.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the requirements.txt file
+def read_requirements():
+    with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+        return f.read().splitlines()
+
+setup(
+    name="redis_worker_API",
+    version="0.5.4",
+    packages=find_packages(),
+    description="Library to exchange messages between workers using Redis Streams",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  # Use "text/markdown" for a markdown README
+    author="GCP",
+    author_email="gonzalo.h.cordova@gmail.com",
+    # url="https://github.com/yourusername/my_library",
+    license="MIT",
+    install_requires=read_requirements(),
+   
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
