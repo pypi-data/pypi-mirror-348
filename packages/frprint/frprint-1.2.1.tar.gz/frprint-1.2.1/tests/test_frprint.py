ce002cc8eb5f21fa8d409c1039e15c0b8aefdb97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:49:57 +0000

    Add package frprint/1.2.1/frprint-1.2.1.tar.gz

---
+++ packages/frprint/frprint-1.2.1.tar.gz/frprint-1.2.1/tests/test_frprint.py
@@ -0,0 +1,20 @@
+def rPrint(String,Int = 1,Boolean = True) :
+    try :
+        verification = int(Int) / 1
+    except Exception as e:
+        raise ValueError("Please put a number in the Int entry")
+    if Boolean != True and Boolean != False :
+        raise Exception("Please add a True or False value to the Boolean entry")
+
+
+    if Boolean == True :
+        for i in range(int(Int)) :
+            print(String, end=" ")
+        return
+    if Boolean == False :
+        for i in range(int(Int)) :
+            print(String)
+        return
+
+
+rPrint("Python","q")
