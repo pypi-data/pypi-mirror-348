50fbfe2b1882e8897e81fff1c4c35f5d81fdf996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:15 +0000

    Add package PyrogramBuildingUtils/0.0.8/pyrogrambuildingutils-0.0.8.tar.gz

---
+++ packages/PyrogramBuildingUtils/pyrogrambuildingutils-0.0.8.tar.gz/pyrogrambuildingutils-0.0.8/README.md
@@ -0,0 +1,22 @@
+
+# PyrogramBuildingUtils
+
+## Description
+
+This project needed for fast building pyrogram templates bot. WIP
+
+## Install
+
+For install you can use this
+
+```bash
+pip install PyrogramBuildingUtils --index-url https://pypi.org/simple
+```
+
+## Usage
+
+You can run fast template build from
+
+```bash
+create_pyrogram
+```
---
+++ packages/PyrogramBuildingUtils/pyrogrambuildingutils-0.0.8.tar.gz/pyrogrambuildingutils-0.0.8/src/pyrogrambuildingutils/utils/pyrogram.py
@@ -0,0 +1,31 @@
+import logging
+
+from pyrogrambuildingutils.templates.botClass import Bot
+from pyrogrambuildingutils.templates.envtemplate import build_env
+from pyrogrambuildingutils.utils.utils import Utils
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Init:
+    logger = logging.getLogger("customPyrogram.init")
+
+    @Utils.init_time_measure()
+    def __init__(self) -> None:
+        # Utils.clear_pycache()
+        self._run()
+
+    def _run(self) -> None:
+        build_env()
+
+    def start(self) -> None:
+        bot = Bot()
+        bot.run()
+
+
+def run():
+    Init()
+
+
+def start():
+    Init().start()
