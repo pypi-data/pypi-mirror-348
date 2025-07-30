271a1dd3e690fbc3cc7daa97922bbb95d7196163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:54 +0000

    Add package noja-reader/1.0.0/noja_reader-1.0.0-py3-none-any.whl

---
+++ packages/noja-reader/noja_reader-1.0.0-py3-none-any.whl/demo.py
@@ -0,0 +1,11 @@
+# import reader
+
+# print(reader.__version__)
+# print(reader)
+
+# from reader import feed, viewer
+# article = feed.get_article(0)
+# viewer.show(article)
+
+from reader import feed
+feed.get_titles()
