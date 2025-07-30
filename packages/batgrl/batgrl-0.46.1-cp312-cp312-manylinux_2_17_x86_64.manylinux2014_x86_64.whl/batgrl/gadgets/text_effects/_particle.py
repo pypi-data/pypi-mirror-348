1f1bc569abb8ebac2ea72a0c8585c371594b2933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:47:05 +0000

    Add package batgrl/0.46.1/batgrl-0.46.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/batgrl/batgrl-0.46.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/batgrl/gadgets/text_effects/_particle.py
@@ -0,0 +1,63 @@
+from collections.abc import Iterator
+from dataclasses import dataclass
+from typing import Self
+
+from ...text_tools import Cell
+from ..text_field import Point, TextParticleField
+
+
+@dataclass
+class Particle:
+    """
+    A wrapper around an index into a text particle field's particle arrays.
+
+    Parameters
+    ----------
+    field : TextParticleField
+        The field which the particle belongs.
+    index : int
+        The index of the particle in the field.
+
+    Attributes
+    ----------
+    field : TextParticleField
+        The field which the particle belongs.
+    index : int
+        The index of the particle in the field.
+    cell : Cell
+        The particle's cell.
+    pos : Point
+        The particle's position.
+
+    Methods
+    -------
+    iter_from_field(field)
+        Yield all particles from a text particle field.
+    """
+
+    field: TextParticleField
+    index: int
+
+    @property
+    def cell(self) -> Cell:
+        """The particle's cell."""
+        return self.field.particle_cells[self.index]
+
+    @cell.setter
+    def cell(self, cell: Cell):
+        self.field.particle_cells[self.index] = cell
+
+    @property
+    def pos(self) -> Point:
+        """The particle's position."""
+        return Point(*self.field.particle_coords[self.index].tolist())
+
+    @pos.setter
+    def pos(self, pos: Point):
+        self.field.particle_coords[self.index] = pos
+
+    @classmethod
+    def iter_from_field(cls, field: TextParticleField) -> Iterator[Self]:
+        """Yield all particles from a text particle field."""
+        for i in range(len(field.particle_coords)):
+            yield cls(field, i)
