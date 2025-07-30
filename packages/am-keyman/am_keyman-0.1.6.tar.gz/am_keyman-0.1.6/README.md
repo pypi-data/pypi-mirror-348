c17c5fd898797160316f9539136eae5ec492422d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:49:22 +0000

    Add package am-keyman/0.1.6/am_keyman-0.1.6.tar.gz

---
+++ packages/am-keyman/am_keyman-0.1.6.tar.gz/am_keyman-0.1.6/README.md
@@ -0,0 +1,63 @@
+# Apple Music Keyman
+
+A small utility that gets Apple Music API keys for you. It even allows getting *user tokens*, which are a bit more annoying since they involve a whole OAuth flow.
+
+This tool is rudimentary and a work in progress.
+
+## How Apple Music authorization works
+
+You need a couple of things:
+
+1. Apple Developer subscription. Yes, a paid one
+2. A certificate generated with access to Apple Music & MusicKit APIs.
+3. A Key ID for that certificate.
+4. A Team ID for your developer account
+
+Using the team ID, private key file (.p8) and key ID, you build a JWT that becomes your **developer token**. This token alone allows access to AM resources that aren't related to a user.
+
+With that developer token, you can initiate an OAuth flow to get a **user token**. This one allows you to access the listening history for your account, for example.
+
+## Using Keyman (uvx)
+
+A convenient option is to use `uvx` to run the tool, without installing it:
+
+```bash
+# Set up the tool
+uvx am-keyman configure TEAM_ID_HERE KEY_ID_HERE KEY_FILE_PATH
+# Run the entire flow (dev token, user token)
+uvx am-keyman get-tokens
+```
+
+## Using Keyman (pip)
+
+The CLI utility is published on PyPI:
+
+```bash
+pip install am-keyman
+am-keyman
+```
+
+## Using Keyman (local development)
+
+```bash
+uv sync
+uv build
+# Set up the tool
+uv run am-keyman configure TEAM_ID_HERE KEY_ID_HERE KEY_FILE_PATH
+# Run the entire flow (dev token, user token)
+uv run am-keyman get-tokens
+# OPTIONAL: Get your developer token 
+uv run am-keyman get-dev-token 
+# OPTIONAL: Grab a user token for yourself
+uv run am-keyman get-user-token DEV_TOKEN_HERE
+# ... a web server will open up, follow the instructions
+# ... after completion, the token will show up in the console
+```
+
+## Important note
+
+If you mean to grab a user token using the command-line utility, **do not use Firefox**.
+
+The stricter security settings built into the browser will break the OAuth flow and it won't work.
+
+Safari worked great for me.
---
+++ packages/am-keyman/am_keyman-0.1.6.tar.gz/am_keyman-0.1.6/am_keyman/main.py
@@ -0,0 +1,161 @@
+from rich.console import Console
+from rich.progress import Progress
+from flask import Flask, request
+from jinja2 import Template
+import click
+import configparser
+import os
+import jwt
+import time
+import requests
+import webbrowser
+import threading
+import logging
+from importlib import resources as impresources
+
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
+
+console = Console()
+
+# Token expiration time must not be greater than 15777000 (6 months in seconds)
+EXP_TIME = 15777000
+
+
+def get_config():
+    config_file = configparser.ConfigParser()
+    if os.path.exists("./config.ini"):
+        config_file.read("./config.ini")
+        return config_file
+    else:
+        console.print(
+            "[red]No config file found. Please run `am_keyman configure.`[/red]"
+        )
+        exit(1)
+
+
+def validate_config():
+    config = get_config()
+    if "am_keyman" not in config:
+        console.print("[red]No config found. Please run `am_keyman configure`[/red]")
+        exit(1)
+    if not config.get("am_keyman", "team_id"):
+        console.print("[red]No team configured. Please run `am_keyman configure`[/red]")
+        exit(1)
+    if not config.get("am_keyman", "key_id"):
+        console.print("[red]No key configured. Please run `am_keyman configure`[/red]")
+        exit(1)
+    if not config.get("am_keyman", "key_file"):
+        console.print(
+            "[red]No key file configured. Please run `am_keyman configure`[/red]"
+        )
+        exit(1)
+    if not os.path.exists(config.get("am_keyman", "key_file")):
+        console.print(
+            f"[red]Key file not found. Make sure it exists at {config.get('am_keyman', 'key_file')}[/red]"
+        )
+        exit(1)
+
+
+@click.group()
+def cli():
+    """
+    This utility allows the easy procurement of Apple Music API tokens.
+    Note that you need an Apple Developer subscription for all of this to work.
+    """
+    pass
+
+
+@cli.command()
+@click.argument("team_id")
+@click.argument("key_id")
+@click.argument("key_file")
+def configure(team_id, key_id, key_file):
+    """Configure the utility with your team ID, key ID, and key file."""
+    new_config = configparser.ConfigParser()
+    new_config["am_keyman"] = {
+        "team_id": team_id,
+        "key_id": key_id,
+        "key_file": key_file,
+    }
+    with open("./config.ini", "w") as config_file:
+        new_config.write(config_file)
+    console.print("[green]Saved to config.ini.[/green]")
+
+
+@cli.command()
+@click.pass_context
+def get_tokens(ctx):
+    """Runs all the authentication steps and returns the tokens."""
+    dev_token = ctx.invoke(get_dev_token)
+    ctx.invoke(get_user_token, dev_token=dev_token)
+
+
+@cli.command()
+def get_dev_token():
+    """Generate a developer token."""
+    validate_config()
+    config = get_config()
+    secret = open(config.get("am_keyman", "key_file"), "r").read()
+    headers = {"alg": "ES256", "kid": config.get("am_keyman", "key_id")}
+    payload = {
+        "iss": config.get("am_keyman", "team_id"),
+        "exp": int(time.time() + EXP_TIME),
+        "iat": time.time(),
+    }
+    token = jwt.encode(payload, secret, algorithm="ES256", headers=headers)
+    with Progress() as progress:
+        task = progress.add_task("[cyan]Validating key...", total=None)
+        req = requests.get(
+            "https://api.music.apple.com/v1/test",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+    if req.status_code == 200:
+        progress.update(task, completed=1)
+        console.print("[green]Key is valid![/green]")
+    else:
+        progress.update(task, completed=1)
+        console.print("[red]Key is invalid![/red]")
+    console.print("[green]Generated token:[/green]")
+    print(token)
+    return token
+
+
+auth_app = Flask(__name__)
+
+
+@cli.command()
+@click.argument("dev_token")
+def get_user_token(dev_token):
+    """Generate a user token."""
+    if not dev_token:
+        console.print("[red]No dev token provided.[/red]")
+        exit(1)
+
+    @auth_app.route("/", methods=["POST", "GET"])
+    def index():
+        if request.method == "POST":
+            json = request.get_json()
+            if not json["token"]:
+                return "Invalid developer token", 403
+            else:
+                console.print("[green]User token generated![/green]")
+                print(json["token"])
+                os._exit(0)
+        else:
+            # Get template from module path
+            templ = impresources.files(__package__) / "user_auth.html"
+            with open(templ) as file:
+                template = Template(file.read())
+                return template.render(developer_token=dev_token)
+
+    def open_browser():
+        webbrowser.open("http://127.0.0.1:8000")
+
+    threading.Timer(1, open_browser).start()
+    auth_app.run(host="127.0.0.1", port=8000)
+
+
+if __name__ == "__main__":
+    cli()
