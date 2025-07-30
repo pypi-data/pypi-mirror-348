ad7cac3a2a2a23861e8a8ac0058e185400190986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:47 +0000

    Add package pyams-lib/0.2.1.1/pyams_lib-0.2.1.1.tar.gz

---
+++ packages/pyams-lib/pyams_lib-0.2.1.1.tar.gz/pyams_lib-0.2.1.1/setup.py
@@ -0,0 +1,39 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyams_lib",
+    version="0.2.1.1",
+    author="Dhiabi.Fathi",
+    author_email="dhiabi.fathi@gmail.com",
+    description="Simulation modeling of analog, digital and mixed-signal electronic elements and circuits",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license_file="LICENSE",
+    url="https://github.com/d-fathi/pyams_lib",
+    packages=find_packages(include=["pyams_lib", "pyams_lib.*"]),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    keywords=[
+        "simulation", "circuit", "electronics", "mixed-signal",
+        "analog", "EDA", "electronic modeling", "electrical engineering", "Digital"
+    ],
+    project_urls={
+        "Website": "https://pyams-lib.readthedocs.io/",
+        "Source": "https://github.com/d-fathi/pyams_lib",
+        "Documentation": "https://pyams-lib.readthedocs.io/",
+        "Bug Tracker": "https://github.com/d-fathi/pyams_lib/issues",
+        "Support": "https://github.com/d-fathi/pyams_lib/discussions",
+        "Donate": "https://ko-fi.com/pyams"
+    }
+)
