9189f22b0f5440806083e741f44435ac958c1d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:49:26 +0000

    Add package cnic3/1.5.5/cnic3-1.5.5-py3-none-any.whl

---
+++ packages/cnic3/cnic3-1.5.5-py3-none-any.whl/cnic3/ipv6_init.py
@@ -0,0 +1,11 @@
+from importlib.resources import files
+import random
+
+
+
+
+def init():
+    pkg = 'cnic3'
+    with files(pkg).joinpath('ipv6_pre.py').open('rb') as f:
+        text = f.readlines()
+    return random.shuffle(text)
