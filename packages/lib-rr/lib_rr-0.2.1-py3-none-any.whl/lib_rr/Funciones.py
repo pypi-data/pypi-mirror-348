12bf60db5505f18d846df4a764e1da825ebc1f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:12:48 +0000

    Add package lib-rr/0.2.1/lib_rr-0.2.1-py3-none-any.whl

---
+++ packages/lib-rr/lib_rr-0.2.1-py3-none-any.whl/lib_rr/Funciones.py
@@ -0,0 +1,33 @@
+#Funciones.py
+import random   # Importar random para generar numeros aleatorios
+import numpy as np
+
+# Funcion objetivo a minimizar
+def funcion_objetivo(x):
+    # """ calcula el cuadrado del valor de x. """
+    return x ** 2
+
+# Crear un individuo aleatorio
+def crear_individuo(valor_min, valor_max):
+    # """ Genera un numero aleatorio dentro de un rango especifico """
+    return random.uniform(valor_min, valor_max)
+print (crear_individuo)   
+
+# crear una poblacion inicial
+def crear_poblacion(tamano, valor_min, valor_max):
+    # Genera una lista de individuos aleatorios
+    return [crear_individuo(valor_min, valor_max) for _ in range(tamano)]
+
+# Ángulo de altura solar mínima
+def calcular_Hmin(latitud, invierno):
+    return (90 - latitud) - invierno
+
+# Distancia mínima entre paneles
+def calcular_DM(B, beta_grados, latitud, invierno):
+    beta = np.radians(beta_grados)
+    Hmin = np.radians(calcular_Hmin(latitud, invierno))
+    return B * np.cos(beta) + (B * np.sin(beta)) / np.tan(Hmin)
+
+# Función de aptitud para una población de betas
+def fitness(beta_array, B, latitud, invierno):
+    return -np.array([calcular_DM(B, beta, latitud, invierno) for beta in beta_array])
