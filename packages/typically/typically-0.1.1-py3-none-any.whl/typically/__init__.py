b26c8b99d59bda5989973f68a5b5d8660fc29af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:13:09 +0000

    Add package typically/0.1.1/typically-0.1.1-py3-none-any.whl

---
+++ packages/typically/typically-0.1.1-py3-none-any.whl/typically/__init__.py
@@ -0,0 +1,71 @@
+"""
+TODO: doc
+"""
+
+# * lib types
+from ._count import (
+    count,
+    ConvertibleToCount,
+    # BitCount,
+    # ByteCount,
+)
+from ._source import (
+    definition,
+    ConvertibleToDefinition,
+    module,
+    ConvertibleToModule,
+)
+from ._extended import estr
+from ._path import (
+    ConvertibleToPath,
+    FilePath,
+    DirectoryPath,
+    Path,
+    PathLike,
+)
+from ._model import Model, Documented, Blob
+
+# * other common types from various standard libs/dependencies
+
+# NOTE: typing_extensions is used over the default `typing` module for
+# more compatibility with pydantic for these types
+from typing_extensions import (
+    TypedDict,  # TODO: add others where pydantic requires typing_extensions type
+)
+
+from typing import (
+    Any,
+    NoReturn,
+    Never,
+    Self,
+    LiteralString,
+    ClassVar,
+    Final,
+    TypeVar,
+    Union,
+    Optional,
+    Literal,
+    TypeAlias,
+    Concatenate,
+    TypeGuard,
+    TypeIs,
+    ForwardRef,
+    overload,
+    # TODO: add others as needed
+)
+
+
+class of:
+    """
+    Various type variables. Called `of` so can be used like:
+
+    ```python
+    import typically as t
+
+    # t.of.M - as in: "type of M"
+    def load(path: t.ConvertibleToPath, model: type[t.of.M]) -> t.of.M:
+        ...
+    ```
+    """
+
+    M = TypeVar("M", bound=Model)
