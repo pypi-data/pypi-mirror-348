74f6f85e893e8175291d5a1428d90af4f696fcf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:36 +0000

    Add package orionis/0.265.0/orionis-0.265.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.265.0-py3-none-any.whl/orionis/framework.py
@@ -0,0 +1,38 @@
+#--------------------------------------------------------------------------
+# Framework Information
+#--------------------------------------------------------------------------
+# Name of the framework
+NAME = "orionis"
+
+# Current version of the framework
+VERSION = "0.265.0"
+
+# Full name of the author or maintainer of the project
+AUTHOR = "Raul Mauricio Uñate Castro"
+
+# Email address of the author or maintainer for contact purposes
+AUTHOR_EMAIL = "raulmauriciounate@gmail.com"
+
+# Short description of the project or framework
+DESCRIPTION = "Orionis Framework – Elegant, Fast, and Powerful."
+
+#--------------------------------------------------------------------------
+# Project URLs
+#--------------------------------------------------------------------------
+# URL to the project's skeleton or template repository (for initial setup)
+SKELETON = "https://github.com/orionis-framework/skeleton"
+
+# URL to the project's main framework repository
+FRAMEWORK = "https://github.com/orionis-framework/framework"
+
+# URL to the project's documentation
+DOCS = "https://orionis-framework.com/"
+
+# API URL to the project's JSON data
+API = "https://pypi.org/pypi/orionis/json"
+
+#--------------------------------------------------------------------------
+# Python Requirements
+#--------------------------------------------------------------------------
+# Minimum Python version required to run the project
+PYTHON_REQUIRES = ">=3.12"
