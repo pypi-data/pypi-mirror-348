2e76373b7ce02dbf5626fe14afeb00cb9d430278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:40 +0000

    Add package pyroblack/2.6.8/pyroblack-2.6.8.tar.gz

---
+++ packages/pyroblack/pyroblack-2.6.8.tar.gz/pyroblack-2.6.8/pyrogram/types/bots_and_keyboards/request_peer_type_channel.py
@@ -0,0 +1,39 @@
+#  pyroblack - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
+#  Copyright (C) 2024-present eyMarv <https://github.com/eyMarv>
+#
+#  This file is part of pyroblack.
+#
+#  pyroblack is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pyroblack is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with pyroblack.  If not, see <http://www.gnu.org/licenses/>.
+
+from ..object import Object
+
+
+class RequestPeerTypeChannel(Object):
+    """Object used to request clients to send a channel identifier.
+
+    Parameters:
+        is_creator (``bool``, *optional*):
+            If True, show only Channel which user is the owner.
+
+        is_username (``bool``, *optional*):
+            If True, show only Channel which has username.
+    """  # TODO user_admin_rights, bot_admin_rights
+
+    def __init__(self, is_creator: bool = None, is_username: bool = None, max: int = 1):
+        super().__init__()
+
+        self.is_creator = is_creator
+        self.is_username = is_username
+        self.max = max
