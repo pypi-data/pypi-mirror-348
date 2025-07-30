ce4d561b2d0bdcdf21fbf56c9d8626b01e50685c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:04 +0000

    Add package voiceflow-module/0.4.2/voiceflow_module-0.4.2.tar.gz

---
+++ packages/voiceflow-module/voiceflow_module-0.4.2.tar.gz/voiceflow_module-0.4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="voiceflow-module",
+    version="0.4.2",
+    author="Alex",
+    author_email="Alex@parnidia.com",
+    description="Module to help you with knowledge base management",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://parnidia.com",
+    packages=find_packages(),
+    classifiers=[                      # Metadata for PyPI
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "xmltodict",
+        "tiktoken"
+    ],
+)
