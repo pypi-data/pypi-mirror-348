7b7d89cd7e718522e77ee614d9f785096fc71cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:42 +0000

    Add package sigenergy2mqtt/2025.5.18/sigenergy2mqtt-2025.5.18.tar.gz

---
+++ packages/sigenergy2mqtt/sigenergy2mqtt-2025.5.18.tar.gz/sigenergy2mqtt-2025.5.18/sigenergy2mqtt/config/device_config.py
@@ -0,0 +1,77 @@
+from .smart_port_config import SmartPortConfig
+from .validation import check_bool, check_host, check_int_list, check_log_level, check_port
+from dataclasses import dataclass, field
+from typing import List
+import logging
+
+
+
+@dataclass
+class RegisterAccess:
+    read_only: bool = True
+    read_write: bool = True
+    write_only: bool = True
+
+@dataclass
+class DeviceConfig:
+    host: str = ""
+    port: int = 502
+
+    ac_chargers: List[int] = field(default_factory=list)
+    dc_chargers: List[int] = field(default_factory=list)
+    inverters: List[int] = field(default_factory=list)
+
+    log_level: int = logging.WARNING
+
+    registers = RegisterAccess()
+
+    smartport = SmartPortConfig()
+
+    def configure(self, config: dict, override: bool = False) -> None:
+        if isinstance(config, dict):
+            for field, value in config.items():
+                match field:
+                    case "host":
+                        if override:
+                            logging.debug(f"Applying 'modbus.host' override from env/cli ({value=})")
+                        self.host = check_host(value, f"modbus.{field}")
+                    case "port":
+                        if override:
+                            logging.debug(f"Applying 'modbus.port' override from env/cli ({value=})")
+                        self.port = check_port(value, f"modbus.{field}")
+                    case "log-level":
+                        if override:
+                            logging.debug(f"Applying 'modbus.log-level' override from env/cli ({value=})")
+                        self.log_level = check_log_level(value, f"modbus.{field}")
+                    case "read-only":
+                        if override:
+                            logging.debug(f"Applying 'modbus.read-only' override from env/cli ({value=})")
+                        self.registers.read_only = check_bool(value, f"modbus.{field}")
+                    case "read-write":
+                        if override:
+                            logging.debug(f"Applying 'modbus.read-write' override from env/cli ({value=})")
+                        self.registers.read_write = check_bool(value, f"modbus.{field}")
+                    case "write-only":
+                        if override:
+                            logging.debug(f"Applying 'modbus.write-only' override from env/cli ({value=})")
+                        self.registers.write_only = check_bool(value, f"modbus.{field}")
+                    case "ac-chargers":
+                        if override:
+                            logging.debug(f"Applying 'modbus.ac-chargers' override from env/cli ({value=})")
+                        self.ac_chargers = check_int_list(value, f"modbus.{field}")
+                    case "dc-chargers":
+                        if override:
+                            logging.debug(f"Applying 'modbus.dc-chargers' override from env/cli ({value=})")
+                        self.dc_chargers = check_int_list(value, f"modbus.{field}")
+                    case "inverters":
+                        if override:
+                            logging.debug(f"Applying 'modbus.inverters' override from env/cli ({value=})")
+                        self.inverters = check_int_list(value, f"modbus.{field}")
+                    case "smart-port":
+                        self.smartport.configure(value, override)
+                    case _:
+                        raise ValueError(f"modbus device configuration element contains unknown option '{field}'")
+            if len(self.inverters) == 0:
+                self.inverters.append(1)
+        else:
+            raise ValueError("modbus device configuration elements must contain options and their values")
