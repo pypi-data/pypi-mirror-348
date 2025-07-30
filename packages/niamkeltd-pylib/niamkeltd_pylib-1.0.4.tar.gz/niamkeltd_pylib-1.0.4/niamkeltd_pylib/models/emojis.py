a9b70cdfb0daee951f7386d7b93cebef17140d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:29 +0000

    Add package niamkeltd-pylib/1.0.4/niamkeltd_pylib-1.0.4.tar.gz

---
+++ packages/niamkeltd-pylib/niamkeltd_pylib-1.0.4.tar.gz/niamkeltd_pylib-1.0.4/niamkeltd_pylib/models/emojis.py
@@ -0,0 +1,12 @@
+# Emoji Code Version iOS 5, iOS 7 https://www.iemoji.com/view/emoji
+class Emojis:
+    THUMBS_UP = "\U0001F44D" # 👍
+    THUMBS_DOWN = "\U0001F44E" # 👎
+    RED_CROSS = "\u274C" # ❌
+    TICK_GREEN = "\u2705" # ✅
+    TICK_GREY = "\u2611\uFE0F" # ☑️
+    INFORMATION = "\u2139\uFE0F" # ℹ️
+    WARNING = "\u26A0\uFE0F" # ⚠️
+    HEART_RED = "\u2764\uFE0F" # ❤️
+    ARROW_CURVE_LEFT = "\u21A9\uFE0F" # ↩️
+    FAST_FORWARD = "\u23E9" # ⏩
