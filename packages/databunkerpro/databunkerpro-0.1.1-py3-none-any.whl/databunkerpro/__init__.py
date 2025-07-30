d0a1abc827fd02fa3c5f7196b612489f8d6ec884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:46 +0000

    Add package databunkerpro/0.1.1/databunkerpro-0.1.1-py3-none-any.whl

---
+++ packages/databunkerpro/databunkerpro-0.1.1-py3-none-any.whl/databunkerpro/__init__.py
@@ -0,0 +1,9 @@
+"""
+DatabunkerPro Python Client Library
+A Python client library for interacting with the DatabunkerPro API.
+"""
+
+from .api import DatabunkerproAPI
+
+__version__ = "0.1.1"
+__all__ = ["DatabunkerproAPI"]
