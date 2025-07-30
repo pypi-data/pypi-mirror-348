42f96e4875cc6fd2547dfe499828ed0926548658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:03 +0000

    Add package ryoma-ai/0.0.4/ryoma_ai-0.0.4.tar.gz

---
+++ packages/ryoma-ai/ryoma_ai-0.0.4.tar.gz/ryoma_ai-0.0.4/ryoma_ai/datasource/base.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+
+from ryoma_ai.datasource.metadata import Catalog
+from pydantic import BaseModel
+
+
+class DataSource(BaseModel, ABC):
+
+    model_config = {
+        "arbitrary_types_allowed": True,
+        "extra": "allow",
+    }
+
+    def __init__(self, type: str, **kwargs):
+        super().__init__(**kwargs)
+        self.type = type
+
+    @abstractmethod
+    def get_catalog(self, **kwargs) -> Catalog:
+        raise NotImplementedError("get_catalog is not implemented for this data source")
+
+    @abstractmethod
+    def crawl_catalogs(self, loader: Any, **kwargs) -> Optional[Catalog]:
+        raise NotImplementedError(
+            "crawl_metadata is not implemented for this data source."
+        )
+
