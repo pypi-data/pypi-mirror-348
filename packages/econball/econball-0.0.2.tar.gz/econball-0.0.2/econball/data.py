4419d5143c6e45e9b55c1958a97c149692f7e56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:36:30 +0000

    Add package econball/0.0.2/econball-0.0.2.tar.gz

---
+++ packages/econball/econball-0.0.2.tar.gz/econball-0.0.2/econball/data.py
@@ -0,0 +1,10 @@
+"""An enumeration for the different data soruces in econball."""
+
+from enum import StrEnum, auto
+
+
+class Data(StrEnum):
+    """Data enumeration."""
+
+    FRED = auto()
+    YFINANCE = auto()
