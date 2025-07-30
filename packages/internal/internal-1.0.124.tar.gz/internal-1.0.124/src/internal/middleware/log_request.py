a48be9d33a9e4a02583656657f7a41053b644ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:04 +0000

    Add package internal/1.0.124/internal-1.0.124.tar.gz

---
+++ packages/internal/internal-1.0.124.tar.gz/internal-1.0.124/src/internal/middleware/log_request.py
@@ -0,0 +1,31 @@
+import logging
+import time
+
+from fastapi import FastAPI, Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class LogRequestMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: FastAPI, logger: logging.Logger):
+        super().__init__(app)
+        self.app = app
+        self.logger = logger
+
+    async def dispatch(self, request: Request, call_next):
+        # 记录请求的URL和参数
+        url = request.url.path
+        method = request.method
+        headers = request.headers
+        params = dict(request.query_params)
+        body = await request.body()
+        self.logger.info(
+            f"Method: {method}, URL: {url} - Headers: {headers} - Params: {params} - Body: {body} start processing...")
+
+        # 记录请求处理时间
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+
+        self.logger.info(
+            f"Method: {method}, URL: {url} - Headers: {headers} - Params: {params} - Body: {body}, Completed in {process_time:.4f} seconds")
+        return response
