3496cb6f0c227c74f501832933f77907e9d9bf13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:47:07 +0000

    Add package batgrl/0.46.1/batgrl-0.46.1-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/batgrl/batgrl-0.46.1-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/batgrl/geometry/basic.py
@@ -0,0 +1,227 @@
+"""Basic geometry functions and types."""
+
+from __future__ import annotations
+
+from numbers import Real
+from typing import NamedTuple, Self
+
+import numpy as np
+from numpy.typing import NDArray
+
+__all__ = [
+    "clamp",
+    "lerp",
+    "points_on_circle",
+    "round_down",
+    "rect_slice",
+    "Point",
+    "Size",
+]
+
+
+def clamp(value: Real, min: Real | None, max: Real | None) -> Real:
+    """
+    If ``value`` is less than ``min``, returns ``min``; else if ``max`` is less than
+    ``value``, returns ``max``; else returns ``value``. A one-sided clamp is possible
+    by setting ``min`` or ``max`` to ``None``.
+
+    Parameters
+    ----------
+    value : Real
+        Value to clamp.
+    min : Real | None
+        Minimum of clamped value.
+    max : Real | None
+        Maximum of clamped value.
+
+    Returns
+    -------
+    Real
+        A value between `min` and `max`, inclusive.
+    """
+    if min is not None and value < min:
+        return min
+
+    if max is not None and value > max:
+        return max
+
+    return value
+
+
+def lerp(a: Real, b: Real, p: Real) -> Real:
+    """Linear interpolation of `a` to `b` with proportion `p`."""
+    return (1.0 - p) * a + p * b
+
+
+def points_on_circle(
+    n: int,
+    radius: float = 1.0,
+    center: tuple[float, float] = (0.0, 0.0),
+    offset: float = 0.0,
+) -> NDArray[np.float64]:
+    """
+    Return `n` points on a circle.
+
+    Parameters
+    ----------
+    n : int
+        Number of points on a circle.
+    radius : float, default: 1.0
+        Radius of circle.
+    center : tuple[float, float], default: (0.0, 0.0)
+        Center of circle.
+    offset : float, default: 0.0
+        Rotate output points by `offset` radians.
+
+    Returns
+    -------
+    NDArray[np.float32]
+        An `(n, 2)`-shaped NDArray of points on a circle.
+    """
+    angles = np.linspace(0, 2 * np.pi, endpoint=False, num=n) + offset
+    return radius * np.stack([np.sin(angles), np.cos(angles)]).T + center
+
+
+def rect_slice(pos: Point, size: Size) -> tuple[slice, slice]:
+    """
+    Return slices for indexing a rect in a numpy array.
+
+    Parameters
+    ----------
+    pos : Point
+        Position of rect.
+    size : Size
+        Size of rect.
+
+    Returns
+    -------
+    tuple[slice, slice]
+        Slices that index a rect in a numpy array.
+    """
+    y, x = pos
+    h, w = size
+    return slice(y, y + h), slice(x, x + w)
+
+
+def round_down(n: float) -> int:
+    """
+    Like the built-in `round`, but always rounds down.
+
+    Used instead of `round` for smoother geometry.
+    """
+    i, r = divmod(n, 1)
+    if r <= 0.5:
+        return int(i)
+    return int(i) + 1
+
+
+class Point(NamedTuple):
+    """
+    A 2-d point.
+
+    Note that y-coordinate is before x-coordinate. This convention is used so that the
+    2-d arrays that underly a gadget's data can be directly indexed with the point.
+
+    Parameters or attributes type-hinted `Point` can often take `tuple[int, int]` for
+    convenience.
+
+    Parameters
+    ----------
+    y : int
+        y-coordinate of point.
+    x : int
+        x-coordinate of point.
+
+    Attributes
+    ----------
+    y : int
+        y-coordinate of point.
+    x : int
+        x-coordinate of point.
+    """
+
+    y: int
+    """y-coordinate of point."""
+    x: int
+    """x-coordinate of point."""
+
+    def __add__(self, other: Self) -> Self:
+        y, x = self
+        oy, ox = other
+
+        return type(self)(y + oy, x + ox)
+
+    def __radd__(self, other: Self) -> Self:
+        oy, ox = other
+        y, x = self
+        return type(self)(oy + y, ox + x)
+
+    def __sub__(self, other: Self) -> Self:
+        y, x = self
+        oy, ox = other
+        return type(self)(y - oy, x - ox)
+
+    def __rsub__(self, other: Self) -> Self:
+        oy, ox = other
+        y, x = self
+        return type(self)(oy - y, ox - x)
+
+    def __neg__(self) -> Self:
+        y, x = self
+        return type(self)(-y, -x)
+
+
+class Size(NamedTuple):
+    """
+    A rectangular area.
+
+    Parameters or attributes type-hinted `Size` can often take `tuple[int, int]` for
+    convenience.
+
+    Parameters
+    ----------
+    height : int
+        Height of area.
+    width : int
+        Width of area.
+
+    Attributes
+    ----------
+    height : int
+        Height of area.
+    width : int
+        Width of area.
+    rows : int
+        Alias for height.
+    columns : int
+        Alias for width.
+    center : Point
+        Center of area.
+    """
+
+    height: int
+    """Height of area."""
+    width: int
+    """Width of area."""
+
+    @property
+    def rows(self) -> int:
+        """Alias for height."""
+        return self.height
+
+    @property
+    def columns(self) -> int:
+        """Alias for width."""
+        return self.width
+
+    def __contains__(self, point: Point) -> bool:
+        """Whether a point is within the rectangle."""
+        y, x = point
+        h, w = self
+        return 0 <= y < h and 0 <= x < w
+
+    @property
+    def center(self) -> Point:
+        """Center of area."""
+        h, w = self
+        return Point(h // 2, w // 2)
---
+++ packages/batgrl/batgrl-0.46.1-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/batgrl/texture_tools.py
@@ -0,0 +1,131 @@
+"""Tools for graphics."""
+
+from pathlib import Path
+from typing import Literal
+
+import cv2
+import numpy as np
+from numpy.typing import NDArray
+
+from .geometry import Point, Region, Size, rect_slice
+
+__all__ = ["Interpolation", "read_texture", "resize_texture", "composite"]
+
+Interpolation = Literal["nearest", "linear", "cubic", "area", "lanczos"]
+"""Interpolation methods for resizing graphic gadgets."""
+
+_INTERPOLATION_TO_CV_ENUM = {
+    "linear": cv2.INTER_LINEAR,
+    "cubic": cv2.INTER_CUBIC,
+    "area": cv2.INTER_AREA,
+    "lanczos": cv2.INTER_LANCZOS4,
+    "nearest": cv2.INTER_NEAREST,
+}
+
+
+def read_texture(path: Path) -> NDArray[np.uint8]:
+    """
+    Return a uint8 RGBA numpy array from a path to an image.
+
+    Parameters
+    ----------
+    path : Path
+        Path to image.
+
+    Returns
+    -------
+    NDArray[np.uint8]
+        An uint8 RGBA array of the image.
+    """
+    image = cv2.imread(str(path.absolute()), cv2.IMREAD_UNCHANGED)
+
+    if image.dtype == np.dtype(np.uint16):
+        image = (image // 257).astype(np.uint8)
+    elif image.dtype == np.dtype(np.float32):
+        image = (image * 255).astype(np.uint8)
+
+    # Add an alpha channel if there isn't one.
+    h, w, c = image.shape
+    if c == 3:
+        default_alpha_channel = np.full((h, w, 1), 255, dtype=np.uint8)
+        image = np.dstack((image, default_alpha_channel))
+
+    return cv2.cvtColor(image, cv2.COLOR_BGRA2RGBA)
+
+
+def resize_texture(
+    texture: NDArray[np.uint8],
+    size: Size,
+    interpolation: Interpolation = "linear",
+    out: NDArray[np.uint8] | None = None,
+) -> NDArray[np.uint8]:
+    """
+    Resize texture.
+
+    Parameters
+    ----------
+    texture : NDArray[np.uint8]
+        An RGBA texture to resize.
+    size : Size
+        The new size of the texture.
+    interpolation : Interpolation, default: "linear"
+        Interpolation used when resizing texture.
+    out : NDArray[np.uint8] | None, default: None
+        Optional output array. If None, a new array is created.
+
+    Returns
+    -------
+    NDArray[np.uint8]
+        A new uint8 RGBA array.
+    """
+    old_h, old_w = texture.shape[:2]
+    h, w = size
+    if old_h == 0 or old_w == 0 or h == 0 or w == 0:
+        return np.zeros((h, w, 4), np.uint8)
+    return cv2.resize(
+        texture, (w, h), dst=out, interpolation=_INTERPOLATION_TO_CV_ENUM[interpolation]
+    )
+
+
+def composite(
+    source: NDArray[np.uint8],
+    dest: NDArray[np.uint8],
+    pos: Point = Point(0, 0),
+    mask_mode: bool = False,
+) -> None:
+    """
+    Composite source texture onto destination texture at given position.
+
+    If `mask_mode` is true, source alpha values less than 255 are ignored.
+
+    Parameters
+    ----------
+    source : NDArray[np.uint8]
+        The texture to composite.
+    dest : NDArray[np.uint8]
+        The texture on which the source is painted.
+    pos : Point, default: Point(0, 0)
+        Position of the source on the destination.
+    mask_mode : bool, default: False
+        Whether to ignore alpha values less than 255.
+    """
+    sh, sw, _ = source.shape
+    dh, dw, _ = dest.shape
+
+    dest_reg = Region.from_rect((0, 0), (dh, dw))
+    source_reg = Region.from_rect(pos, (sh, sw))
+
+    if intersection := source_reg & dest_reg:
+        rpos, size = next(intersection.rects())
+        dest_tex = dest[rect_slice(rpos, size)]
+        source_tex = source[rect_slice(rpos - pos, size)]
+        source_alpha = source_tex[..., 3]
+
+        if mask_mode:
+            mask = source_alpha == 255
+            dest_tex[mask] = source_tex[mask]
+        else:
+            buffer = np.subtract(source_tex, dest_tex, dtype=float)
+            buffer *= source_alpha[..., None]
+            buffer /= 255
+            np.add(buffer, dest_tex, out=dest_tex, casting="unsafe")
