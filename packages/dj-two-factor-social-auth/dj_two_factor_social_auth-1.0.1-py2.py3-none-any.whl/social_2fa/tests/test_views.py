4c64316f5a2dc18f600a45edea1b076ee834524a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:09:44 +0000

    Add package dj-two-factor-social-auth/1.0.1/dj_two_factor_social_auth-1.0.1-py2.py3-none-any.whl

---
+++ packages/dj-two-factor-social-auth/dj_two_factor_social_auth-1.0.1-py2.py3-none-any.whl/social_2fa/tests/test_views.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.urls import reverse
+from model_bakery import baker
+
+
+class AuthenticationViewTests(TestCase):
+    def setUp(self):
+        self.user = baker.make("User")
+        session = self.client.session
+        baker.make(
+            "Partial",
+            token="foo",
+            kwargs={"user": self.user.id},
+            backend="test-backend",
+        )
+        session["tfa_partial_token"] = "foo"
+        session.save()
+        self.address = reverse("social_2fa:two_factor_authentication")
+
+    def test_get(self):
+        response = self.client.get(self.address)
+        self.assertContains(
+            response,
+            '<input type="text" name="otp_token" maxlength="6" minlength="6" autofocus="autofocus"'
+            'pattern="[0-9]*" autocomplete="one-time-code" id="id_otp_token">',
+            html=True,
+        )
+
+    def test_post_invalid_token(self):
+        response = self.client.post(self.address, {"otp_token": "123456"})
+        self.assertContains(
+            response,
+            "<li>Invalid token. Please make sure you have entered it correctly.</li>",
+            html=True,
+        )
+
+    def test_post(self):
+        device = self.user.staticdevice_set.create()
+        device.token_set.create(token="123456")
+        response = self.client.post(self.address, {"otp_token": "123456"})
+        self.assertRedirects(
+            response,
+            "/complete/test-backend/",
+            fetch_redirect_response=False,
+        )
