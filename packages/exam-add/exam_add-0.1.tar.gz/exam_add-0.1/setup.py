faadfb9c73e8c69d89a6e722c4a92cd19c94146b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:07 +0000

    Add package exam-add/0.1/exam_add-0.1.tar.gz

---
+++ packages/exam-add/exam_add-0.1.tar.gz/exam_add-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='exam_add',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add any command-line scripts here
+        ],
+    },
+)
